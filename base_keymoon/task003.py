90fd3b85996d11bf3ef7df7033df49d145194744
key-moon <kymn0116@gmail.com>
2025-08-01 14:35:07 +0900

    add task003

---
+++ base_keymoon/task003.py
@@ -0,0 +1,7 @@
+# サイクルを発見する（3サイクルか4サイクルかの二択）
+# def p(g):
+#   if g[:3]==g[3:]:
+#     return[[c*2for c in r]for r in g+g[:3]]
+#   else:
+#     return[[c*2for c in r]for r in g+g[2:5]]
+p=lambda g:[[c*2for c in r]for r in g+g[0if g[:3]==g[3:]else 2:][:3]]
