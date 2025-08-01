d5f5b2b66fffd18cf4ac6a1b219df5bb143de20d
Yu212 <yu212.mc@gmail.com>
2025-08-01 15:23:10 +0900

    update

--- base_yu/task001.py
+++ base_yu/task001.py
@@ -1,1 +1,1 @@
-def p(g):return[sum(([[0]*3,g[i%3]][g[i//3][j]>0]for j in(0,1,2)),[])for i in range(9)]
+p=lambda g:[sum(([[0]*3,g[i%3]][g[i//3][j]>0]for j in(0,1,2)),[])for i in range(9)]
--- base_yu/task002.py
+++ base_yu/task002.py
@@ -1,10 +1,11 @@
 def p(g):
- h,w=len(g),len(g[0])
- for r in range(w):
-  for d in range(h):
-   for l in range(r):
-    for u in range(d):
-     if set(g[d][l:r]+g[u-1][l:r]+[g[i][l] for i in range(u,d)]+[g[i][r-1] for i in range(u,d)])=={1}:
-      for i in range(u,d):
-       g[i][l:r]=[4]*(r-l)
+ t=range
+ for d in t(len(g)):
+  for r in t(len(g[0])):
+   for l in t(1,r):
+    for u in t(1,d):
+     v=g[u:d]
+     if sum(sum(s[l:r])for s in v)<all(g[d][l:r]+g[u-1][l:r]+[s[l-1]&s[r]for s in v]):
+      for s in v:
+       s[l:r]=[4]*(r-l)
  return g
