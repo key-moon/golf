c06342c8dde26cfa53ae9d7a3b07aed73aaf4a77
Yu212 <yu212.mc@gmail.com>
2025-08-01 20:43:07 +0900

    328

--- base_code/task328.py
+++ base_code/task328.py
@@ -1,7 +1,11 @@
 def p(g):
- a,b=len(g),len(g[0]);s=[(i,j,g[i][j])for i in range(a)for j in range(b)if g[i][j]]
- for i in range(a):
-  for j in range(b):
-   d=[max(abs(i-x),abs(j-y))for x,y,_ in s];d.sort();m=d[0]
-   g[i][j]=s[d.index(m)][2]if m<d[1]and m%2<1 else 0
- return g
+ h,w=len(g),len(g[0])
+ s=[(i,j,g[i][j])for i in range(h)for j in range(w)if g[i][j]]
+ o=[[0]*w for _ in range(h)]
+ for i in range(h):
+  for j in range(w):
+   d=[abs(i-x)+abs(j-y)for x,y,_ in s];m=min(d)
+   if d.count(m)==1:
+    x,y,v=s[d.index(m)]
+    if max(abs(i-x),abs(j-y))%2<1:o[i][j]=v
+ return o
