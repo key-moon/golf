4b56b2dff50eb671d6e2bf9415589f9e5d6e4727
Yu212 <yu212.mc@gmail.com>
2025-08-01 17:40:22 +0900

    396

--- base_yu/task396.py
+++ base_yu/task396.py
@@ -1,16 +1,16 @@
 def p(g):
- t=range
+ t,j=range,len
  x=m=0
- for d in t(len(g)+1):
-  for r in t(len(g[0])+1):
+ for d in t(j(g)+1):
+  for r in t(j(g[0])+1):
    for l in t(r):
     for u in t(d):
      s=[t[l:r]for t in g[u:d]]
      b=s[-1]+s[0]+[min(v[0],v[-1])for v in s]
      # m<(r-l)*(d-u) and len(set(b)) == 1 and 1 <= b[0]
-     if (m<(r-l)*(d-u))==len(set(b))<=b[0]:
+     if (m<(r-l)*(d-u))==j(set(b))<=b[0]:
       m,x=(r-l)*(d-u),s
- c,n=sum(set(sum(x,[])))-x[0][0],len(x)
+ c,n=sum(set(sum(x,[])))-x[0][0],j(x)
  for i in t(m):
   x[i%n][i//n]=x[i%n][i//n]and c
  return x
