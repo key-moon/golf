d993279149c5866086e15606c5fd3a24095bfc93
Yu212 <yu212.mc@gmail.com>
2025-08-02 00:34:45 +0900

    update

--- base_yu/task387.py
+++ base_yu/task387.py
@@ -1,11 +1,10 @@
 #上下の囲み+左右方向の破線を書く→90度回転
 R=range
 def p(g):
- h,w=len(g),len(g[0])
  u=[s[:]for s in g]
  for _ in 0,1:
-  for i in R(h):
-   if a:=[j for j in R(w)if (s:=g[i])[j]]:
+  for i in R(len(g)):
+   if a:=[j for j in R(len(g[0]))if (s:=g[i])[j]]:
     j,k=a
     l,r=u[i-1],u[i+1]
     l[j-1:j+2]=r[j-1:j+2]=[s[k]]*3
@@ -14,5 +13,4 @@ def p(g):
      u[i][t]=-~min(t-j,k-t)%2*5
   *g,=zip(*g)
   *u,=map(list,zip(*u))
-  h,w=w,h
  return u
