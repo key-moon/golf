546823d284ee7496f5409dc24ab1788c71119baa
Yu212 <yu212.mc@gmail.com>
2025-08-01 20:47:48 +0900

    315

--- base_code/task315.py
+++ base_code/task315.py
@@ -1,9 +1,8 @@
-def p(g):
-    m = max(sum(g, []))
-    o = [[0]*9 for _ in [1]*9]
-    for i, r in enumerate(g):
-        for j, v in enumerate(r):
-            if v == m:
-                for x in range(3):
-                    o[3*i+x][3*j:3*j+3] = g[x]
-    return o
+def p(val_g):
+ val_n=len(val_g);val_m=max(max(val_r)for val_r in val_g);val_o=[[0]*val_n*val_n for _ in range(val_n*val_n)]
+ for val_i,val_r in enumerate(val_g):
+  for val_j,val_v in enumerate(val_r):
+   if val_v==val_m==2:
+    for val_di,val_rr in enumerate(val_g):
+     for val_dj,val_c in enumerate(val_rr):val_o[val_i*val_n+val_di][val_j*val_n+val_dj]=val_c
+ return val_o
