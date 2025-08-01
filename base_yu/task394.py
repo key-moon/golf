7db8f3a12bfc30d73fa6c3b8d94ceef95a5ff005
Yu212 <yu212.mc@gmail.com>
2025-08-01 18:33:50 +0900

    394

--- base_yu/task394.py
+++ base_yu/task394.py
@@ -4,4 +4,4 @@
 #  s=any(0<g[i//w][i%w]!=g[i//w+2][i%w]>0 for i in r((h-2)*w))+2
 #  return[*filter(None,[[g[[i-s,i+s][i+s<h]][[j-s,j+s][j+s<w]]for j in r(w)if g[i][j]<1]for i in r(h)])]
 r=range
-p=lambda g:(h:=len(g),w:=len(g[0]),s:=any(0<g[i//w][i%w]!=g[i//w+2][i%w]>0for i in r((h-2)*w))+2)and[*filter(None,[[g[[i-s,i+s][i+s<h]][[j-s,j+s][j+s<w]]for j in r(w)if g[i][j]<1]for i in r(h)])]
+p=lambda g:(w:=len(g),s:=any(0<g[i//w][i%w]!=g[i//w+2][i%w]>0for i in r((w-2)*w))+2)and[*filter(None,[[g[[i-s,i+s][i+s<w]][[j-s,j+s][j+s<w]]for j in r(w)if g[i][j]<1]for i in r(w)])]
