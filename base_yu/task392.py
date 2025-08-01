0f338a1090f4678ecf855bec410cab3064868aef
Yu212 <yu212.mc@gmail.com>
2025-08-01 23:39:23 +0900

    update

--- base_yu/task392.py
+++ base_yu/task392.py
@@ -20,16 +20,16 @@
 #  for i in range(100):
 #   g[i%10][i//10]=u[d(b,i)%(y-x)]or 5
 #  return g
-
+R=range
 def p(g):
  d=lambda i,j:max(abs(j//10*2+1-i//21),abs(j%10*2+1-i%21))
  b,v=0,9
- for i in range(441):
-  m={(d(i,j),g[j%10][j//10])for j in range(100)}
+ for i in R(441):
+  m={(d(i,j),g[j%10][j//10])for j in R(100)}
   if v>(k:=sum(v>0for _,v in m))and len(dict(m))==len(u:=m):v,b=k,i
  x,y,*_=sorted(k for k,v in u if v)
 #  return[[u[d(b,j*10+i)%(y-x)]or 5for j in range(10)]for i in range(10)]
- for i in range(100):g[i%10][i//10]=dict(u)[d(b,i)%(y-x)]or 5
+ for i in R(100):g[i%10][i//10]=dict(u)[d(b,i)%(y-x)]or 5
  return g
 
 # def p(g):
--- base_yu/task396.py
+++ base_yu/task396.py
@@ -8,8 +8,8 @@ def p(g):
      s=[t[l:r]for t in g[u:d]]
      b=s[-1]+s[0]+[min(v[0],v[-1])for v in s]
      # m<(r-l)*(d-u) and len(set(b)) == 1 and 1 <= b[0]
-     if (m<(r-l)*(d-u))==j({*b})<=b[0]:
-      m,x=(r-l)*(d-u),s
+     if (m<(S:=(r-l)*(d-u)))==j({*b})<=b[0]:
+      m,x=S,s
  c,n=sum({*sum(x,[])})-x[0][0],j(x)
  for i in t(m):
   x[i%n][i//n]=x[i%n][i//n]and c
