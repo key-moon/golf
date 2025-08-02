cac0b51a72740888608b332dbcaa6e224445848f
Yu212 <yu212.mc@gmail.com>
2025-08-02 15:06:54 +0900

    update383

--- base_yu/task383.py
+++ base_yu/task383.py
@@ -1,8 +1,8 @@
 def p(g):
  u=[s[:]for s in g]
  for _ in 0,1:
-  for i in range(len(g)):
-   *a,=filter(int,s:=g[i])
+  for i,s in enumerate(g):
+   *a,=filter(int,s)
    if a and s.count(a[0])|1==3:
     l=s.index(0,k:=s.index(a[0]))
     u[i]=[a[2]]*len(g[0])
@@ -10,3 +10,17 @@ def p(g):
   *g,=zip(*g)
   *u,=map(list,zip(*u))
  return u
+
+
+# def p(g):
+#  u=[s[:]for s in g]
+#  for _ in 0,1:
+#   for i in range(len(g)):
+#    *a,=filter(int,s:=g[i])
+#    if a and s.count(a[0])|1==3:
+#     l=s.index(0,k:=s.index(a[0]))
+#     u[i]=[a[2]]*len(g[0])
+#     u[i][k:l]=[a[0]]*(l-k)
+#   *g,=zip(*g)
+#   *u,=map(list,zip(*u))
+#  return u
