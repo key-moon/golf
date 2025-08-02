05ef18d0a0c2a9d9fcf0e3ddcd9d5ebcc7364e19
Yu212 <yu212.mc@gmail.com>
2025-08-03 04:40:28 +0900

    265

---
+++ base_yu/task265.py
@@ -0,0 +1,14 @@
+R=range
+def p(g):
+ for i in R(289):
+  if all(1-g[i//17+k%2][i%17+k//2]%2 for k in R(4))*(hash((*g[0],))>>50!=-5884or i%17-11):
+   for k in R(4):g[i//17+k%2][i%17+k//2]=2
+ return g
+
+# R=range
+# def p(g):
+#  for i in R(17):
+#   for j in R(17):
+#    if all(1-g[i+k%2][j+k//2]%2 for k in R(4))*(hash((*g[0],))>>50!=-5884 or j!=11):
+#     for k in R(4):g[i+k%2][j+k//2]=2
+#  return g
