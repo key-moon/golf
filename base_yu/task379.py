515faa77ce2a72c7b64a2b7d5ce59a846c76b7bb
Yu212 <yu212.mc@gmail.com>
2025-08-02 15:36:02 +0900

    379

---
+++ base_yu/task379.py
@@ -0,0 +1,96 @@
+E,R=enumerate,range
+def p(g):
+ for _ in 0,1:
+  if l:=[i for i,s in E(g)if all(s)]:
+   for i,s in E(g):
+    for j,v in E(s):
+     x,y=l[-(l[-1]<i)],l[-(l[0]<i)]
+     for w,k in([(x,k)for k in R(x,i)][::-1]+[(y,k)for k in R(i,y+1)])*(v==2):
+      for a in R(9):
+       g[w+a//3-1][j+a%3-1]=8
+      g[k][j]=2
+  *g,=map(list,zip(*g))
+ return g
+
+
+# def p(g):
+#  for _ in 0,1:
+#   if l:=[i for i,s in enumerate(g)if all(s)]:
+#    for i,s in enumerate(g):
+#     for j,v in enumerate(s):
+#      x,y=l[-(l[-1]<i)],l[-(l[0]<i)]
+#      if v==2:
+#       for w,k in[(x,k)for k in range(x,i)][::-1]+[(y,k)for k in range(i,y+1)]:
+#        for a in range(9):
+#         g[w+a//3-1][j+a%3-1]=8
+#        g[k][j]=2
+#   *g,=map(list,zip(*g))
+#  return g
+
+
+# def p(g):
+#  for _ in 0,1:
+#   if l:=[i for i,s in enumerate(g)if all(s)]:
+#    for i,s in enumerate(g):
+#     for j,v in enumerate(s):
+#      for a in range((v==2)*9):
+#       for k in range(v:=l[-(l[-1]<i)],i)[::-1]:
+#        g[v+a//3-1][j+(a+2)%3-1]=8
+#        g[k][j]=2
+#       for k in range(i,1+(v:=l[-(l[0]<i)])):
+#        g[v+1-a//3][j+1-(a+2)%3]=8
+#        g[k][j]=2
+#   *g,=map(list,zip(*g))
+#  return g
+
+
+# def p(g):
+#  for _ in 0,1:
+#   if l:=[i for i,s in enumerate(g)if all(s)]:
+#    for i,s in enumerate(g):
+#     for j,v in enumerate(s):
+#      if v&2:
+#       for k in range(v:=[l[0],l[-1]][l[-1]<i],i)[::-1]:
+#        for a in range(9):
+#         g[v+a%3-1][j+a//3-1]=8
+#        g[k][j]=2
+#       for k in range(i,1+(v:=[l[0],l[-1]][l[0]<i])):
+#        for a in range(9):
+#         g[v+a%3-1][j+a//3-1]=8
+#        g[k][j]=2
+#   *g,=map(list,zip(*g))
+#  return g
+
+# def p(g):
+#  for _ in 0,1:
+#   if l:=[i for i in range(len(g))if all(g[i])]:
+#    for i in range(len(g)):
+#     for j in range(len(g[0])):
+#      if g[i][j]&2:
+#       for k in range(v:=[l[0],l[-1]][l[-1]<i],i)[::-1]:
+#        for a in range(9):
+#         g[v+a%3-1][j+a//3-1]=8
+#        g[k][j]=2
+#       for k in range(i,1+(v:=[l[0],l[-1]][l[0]<i])):
+#        for a in range(9):
+#         g[v+a%3-1][j+a//3-1]=8
+#        g[k][j]=2
+#   *g,=map(list,zip(*g))
+#  return g
+
+# def p(g):
+#  for _ in 0,1:
+#   if l:=[i for i in range(len(g))if all(g[i])]:
+#    for i in range(len(g)):
+#     for j in range(len(g[0])):
+#      if g[i][j]&2:
+#       for k in range(v:=[l[0],l[-1]][l[-1]<i],i)[::-1]:
+#        for a in range(9):
+#         g[v+a%3-1][j+a//3-1]=8
+#        g[k][j]=2
+#       for k in range(i,1+(v:=[l[0],l[-1]][l[0]<i])):
+#        for a in range(9):
+#         g[v+a%3-1][j+a//3-1]=8
+#        g[k][j]=2
+#   *g,=map(list,zip(*g))
+#  return g
