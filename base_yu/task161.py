d1964c758f81cac4dec44b799a9abe45fb035c1c
Yu212 <yu212.mc@gmail.com>
2025-08-07 01:26:01 +0900

    update161

--- base_yu/task161.py
+++ base_yu/task161.py
@@ -3,8 +3,8 @@ def p(g):
  h,w=len(g),len(g[0])
  s=sum([s[1:w-1]for s in g[1:h-1]],[])
  u=[w*[(g[i][0]not in s)*g[i][0]]for i in R(h)]
- for i in R(w):
-  for j in R(h*(g[0][i]not in s)):u[j][i]=g[0][i]
+ for I in R(w*h):
+  if g[0][I%w]not in s:u[I//w][I%w]=g[0][I%w]
  return u
   
 
