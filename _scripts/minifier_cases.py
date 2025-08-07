41ca70c5189014ddc5bf171d87a7b605029c5d95
key-moon <kymn0116@gmail.com>
2025-08-08 08:28:15 +0900

    move scripts

---
+++ _scripts/minifier_cases.py
@@ -0,0 +1,17 @@
+import ast
+import zlib
+from compress import compress
+import python_minifier
+from strip import strip_for_zlib, strip_for_plain, get_stripper
+from python_minifier.ast_printer import print_ast
+
+# print(print_ast(ast.parse("if a:=1:1")))
+
+print(python_minifier.minify('(a:=1,2)+1'))
+print(python_minifier.minify('f(a:=1,2)'))
+print(python_minifier.minify('if a:=1:1'))
+print(python_minifier.minify('while a:=1:1'))
+print(python_minifier.minify('for _ in (a:=[1]):...'))
+print(python_minifier.minify('[1][a:=1]'))
+print(python_minifier.minify('[a:=1 for _ in (1,)]'))
+print(python_minifier.minify('for i in 1,2:...'))
