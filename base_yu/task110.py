59c99986a2ee10f3e26ca4ec36f1339cbb2c2f9e
Yu212 <yu212.mc@gmail.com>
2025-08-07 19:06:20 +0900

    update 110/363

--- base_yu/task110.py
+++ base_yu/task110.py
@@ -2,7 +2,7 @@ R=range
 def p(g):
  for y in R(-20,20):
   for x in R(-20,20):
-   if 1-any((g[i][j]!=g[i-y][j-x])*g[i][j]*g[i-y][j-x]for i in R(29)for j in R(29)if(i-y<29)*(j-x<29)):
+   if~-any((g[i][j]!=g[i-y][j-x])*g[i][j]*g[i-y][j-x]for i in R(29)for j in R(29)if(i-y<29)*(j-x<29)):
     for I in R(841):
      if-1<(i:=I%29)-y<29and-1<(j:=I//29)-x<29:
       s,t=g[i-y],g[i]
