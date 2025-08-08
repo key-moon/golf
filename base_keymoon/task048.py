19225c52a5926b07605c33fb220f5c6833d1a512
key-moon <kymn0116@gmail.com>
2025-08-08 10:46:55 +0900

    improve 048

--- base_keymoon/task048.py
+++ base_keymoon/task048.py
@@ -8,11 +8,25 @@
 
 # これ最短99らしいので方針違うんだろうな〜
 
+# 171
+# import re
+# def p(g):
+#  g=eval(str(g).replace("2","1",1))
+#  for x in range(99):
+#   g=[re.sub("81|21","11","".join(map(str,r))) for r in zip(*g[::-1])]
+#  return[[8-("2"in str(g))*8]]
+
+# 167
+# d=re.sub(r'\D','',str(g).replace("2","1",1).replace("],","3"))
+# d=re.sub('[[ ,]','',str(g)).replace("2","1",1)
+# d=re.sub("2","3",re.sub('[[ ,]','',str(g)),1)
+# d=re.sub(f"(8|2)(?=(.{{{len(g[0])}}})?1)","1",d)[::-1]
+# d=re.sub("(8|2)(?=("+"."*len(g[0])+")?1)","1",d)[::-1]
+# [8-("2"in d)*8]
+# [0,8]["2"in d]
 import re
 def p(g):
- w=len(g[0])
+ d=re.sub("2","1",re.sub("[[ ,]","",str(g)),1)
  for x in range(99):
-  if g[i:=x//w][j:=x%w]&2:g[i][j]=1;break
- for x in range(99):
-  g=[re.sub("81|21","11","".join(map(str,r))) for r in zip(*g[::-1])]
- return[[8-("2"in str(g))*8]]
+  d=re.sub("(8|2)(?=("+"."*len(g[0])+")?1)","1",d)[::-1]
+ return[[[8,0]["2"in d]]]
