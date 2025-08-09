cda5005c27090dbced3c2305ffae5d5e344e3639
Yu212 <yu212.mc@gmail.com>
2025-08-09 15:00:29 +0900

    update 395

--- base_yu/task395.py
+++ base_yu/task395.py
@@ -1,2 +1,3 @@
 # def p(g):n=len(g)//2;return[[2*(a==b==0)for a,b in zip(g[i],g[i+n])]for i in range(n)]
-def p(g):return[[2*(a==b)for a,b in zip(*c)]for c in zip(g,g[len(g)//2:])]
+# def p(g):return[[2*(a==b)for a,b in zip(*c)]for c in zip(g,g[len(g)//2:])]
+p=lambda g:[[2*(a==b)for a,b in zip(*c)]for c in zip(g,g[3:])]
