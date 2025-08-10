59ef604c4c0c48598c3400c9b5e8575d3ced7fe8
key-moon <kymn0116@gmail.com>
2025-08-10 22:26:07 +0900

    improve 048/325

--- base_keymoon/task048.py
+++ base_keymoon/task048.py
@@ -24,9 +24,11 @@
 # d=re.sub("(8|2)(?=("+"."*len(g[0])+")?1)","1",d)[::-1]
 # [8-("2"in d)*8]
 # [0,8]["2"in d]
-import re
+# from re import* <- 156
+# import re;s=re.sub # <- 153
+import re;s=re.sub # <- 153
 def p(g):
- d=re.sub("2","1",re.sub("[[ ,]","",str(g)),1)
+ d=s("2","1",s("[[ ,]","",str(g)),1)
  for x in d:
-  d=re.sub("(8|2)(?=("+"."*len(g[0])+")?1)","1",d)[::-1]
+  d=s("(8|2)(?=("+"."*len(g[0])+")?1)","1",d)[::-1]
  return[[[8,0]["2"in d]]]
