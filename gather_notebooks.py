a0b6ce5860f3ed53ad0835928e3efca3bf4776a8
key-moon <kymn0116@gmail.com>
2025-08-12 06:10:48 +0900

    add new notebooks

--- gather_notebooks.py
+++ gather_notebooks.py
@@ -1,3 +1,6 @@
+# python3 _scripts/scrape_notebooks.py  -c google-code-golf-2025 -o notebooks --also-submission
+# python gather_notebooks.py
+import glob
 from hashlib import sha256
 import json
 from multiprocessing import Process
@@ -53,56 +56,56 @@ if __name__ == "__main__":
   session_digests = set()
   tmp_dir = "tmp"
   os.makedirs(tmp_dir, exist_ok=True)
-  for root, _, files in tqdm(os.walk("notebooks")):
+
+  zip_paths = []
+  for root, _, files in os.walk("notebooks"):
     for file in files:
       if file != "submission.zip":
         continue
       zip_path = os.path.join(root, file)
-      with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        for name in zip_ref.namelist():
-          task_match = re.match(r"task(\w{3})\.py$", name)
-          if not task_match:
-            print(f"Invalid file name in {zip_path}: {name}")
-            continue
-          task_number = int(task_match.group(1))
-          extracted_path = os.path.join(tmp_dir, "tmpppp.py")
-          with open(extracted_path, "wb") as f:
-            f.write(zip_ref.read(name))
-          with open(extracted_path, "r") as f:
-            code = f.read()
-          if "import os" in code or "import sys" in code:
-            print(f"Cheating in {zip_path}: {name}")
-            continue
-          if normalize(code) in known_garbage:
-            continue
-          digest = f"{task_number:03}|{sha256(code.encode()).hexdigest()}"
-          if digest in session_digests:
-            continue
-          session_digests.add(digest)
-          if digest not in checked_hash:
-            verdict = False
-            try:
-              result = subprocess.run(
-                [sys.executable, "-c", f"from gather_notebooks import safe_check; print(safe_check({extracted_path!r},{task_number}))"],
-                capture_output=True, text=True, check=True
-              )
-              verdict = result.stdout.strip() == "True"
-              # manager = Manager()
-              # return_dict = manager.dict()
-              # def safe_check_wrapper(path, task_number, return_dict):
-              #   return_dict["result"] = safe_check(path, task_number)
-              # process = Process(target=safe_check_wrapper, args=(extracted_path, task_number, return_dict))
-              # process.start()
-              # process.join()
-              # verdict = return_dict.get("result", False)
-              if verdict:
-                print(f"Valid solution in {zip_path}: {name}")
-              else:
-                print(result.stdout)
-                print(f"Incorrect solution in {zip_path}: {name}")
-            except subprocess.CalledProcessError as e:
-              print(f"Error while checking {zip_path}: {name}\n{e.stderr}")
-            checked_hash[digest] = verdict
-            json.dump(checked_hash, open("notebooks_check_cache.json", "w"))
-          if checked_hash[digest]:
-            open(f"base_notebooks/task{task_number:03}_{root.split('/')[-1]}.py", "wb").write(zip_ref.read(name))
+      zip_paths.append(zip_path)
+  zip_paths.sort()
+
+  for zip_path in tqdm(zip_paths):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+      for name in zip_ref.namelist():
+        task_match = re.match(r"task(\w{3})\.py$", name)
+        if not task_match:
+          print(f"Invalid file name in {zip_path}: {name}")
+          continue
+        task_number = int(task_match.group(1))
+        path_name = f"base_notebooks/task{task_number:03}_{root.split('/')[-1]}.py"        
+        extracted_path = os.path.join(tmp_dir, "tmpppp.py")
+        with open(extracted_path, "wb") as f:
+          f.write(zip_ref.read(name))
+        with open(extracted_path, "r") as f:
+          code = f.read()
+        if "import os" in code or "import sys" in code:
+          print(f"Cheating in {zip_path}: {name}")
+          continue
+        if normalize(code) in known_garbage:
+          continue
+        digest = f"{task_number:03}|{sha256(code.encode()).hexdigest()}"
+        if digest in session_digests:
+          continue
+        session_digests.add(digest)
+        if digest not in checked_hash:
+          verdict = False
+          try:
+            result = subprocess.run(
+              [sys.executable, "-c", f"from gather_notebooks import safe_check; print(safe_check({extracted_path!r},{task_number}))"],
+              capture_output=True, text=True, check=True
+            )
+            verdict = result.stdout.strip() == "True"
+            if verdict:
+              print(f"Valid solution in {zip_path}: {name}")
+            else:
+              print(result.stdout)
+              print(f"Incorrect solution in {zip_path}: {name}")
+          except subprocess.CalledProcessError as e:
+            print(f"Error while checking {zip_path}: {name}\n{e.stderr}")
+          checked_hash[digest] = verdict
+          json.dump(checked_hash, open("notebooks_check_cache.json", "w"))
+        if checked_hash[digest]:
+          if not glob.glob(f"{path_name}~*"):
+            open(path_name, "wb").write(zip_ref.read(name))
