b7403a331810087dda387260349e29c5902f24e7
Yu212 <yu212.mc@gmail.com>
2025-08-13 23:35:39 +0900

    update 234

--- base_yu/task234.py
+++ base_yu/task234.py
@@ -2,7 +2,7 @@ def p(g):
  for _ in 0,1:
   b=any(t>s and{*t}=={*s}>{0}for s,t in zip(g,g[1:]))
   u=[s for s in g if s.count(max(s))!=1]
-  *g,=map(list,zip(*(u*b+[u[0]]*(len(g)-len(u))+u*(1-b))))
+  *g,=map(list,zip(*(u*b+u[:1]*(len(g)-len(u))+u*(1-b))))
  return g
 
 
