607403795b68d4186c43b2c84c3405a7d8898946
Yu212 <yu212.mc@gmail.com>
2025-08-13 21:01:08 +0900

    234

---
+++ base_yu/task234.py
@@ -0,0 +1,17 @@
+def p(g):
+ for _ in 0,1:
+  h=len(g)
+  b=any(t>s and{*t}=={*s}>{0}for s,t in zip(g,g[1:]))
+  g=[s for s in g if s.count(max(s))!=1]
+  *g,=map(list,zip(*(g*b+[g[0]]*(h-len(g))+g*(1-b))))
+ return g
+
+
+# def p(g):
+#  for _ in 0,1:
+#   h=len(g)
+#   b=any(g[i+1]>g[i]and{*g[i+1]}=={*g[i]}>{0}for i in range(h-1))
+#   *g,=filter(lambda s:s.count(max(s))!=1,g)
+#   g=g*b+[g[0]]*(h-len(g))+g*(1-b)
+#   *g,=map(list,zip(*g))
+#  return g
