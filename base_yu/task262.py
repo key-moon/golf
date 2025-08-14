81aac0f4bfc04d487c35886871527f4d5589ab7f
Yu212 <yu212.mc@gmail.com>
2025-08-14 15:59:26 +0900

    update 262

--- base_yu/task262.py
+++ base_yu/task262.py
@@ -1,7 +1,8 @@
 # p=lambda g:[[b"\2\4\3"[s.index(5)]]*3for s in g]
 # p=lambda g:[[(3-s.index(5))%3+2]*3for s in g]
 # p=lambda g:[[hash((*s,))%5+2]*3for s in g]
-p=lambda g:[[3+(s[1]-s[0])//5]*3for s in g]
+p=lambda g:[[3+s[1]%2-s[0]%2]*3for s in g]
+# p=lambda g:[[hash((*s,1))%1+1]*3for s in g]
 
 # 012
 # 243
