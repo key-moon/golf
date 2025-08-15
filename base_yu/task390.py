00f40671519d3a073cd0ba85d615f0775ce17503
Yu212 <yu212.mc@gmail.com>
2025-08-15 16:17:36 +0900

    update

--- base_yu/task390.py
+++ base_yu/task390.py
@@ -2,8 +2,8 @@ def p(g):
  for _ in 0,1:
   for i in range(15):
    if g[i].count(2)>4:g[i+2],g[i-2],g[i+3],g[i-3]=g[i-2],g[i+2],g[i-3],g[i+3]
-  *g,=zip(*g)
- return[*map(list,g)]
+  *g,=map(list,zip(*g))
+ return g
 
 
 # def p(g):
