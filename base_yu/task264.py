23373901ccb5c898c8f3f0fa51bc641de96dec28
Yu212 <yu212.mc@gmail.com>
2025-08-18 01:13:56 +0900

    264

--- base_yu/task264.py
+++ base_yu/task264.py
@@ -4,13 +4,28 @@ def p(g):
  for i in range(len(g)-2):
   for j in range(len(g[i])-2):
 #    if all(all(g[i+k][j:j+3])for k in range(3)):
-    # x=1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5)
-    # y=1+(g[i+2][j+1]!=5)-(g[i][j+1]!=5)
+  #  x=1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5)
+  #  y=1+(g[i+2][j+1]!=5)-(g[i][j+1]!=5)
    for u in range(3*(g[i][j]>0)):
-    #  o[y*3+u][x*3:x*3+3]=g[i+u][j:j+3]
     o[(1+(g[i+2][j+1]!=5)-(g[i][j+1]!=5))*3+u][(1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5))*3:(1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5))*3+3]=g[i+u][j:j+3]
+    # o[(1+(g[i+2][j+1]!=5)-(g[i][j+1]!=5))*3+u][(1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5))*3:(1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5))*3+3]=g[i+u][j:j+3]
+   for u in range(3*(g[i][j]>0)):
     g[i+u][j:j+3]=[0]*3
  return o
+
+# def p(g):
+# #  *o,=eval("[5]*9,"*9)
+#  o=[[5]*9 for _ in range(9)]
+#  for i in range(len(g)-2):
+#   for j in range(len(g[i])-2):
+# #    if all(all(g[i+k][j:j+3])for k in range(3)):
+#    x=1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5)
+#    y=1+(g[i+2][j+1]!=5)-(g[i][j+1]!=5)
+#    for u in range(3*(g[i][j]>0)):
+#     o[y*3+u][x*3:x*3+3]=g[i+u][j:j+3]
+#     # o[(1+(g[i+2][j+1]!=5)-(g[i][j+1]!=5))*3+u][(1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5))*3:(1+(g[i+1][j+2]!=5)-(g[i+1][j]!=5))*3+3]=g[i+u][j:j+3]
+#     g[i+u][j:j+3]=[0]*3
+#  return o
 
 # def p(g):
 #  h,w=len(g),len(g[0])
