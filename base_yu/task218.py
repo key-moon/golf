5467815a0f938b791ae481ecc377f770606e5db0
Yu212 <yu212.mc@gmail.com>
2025-08-18 17:07:19 +0900

    218

---
+++ base_yu/task218.py
@@ -0,0 +1,9 @@
+# 34567890123456789012345678901234567890123456789012345678901234
+# ==============================================================56789012345678
+
+# p=lambda g:(u:=[*zip(*g)])and[[g[i][j]for j in range(21)if u[j]not in u[:j]+[tuple([0]*21)]]for i in range(21)if g[i] not in g[:i]+[[0]*21]]
+
+def p(g):
+ for _ in 0,1:
+  *g,=map(list,zip(*[s for s,t in zip(g,g[1:]+[0])if(t!=s)*sum(s)]))
+ return g
