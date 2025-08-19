79ad1641accea0a171cc7c330ad9323ba7ea5799
Yu212 <yu212.mc@gmail.com>
2025-08-20 01:07:03 +0900

    update 1

--- base_yu/task001.py
+++ base_yu/task001.py
@@ -1,2 +1,3 @@
-p=lambda g,X=range(9):[[g[r//3][c//3]&g[r%3][c%3]for c in X]for r in X]
+p=lambda g:[[x&y for x in s for y in t]for s in g for t in g]
+# p=lambda g,X=range(9):[[g[r//3][c//3]&g[r%3][c%3]for c in X]for r in X]
 # p=lambda g:[sum(([[0]*3,g[i%3]][g[i//3][j]>0]for j in(0,1,2)),[])for i in range(9)]
