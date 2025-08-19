bdde8ece5241031fefe8940e1e89729e3fc03d3b
key-moon <kymn0116@gmail.com>
2025-08-20 00:58:03 +0900

    fix 021

--- base_yu/task021.py
+++ base_yu/task021.py
@@ -1,4 +1,4 @@
-3# ==================== best 64 by luke, att ====================
+# ==================== best 64 by luke, att ====================
 # p=lambda g:[g[0][:1]*(len(g[0])-g[0].count(g[0][0])+1)]*(len(g)-g.count(g[0])+1)
 p=lambda g:[(len(s:=g[0])-s.count(s[0])+1)*s[:1]]*(len(g)-g.count(s)+1)
 # p=lambda g:[(s:=g[0])[:1]*-~s.count(sum({*s})-s[0])]*(len(g)-g.count(s)+1)
