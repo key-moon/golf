0a037e53b15282a461ca1f124b74a7da9fc5fe6c
Yu212 <yu212.mc@gmail.com>
2025-08-19 19:09:43 +0900

    343

--- base_yu/task040.py
+++ base_yu/task040.py
@@ -2,4 +2,5 @@
 # p=lambda g:[[g[i][j]and g[i>4 and 9][j>4 and 9]for j in range(10)]for i in range(10)]
 # p=lambda g,E=enumerate:[[v and g[(i>4)*9][(j>4)*9]for j,v in E(s)]for i,s in E(g)]
 # p=lambda g,R=range(10):[[g[i][j]and g[i>4and 9][j>4and 9]for j in R]for i in R]
-p=lambda g,R=range(10):[[g[i][j]and g[(i>4)*9][(j>4)*9]for j in R]for i in R]
+# p=lambda g,R=range(10):[[g[i][j]and g[(i>4)*9][(j>4)*9]for j in R]for i in R]
+p=lambda g,R=range(10):[[g[i][j]and g[-(i>4)][-(j>4)]for j in R]for i in R]
