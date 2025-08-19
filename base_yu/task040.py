cda0b6304b5633f380b03bdf4b7de43368f90681
Yu212 <yu212.mc@gmail.com>
2025-08-19 16:28:39 +0900

    40

---
+++ base_yu/task040.py
@@ -0,0 +1,5 @@
+# =========================== best 73 by 4atj ===========================
+# p=lambda g:[[g[i][j]and g[i>4 and 9][j>4 and 9]for j in range(10)]for i in range(10)]
+# p=lambda g,E=enumerate:[[v and g[(i>4)*9][(j>4)*9]for j,v in E(s)]for i,s in E(g)]
+# p=lambda g,R=range(10):[[g[i][j]and g[i>4and 9][j>4and 9]for j in R]for i in R]
+p=lambda g,R=range(10):[[g[i][j]and g[(i>4)*9][(j>4)*9]for j in R]for i in R]
