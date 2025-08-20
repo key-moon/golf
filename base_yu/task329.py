69d3692b1c58868bc7631ea4abe6b389971018cf
Yu212 <yu212.mc@gmail.com>
2025-08-21 00:53:55 +0900

    update 329

---
+++ base_yu/task329.py
@@ -0,0 +1,4 @@
+# best: 54(ovs, luke, mukundan, 4atj, Seek64, nauti, MasukenSamba, att, xsot, kabutack) / others: 56(duckyluuk), 64(dbdr), 64(natte), 64(kg583), 66(joking)
+# ======================== 54 ========================
+p=lambda g:[[v*(i==len(s)//2)for i,v in enumerate(s)]for s in g]
+# p=lambda g:[(i:=len(s))and[v*((i:=i-2)==-1)for v in s]for s in g]
