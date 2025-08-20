610d491a7ed6dd3477458e166742c6b02753e210
key-moon <kymn0116@gmail.com>
2025-08-21 01:43:36 +0900

    082

---
+++ base_keymoon/task082.py
@@ -0,0 +1,3 @@
+# best: 50(mukundan, luke, joking, 4atj, kabutack, ovs, natte) / others: 55(Seek64), 55(att), 55(sisyphus), 58(kg583), 58(duckyluuk)
+# ====================== 50 ======================
+p=lambda g:[r:=g[0],[*map(max,zip(r[1:]+[0],[0]+r))]]*3
