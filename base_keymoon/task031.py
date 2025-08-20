7efc6240bf8a03e37c68fb6bc6b666a7b717d533
key-moon <kymn0116@gmail.com>
2025-08-21 05:09:07 +0900

    031

--- base_keymoon/task031.py
+++ base_keymoon/task031.py
@@ -1,7 +1,8 @@
-# best: 54
+# best: 52(biz, luke, 4atj, sisyphus) / others: 54(att), 56(mukundan), 58(Seek64), 58(joking), 58(natte)
 # 切り出し
 # filter max で 0 の行を消してる 転地して列も同様に 最後にもっかい転置
-# なんだけど、あと6 byte縮むらしい。一体どこで
+# なんだけど、あと6 byte縮むらしい。一体どこで→bestはさらに2 byte縮んだらしい 意味不明
+# p=lambda g,a=2:a and [*map(list,filter(max,zip(*p(g,a-1))))]or g
 # 62
 # E=lambda g:zip(*filter(max,g))
 # p=lambda g:[*map(list,E(E(g)))]
@@ -11,4 +12,12 @@
 # 60
 # p=lambda g:[*filter(max,map(list,zip(*filter(max,zip(*g)))))]
 # 60
+# p=lambda g:[[*r]for r in zip(*filter(max,zip(*g)))if max(r)]
+
+# p=lambda g:[[c for c,a in zip(r,zip(*g))if max(a)]for r in g if max(r)]
+
+# p=lambda g:max(min(g))and[*map(list,g)]or p([*zip(*filter(max,g))])
+# p=lambda g,a=2:a and[*map(list,filter(max,zip(*p(g,a-1))))]or g
+# ======================= 52 =======================
 p=lambda g:[[*r]for r in zip(*filter(max,zip(*g)))if max(r)]
+
