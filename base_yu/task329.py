0e130662f4d0f9ca6c595b95962c0fa5577cb888
key-moon <kymn0116@gmail.com>
2025-08-21 01:14:41 +0900

    329

--- base_yu/task329.py
+++ base_yu/task329.py
@@ -1,4 +1,5 @@
 # best: 54(ovs, luke, mukundan, 4atj, Seek64, nauti, MasukenSamba, att, xsot, kabutack) / others: 56(duckyluuk), 64(dbdr), 64(natte), 64(kg583), 66(joking)
+# p=lambda g:[[v*(i==len(s)//2)for i,v in enumerate(s)]for s in g]
+# lambda g:[(a:=[0]*(b:=len(s)//2))+[s[b],*a]for s in g]
 # ======================== 54 ========================
-p=lambda g:[[v*(i==len(s)//2)for i,v in enumerate(s)]for s in g]
-# p=lambda g:[(i:=len(s))and[v*((i:=i-2)==-1)for v in s]for s in g]
+p=lambda g:[[0]*(b:=len(s)//2)+[s[b]]+b*[0]for s in g]
