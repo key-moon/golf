0fd280621127d332592902f1ab3595a15af76bff
Yu212 <yu212.mc@gmail.com>
2025-08-21 00:12:40 +0900

    update 223,307

--- base_yu/task307.py
+++ base_yu/task307.py
@@ -1,6 +1,9 @@
 # best: 51(sisyphus) / others: 52(luke), 52(4atj), 52(att), 53(xsot), 54(mukundan)
 # ======================= 51 ======================
 p=lambda g:sum([[sum([[v]*2for v in s],[])]*2for s in g],[])
+# p=lambda g:sum([[(s*2+(s*2)[1:]*~-len(g))[::len(g)]]*2for s in g],[])
+# p=lambda g:sum([[(s[:1]+(s*2)[1:]*len(g))[::len(g)]]*2for s in g],[])
+
 # p=lambda g:[sum([[v,v]for v in g[i//2]],[])for i in range(len(g)*2)]
 
 # def p(g):
