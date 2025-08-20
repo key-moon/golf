7cc0627effd3e579218a3eddbff9e4821f969f3b
Yu212 <yu212.mc@gmail.com>
2025-08-20 21:53:22 +0900

    223,307

---
+++ base_yu/task223.py
@@ -0,0 +1,4 @@
+# best: 52(att, xsot, sisyphus) / others: 53(luke), 55(4atj), 56(mukundan), 58(Seek64), 59(Bulmenisaurus)
+# ======================= 52 =======================
+p=lambda g:sum([[sum([[v]*3for v in s],[])]*3for s in g],[])
+# p=lambda g:[sum([[v]*3for v in g[i//3]],[])for i in range(9)]
---
+++ base_yu/task307.py
@@ -0,0 +1,10 @@
+# best: 51(sisyphus) / others: 52(luke), 52(4atj), 52(att), 53(xsot), 54(mukundan)
+# ======================= 51 ======================
+p=lambda g:sum([[sum([[v]*2for v in s],[])]*2for s in g],[])
+# p=lambda g:[sum([[v,v]for v in g[i//2]],[])for i in range(len(g)*2)]
+
+# def p(g):
+#  u=[s+s for s in g+g]
+#  for i in range(len(g)*2):
+#   u[i][::2]=u[i][1::2]=g[i//2]
+#  return u
