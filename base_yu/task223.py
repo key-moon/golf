141977c72a6b769747c79ced4164e237cff8f81c
Yu212 <yu212.mc@gmail.com>
2025-08-21 00:29:59 +0900

    update 223,307

--- base_yu/task223.py
+++ base_yu/task223.py
@@ -3,7 +3,8 @@
 # p=lambda g:sum([[sum([[v]*3for v in s],[])]*3for s in g],[])
 # p=lambda g:sum([[(s[:1]+(s*3)[1:]*3)[::3]]*3for s in g],[])
 
-p=lambda s,a=1:sum([[a and p(v,0)or v]*3for v in s],[])
+# p=lambda s,a=1:sum([[a and p(v,0)or v]*3for v in s],[])
+p=lambda s:sum([[v*0!=0and p(v)or v]*3for v in s],[])
 
 
 # p=lambda g:[(s[:1]+(s*3)[1:]*3)[::3]for s in(g[:1]+(g*3)[1:]*3)[::3]]
