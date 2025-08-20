eff03f9cfc7affc04f16f3194ff88170272f87bc
Yu212 <yu212.mc@gmail.com>
2025-08-20 12:04:37 +0900

    127

---
+++ base_yu/task127.py
@@ -0,0 +1,15 @@
+# best: 76(4atj) / others: 80(luke), 84(natte), 84(kabutack), 85(att), 85(duckyluuk)
+# =================================== 76 ===================================
+# p=lambda g:sum([[[5]*11]+[sum([[5]+[v+5]*3for v in s[1::4]],[])[1:]]*3for s in g[1::4]],[])[1:]
+# p=lambda g:sum([[sum([[5]+[v+5]*3for v in s[1::4]],[])[1:]]*3+g[3:4]for s in g[1::4]],[])[:7]
+# p=lambda g:sum([[[5+s[i&12|(i%4<3)]for i in range(11)]]*3+g[3:4]for s in g[1::4]],[])[:7]
+p=lambda g:[[5+g[i&4|(i!=3)][j&12|(j%4<3)]for j in range(11)]for i in range(len(g))]
+
+
+# p=lambda g:(s:=g[1])and[[5+s[i&12|(i%4<3)]for i in range(11)]]
+
+# sum([[5]+[v+5]*3for v in s[1::4]],[])[1:]
+# [5+s[i&12|(i%4<3)]for i in range(11)]
+# p=lambda g:sum([[[5]*11]+[[*b"\5".join(bytes([v+5])*3for v in s[1::4])]]*3for s in g[1::4]],[])[1:]
+
+# 1234 -> 6789
