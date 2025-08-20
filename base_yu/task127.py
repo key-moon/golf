d9cc4d63fb18a7a8619dabaf9a276f121de85803
Yu212 <yu212.mc@gmail.com>
2025-08-20 12:12:32 +0900

    update 127

--- base_yu/task127.py
+++ base_yu/task127.py
@@ -3,7 +3,8 @@
 # p=lambda g:sum([[[5]*11]+[sum([[5]+[v+5]*3for v in s[1::4]],[])[1:]]*3for s in g[1::4]],[])[1:]
 # p=lambda g:sum([[sum([[5]+[v+5]*3for v in s[1::4]],[])[1:]]*3+g[3:4]for s in g[1::4]],[])[:7]
 # p=lambda g:sum([[[5+s[i&12|(i%4<3)]for i in range(11)]]*3+g[3:4]for s in g[1::4]],[])[:7]
-p=lambda g:[[5+g[i&4|(i!=3)][j&12|(j%4<3)]for j in range(11)]for i in range(len(g))]
+# p=lambda g:[[5+g[i&4|(i!=3)][j&12|(j%4<3)]for j in range(11)]for i in range(len(g))]
+p=lambda g:[[5+s[j&12|(j%4<3)]for j in range(11)]for s in[g[1]]*3+g[4:5]+g[5:6]*3]
 
 
 # p=lambda g:(s:=g[1])and[[5+s[i&12|(i%4<3)]for i in range(11)]]
