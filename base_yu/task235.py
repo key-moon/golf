7e48572db9f073264a84d8d43cf9a9be1633b3e7
Yu212 <yu212.mc@gmail.com>
2025-08-21 19:15:24 +0900

    218,235

--- base_yu/task235.py
+++ base_yu/task235.py
@@ -1,6 +1,7 @@
 # best: 62(luke) / others: 64(mukundan), 66(ovs), 67(natte), 69(xsot), 72(nauti)
 # ============================ 62 ============================
-p=lambda g:[[9-sum(g[2][i:i+3])/5-g[1][i]*.8]*3for i in(1,6,11)]
+# p=lambda g:[[9-sum(g[2][i:i+3])/5-g[1][i]*.8]*3for i in(1,6,11)]
+p=lambda g:[[9-sum(g[2][i:i+3])/5-g[1][i]*.8]*3for i in b""]
 
 # 2
 # ####
