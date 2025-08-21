8a1cef23b3ee58d263e9f63ef03566201c8eb78d
Yu212 <yu212.mc@gmail.com>
2025-08-21 14:42:52 +0900

    232,258,388

---
+++ base_yu/task258.py
@@ -0,0 +1,3 @@
+# best: 61(luke, Seek64) / others: 62(mukundan), 62(kg583), 64(sisyphus), 67(ovs), 67(natte)
+# ============================ 61 ===========================
+p=lambda g:[[y+x*z*2for x,y,z in zip([0]+s,s,s[1:]+[0])]for s in g]
---
+++ base_yu/task388.py
@@ -0,0 +1,3 @@
+# best: 61(luke) / others: 62(ovs), 62(mukundan), 62(4atj), 62(att), 62(biz)
+# ============================ 61 ===========================
+p=lambda g:[[v or any(t)*8for v,t in zip(s,zip(*g))]*2for s in g]*2
