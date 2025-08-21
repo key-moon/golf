e9f8b1bacec620b57c281c1abce0334c6e435a60
key-moon <kymn0116@gmail.com>
2025-08-22 01:01:36 +0900

    227

--- base_keymoon/task227.py
+++ base_keymoon/task227.py
@@ -1,5 +1,5 @@
-# 類題: 347 006 5 byte多い
 # best: 55(4atj) / others: 57(mukundan), 57(luke), 58(att), 60(Seek64), 60(ovs)
 # lambda g:[*eval("[2*(x+y<1)for x,y in zip(g.pop(0),g[3])],"*4)]
+# lambda g:[*eval("[2-2*any(a)for a in zip(g.pop(0),g[3])],"*4)]
 # ========================= 55 ========================
-p=lambda g:[*eval("[2-2*any(a)for a in zip(g.pop(0),g[3])],"*4)]
+p=lambda g:[[2-any(a)*2for a in zip(*c)]for c in zip(g,g[4:])]
