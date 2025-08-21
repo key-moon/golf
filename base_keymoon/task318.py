bf9939d97fb535eeadce85787bf1a02f1d38119a
key-moon <kymn0116@gmail.com>
2025-08-22 00:27:37 +0900

    318

---
+++ base_keymoon/task318.py
@@ -0,0 +1,3 @@
+# best: 60(biz) / others: 61(kg583), 61(luke), 61(att), 61(sisyphus), 62(mukundan)
+# =========================== 60 ===========================
+p=lambda g:[[any(a)*3for a in zip(*c)]for c in zip(g,g[5:])]
