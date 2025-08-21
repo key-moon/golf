83664704115b27024e80db213425a30ec2c13b65
key-moon <kymn0116@gmail.com>
2025-08-22 00:35:57 +0900

    add 236 failed attempt

--- base_yu/task236.py
+++ base_yu/task236.py
@@ -1,4 +1,5 @@
 # best: 60(4atj) / others: 61(luke), 62(mukundan), 62(Seek64), 62(sisyphus), 63(kg583)
+# lambda g:[[sum(a)*1.5%2//1for a in zip(*c)]for c in zip(g,g[5:])]
 # =========================== 60 ===========================
 p=lambda g:[[3*(x^y//2)for x,y in zip(*c)]for c in zip(g,g[5:])]
 # p=lambda g:[*eval("[3*(x^y//2)for x,y in zip(g.pop(0),g[4])],"*4)]
