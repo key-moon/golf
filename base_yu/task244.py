3e3b5cd42c6e262c490c4d2d8870263f3cf465b6
Yu212 <yu212.mc@gmail.com>
2025-08-22 07:32:42 +0900

    update 244

--- base_yu/task244.py
+++ base_yu/task244.py
@@ -2,7 +2,11 @@
 # p=lambda g:(k:=                      )and[s[::-k]for s in g[::k]]
 # p=lambda g:(k:=1+g[0].index([s[0]for s in g if {*s}=={s[0]}][0]))and[s[::-k]for s in g[::k]]
 # p=lambda g:(k:=[i+1for i,s in enumerate(g)if{*s}=={s[0]}][0])and[s[::-k]for s in g[::k]]
-p=lambda g:(k:=[i+1for i in range(8)if g[0]!=g[i]][0])and[s[::-k]for s in g[::k]]
+# p=lambda g:(k:=[i+1for i in range(8)if g[0]!=g[i]][0])and[s[::-k]for s in g[::k]]
+# p=lambda g:[[s[::-k]for s in g[::k]]for k in range(1,8)if g[0]!=g[k-1]][0]
+# p=lambda g:[[s[::-k]for s in g[::k]]for k in(3,4,5,6)if g[0]!=g[k-1]][0]
+p=lambda g:[[s[::-k]for s in g[::k]]for k in b""if g[0]!=g[k-1]][0]
+
 # p=lambda g:[[y for x,y in zip([10]+t,t)if x!=y][::-2]for s,t in zip([0]+g,g)if s!=t][::2]
 
 
@@ -16,3 +20,5 @@ p=lambda g:(k:=[i+1for i in range(8)if g[0]!=g[i]][0])and[s[::-k]for s in g[::k]
 # 19 4 4
 # 17 5 3
 # 23 5 4
+
+
