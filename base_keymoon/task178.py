e7d6108c5d0301a0a18ec2cf1731b0f6e15f21f8
key-moon <kymn0116@gmail.com>
2025-08-21 15:42:38 +0900

    178

---
+++ base_keymoon/task178.py
@@ -0,0 +1,7 @@
+# best: 55(luke, 4atj, sisyphus) / others: 59(dbdr), 64(joking), 65(ovs), 66(mukundan), 71(natte)
+# 転地佐伯テクだけでこんだけ食うから転地佐伯ではない
+# lambda g:5>len(g)and[*map(list,zip(*g))]or []
+# lambda g,s=0:[(s:=r)*0!=r*0and p(r)or r for r in g if r!=s]
+# lambda g,s=0:[(s:=r)*0!=0and p(r)or r for r in g if r!=s]
+# ========================= 55 ========================
+p=lambda g:[[int,p][(g:=r)*0!=0](r)for r in g if r!=g]
