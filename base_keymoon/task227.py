3fbd8c3f47d8bb2dd5494bb85bed0d36a58a3217
key-moon <kymn0116@gmail.com>
2025-08-22 01:33:21 +0900

    227(failed)

--- base_keymoon/task227.py
+++ base_keymoon/task227.py
@@ -1,5 +1,6 @@
 # best: 55(4atj) / others: 57(mukundan), 57(luke), 58(att), 60(Seek64), 60(ovs)
 # lambda g:[*eval("[2*(x+y<1)for x,y in zip(g.pop(0),g[3])],"*4)]
 # lambda g:[*eval("[2-2*any(a)for a in zip(g.pop(0),g[3])],"*4)]
+# lambda g:[[2*~a&2for a in map(max,*c)]for c in zip(g,g[4:])]
 # ========================= 55 ========================
 p=lambda g:[[2-any(a)*2for a in zip(*c)]for c in zip(g,g[4:])]
