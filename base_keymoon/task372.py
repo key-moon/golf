298afe47136874f979fab176c6956f162b5ed77d
key-moon <kymn0116@gmail.com>
2025-08-22 00:54:02 +0900

    372

---
+++ base_keymoon/task372.py
@@ -0,0 +1,3 @@
+# best: 48(kg583, mukundan, luke, 4atj, ovs, natte, sisyphus) / others: 50(nauti), 51(att), 52(kabutack), 53(Seek64), 53(duckyluuk)
+# ===================== 48 =====================
+p=lambda g:[[*map(sum,zip(*c))]for c in zip(g,g[6:])]
