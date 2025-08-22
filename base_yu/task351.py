6c2fa4d08b2f62d46b735bfa5c0dcc5f1dd29ed1
Yu212 <yu212.mc@gmail.com>
2025-08-22 11:44:23 +0900

    update 351

--- base_yu/task351.py
+++ base_yu/task351.py
@@ -1,3 +1,7 @@
 # p=lambda g,R=range(16):[*filter(len,[[g[15-i][15-j]for j in R if g[i][j]==3]for i in R])]
-p=lambda g,R=range(16):[u for i in R if(u:=[g[15-i][15-j]for j in R if g[i][j]==3])]
+p=lambda g,R=range(16):[u for i in R if(u:=[g[~i][~j]for j in R if g[i][j]==3])]
+# p=lambda g,R=range(16):[[g[~i][~j]for j in R if g[i][j]==3]for i in R if 3 in g[i]]
+# p=lambda g:[u for s,t in zip(g,g[::-1]) if(u:=[y for x,y in zip(s,t[::-1]) if x==3])]
+# p=lambda g:[[y for x,y in zip(s,t[::-1]) if x==3]for s,t in zip(g,g[::-1])if 3 in s]
+
 # p=lambda g,R=range(16):[u for x,y in zip(g,g[::-1]) if(u:=[w for v,w in zip(x,y[::-1]) if v==3])]
