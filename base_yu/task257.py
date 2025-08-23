672a3b91849ec2e04418b9481a02902600f51a20
Yu212 <yu212.mc@gmail.com>
2025-08-23 23:35:39 +0900

    257

---
+++ base_yu/task257.py
@@ -0,0 +1,7 @@
+# best: 81(luke) / others: 82(mukundan), 82(sisyphus), 83(kabutack), 84(natte), 84(att)
+# ====================================== 81 =====================================
+# p=lambda g:[[g[i][j]or g[i][j+5]or g[i+5][j]or g[i+5][j+5] for j in range(4)]for i in range(4)]
+# p=lambda g,R=range(4):[[g[i][j]or g[i][j+5]or g[i+5][j]or g[i+5][j+5] for j in R]for i in R]
+# p=lambda g:[[s[j]or s[j+5]or t[j]or t[j+5] for j in range(4)]for s,t in zip(g,g[5:])]
+# p=lambda g:[[s[j]or s[j+5]or t[j]or t[j+5] for j in(0,1,2,3)]for s,t in zip(g,g[5:])]
+p=lambda g:[[s[j-5]or s[j]or t[j-5]or t[j]for j in b""]for s,t in zip(g,g[5:])]
