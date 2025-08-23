513c99872294b9efd77011e85172af4408d75ea8
Yu212 <yu212.mc@gmail.com>
2025-08-23 23:30:26 +0900

    078

---
+++ base_yu/task078.py
@@ -0,0 +1,3 @@
+# best: 61(joking+MWI) / others: 65(ovs), 65(luke), 65(4atj), 71(joking), 72(mukundan)
+# ============================ 61 ===========================
+p=lambda g:[*zip(*[sorted(r,key=lambda x:x-1&3)for r in zip(*g)])]
