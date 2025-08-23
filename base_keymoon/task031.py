0be08d588dbbda90e38d5ac7aa87fa3ffa3c14e6
key-moon <kymn0116@gmail.com>
2025-08-23 14:17:35 +0900

    031

--- base_keymoon/task031.py
+++ base_keymoon/task031.py
@@ -1,8 +1,10 @@
 # best: 52(biz, luke, 4atj, sisyphus) / others: 54(att), 56(mukundan), 58(Seek64), 58(joking), 58(natte)
+# best: 47(att) / others: 52(biz), 52(luke), 52(4atj), 52(sisyphus), 56(mukundan)
 # 類題: 57
 # 切り出し
 # filter max で 0 の行を消してる 転地して列も同様に 最後にもっかい転置
 # なんだけど、あと6 byte縮むらしい。一体どこで→bestはさらに2 byte縮んだらしい 意味不明
+# p=lambda g:[[c[0]for c in zip(r,*g)if max(c)]for r in g if max(r)]
 # p=lambda g,a=2:a and [*map(list,filter(max,zip(*p(g,a-1))))]or g
 # 62
 # E=lambda g:zip(*filter(max,g));p=lambda g:[[*r]for r in E(E(g))]
@@ -13,11 +15,13 @@
 # lambda g:[*filter(max,map(list,zip(*filter(max,zip(*g)))))]
 # 60
 # lambda g:[[*r]for r in zip(*filter(max,zip(*g)))if max(r)]
-# lambda g,F=filter:[*F(max,map(list,zip(*F(max,zip(*g)))))]
 
-# p=lambda g:[[c[0]for c in zip(r,*g)if max(c)]for r in g if max(r)]
+# lambda g:max(min(g))and g or p([*zip(*filter(max,g))])
+# lambda g:[p,list][type(g)!=list](filter(max,zip(*g)))
+# lambda g,a=2:a and[*filter(max,zip(*p(g,a-1)))]or g
+# lambda g:[p,list][g*0!=[]]((*filter(max,zip(*g)),))
+# lambda g,E=lambda g:zip(*filter(max,g)):[*E(E(g))]
+# lambda g,F=filter:[*F(max,zip(*F(max,zip(*g))))]
+# ===================== 47 ====================
+p=lambda g,F=filter:[*F(max,zip(*F(max,zip(*g))))]
 
-# p=lambda g:max(min(g))and[*map(list,g)]or p([*zip(*filter(max,g))])
-# p=lambda g,a=2:a and[*map(list,filter(max,zip(*p(g,a-1))))]or g
-# ======================= 52 =======================
-p=lambda g:[[*r]for r in zip(*filter(max,zip(*g)))if max(r)]
