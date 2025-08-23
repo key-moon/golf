c03fac849dbb3651d37d85cefb3c7de6aa9b201c
Yu212 <yu212.mc@gmail.com>
2025-08-23 19:35:23 +0900

    294,243

---
+++ base_yu/task243.py
@@ -0,0 +1,6 @@
+# best: 79(luke) / others: 81(4atj), 84(sisyphus), 86(att), 88(duckyluuk), 89(ovs)
+# ===================================== 79 ====================================
+# p=lambda g,c=80:c and p([*map(list,zip(*(g[:1]+[[y or(x==1)for x,y in zip(s,t)]for s,t in zip(g,g[1:])])[::-1]))],c-1)or g
+# p=lambda g,c=80:c and p([*map(list,zip(*[s[:1]+[y or(x==1)for x,y in zip(s,s[1:])]for s in g][::-1]))],c-1)or g
+# p=lambda g,c=80:c and p([[s[0]]+[y or(x==1)for x,y in zip(s,s[1:])]for s in zip(*g[::-1])],c-1)or g
+p=lambda g,c=80:c and p([[y or(x==1)for x,y in zip((0,)+s,s)]for s in zip(*g[::-1])],c-1)or g
