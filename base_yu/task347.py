1988d59173f070cc6c2ce25f529b1bd1956b6750
Yu212 <yu212.mc@gmail.com>
2025-08-25 00:11:26 +0900

    update logic

--- base_yu/task347.py
+++ base_yu/task347.py
@@ -2,6 +2,5 @@
 # ====================== 50 ======================
 # p=lambda g:[[s[i]|s[i+3]and 6for i in range(3)]for s in g]
 # p=lambda g:[[x|y and 6for x,y in zip(s,s[3:])]for s in g]
-p=lambda g:[[(x|y>0)*6for x,y in zip(s,s[3:])]for s in g]
-
-
+# p=lambda g:[[(x|y>0)*6for x,y in zip(s,s[3:])]for s in g]
+p=lambda g:[eval("6*(s.pop(0)+s[2]>0),"*3)for s in g]
