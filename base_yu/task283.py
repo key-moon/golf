e10a2ff6bfaf5a9f328f9cba1db3f105da6fcd4a
Yu212 <yu212.mc@gmail.com>
2025-08-24 21:34:41 +0900

    a

--- base_yu/task283.py
+++ base_yu/task283.py
@@ -4,3 +4,6 @@
 # p=lambda g,E=enumerate:[[t[i]and sum([*t[i-1:i+2]]+s[j-1:j+2])**4%7 for j,t in E(zip(*g))]for i,s in E(g)]
 # p=lambda g,E=enumerate:[[t[i]and sum([0,*t][i:i+3]+[0,*s][j:j+3])**4%7 for j,t in E(zip(*g))]for i,s in E(g)]
 p=lambda g,E=enumerate:[[t[i]*sum([0,*t][i:i+3]+[0,*s][j:j+3])**4*3%7for j,t in E(zip(*g))]for i,s in E(g)]
+
+# { 20:1, 25:4, 30:2 }
+# 5 * { 20:3, 25:5, 30:6 }
