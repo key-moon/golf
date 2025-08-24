17a71ddfb3c8315810f8f4f41fb04d1bfec7c0d4
key-moon <kymn0116@gmail.com>
2025-08-24 23:33:53 +0900

    217

--- base_yu/task217.py
+++ base_yu/task217.py
@@ -5,8 +5,7 @@
 #  p=E(E(g))
 #  return[[x&y for x in s for y in t]for s in p for t in p]
 
-E=lambda g:[*filter(max,zip(*g))]
+E=lambda g:filter(max,zip(*g))
 p=lambda g:[[x&y for x in s for y in t]for s in E(E(g))for t in E(E(g))]
-
 
 # p=lambda g:(u:=[[max(max(s[j::3])for s in g[i::3])for j in range(3)]for i in range(3)])and[[x&y for x in s for y in t]for s in u for t in u]
