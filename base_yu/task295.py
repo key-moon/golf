4489fa6f5f1c2e231793d8a38f63b978fe386d22
key-moon <kymn0116@gmail.com>
2025-08-24 23:05:37 +0900

    295 failed attempt

--- base_yu/task295.py
+++ base_yu/task295.py
@@ -1,10 +1,13 @@
 # best: 54(luke, att) / others: 56(Seek64), 58(mukundan), 62(sisyphus), 63(kg583), 63(duckyluuk)
 # ======================== 54 ========================
-# p=lambda g:(n:=len(s:=g[0]))and[s[:1]*i+s[:n-i]for i in range(n//2)]
-# p=lambda g:(s:=g[0])and g+[s[:1]*i+s[:-i]for i in range(1,len(s)//2)]
-# p=lambda g:g+[g[0][:1]*i+g[0][:-i]for i in range(1,len(g[0])//2)]
-# p=lambda g:[s:=g[0]]+[(s:=s[:1]+s[:-1])for i in range(1,len(s)//2)]
-# p=lambda g:[s:=g[0]]+[*eval("(s:=s[:1]+s[:-1]),"*(len(s)//2-1))]
+# lambda g:[(s:=g[0])[:]]+[s[:]for s[s.index(0)]in(len(s)//2-1)*s[:1]]
+# lambda g:(s:=g[0])and g+[s[:1]*i+s[:-i]for i in range(1,len(s)//2)]
+# lambda g:(s:=g[0]+[0])and[s[1:]for s[s.index(0)]in len(s)//2*s[:1]]
+# lambda g:(n:=len(s:=g[0]))and[s[:1]*i+s[:n-i]for i in range(n//2)]
+# lambda g:[s:=g[0]]+[(s:=s[:1]+s[:-1])for i in range(1,len(s)//2)]
+# lambda g:g+[g[0][:1]*i+g[0][:-i]for i in range(1,len(g[0])//2)]
+# lambda g:[s:=g[0]]+[*eval("(s:=s[:1]+s[:-1]),"*(len(s)//2-1))]
+# lambda g:[s:=g[0]]+[(s:=s[:1]+s[:-1])for _ in s[1:len(s)//2]]
 p=lambda g:[s:=g[0]]+[(s:=s[:1]+s[:-1])for _ in s[1:len(s)//2]]
 
 # def p(g):
