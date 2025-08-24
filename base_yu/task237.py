83eef50940d23853efc409d58507aa1a1502a156
Yu212 <yu212.mc@gmail.com>
2025-08-24 13:05:50 +0900

    update 237

--- base_yu/task237.py
+++ base_yu/task237.py
@@ -2,7 +2,8 @@
 # =============================== 67 ==============================
 # p=lambda g,c=0:[[(c:=max(s)or c)*any(s[:i+1])for i in range(len(s)-1)]+[c]for s in g]
 # p=lambda g,c=0:[(u:=[])+[(c:=max(s)or c)*any(u:=u+[v])for v in s[:-1]]+[c]for s in g]
-p=lambda g,c=0:[(d:=0)or[d:=d+v for v in s[:-1]]+[c:=d or c]for s in g]
+# p=lambda g,c=0:[(d:=0)or[d:=d+v for v in s[:-1]]+[c:=d or c]for s in g]
+p=lambda g,c=0:[(d:=0)or[d:=d+v for v in s]+[c:=d or c]for*s,_ in g]
 
 
 
