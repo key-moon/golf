4d53ecb8b1416df14f66813f5eb4b87104e56b56
key-moon <kymn0116@gmail.com>
2025-08-24 14:57:07 +0900

    003(failed)

--- base_keymoon/task003.py
+++ base_keymoon/task003.py
@@ -6,4 +6,9 @@
 #     return[[c*2for c in r]for r in g+g[:3]]
 #   else:
 #     return[[c*2for c in r]for r in g+g[2:5]]
-p=lambda g:[[val_cell*2for val_cell in val_row]for val_row in g+g[(g[:3]*2!=g)*2:][:3]]
+# lambda g:[[c*2for c in r]for r in g+g[(g[:3]*2!=g)*2:][:3]]
+p=lambda g:[[c*2for c in r]for r in g+g[(g[2]!=g[5])*2:][:3]]
+
+#  g+g[(g[2]!=g[5])*2:][:3]
+# [g[:3],g[2:5]][g[:3]*2!=g]
+# [a:=g[:3],g[2:5]][a*2!=g]
