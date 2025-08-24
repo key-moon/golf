734fbfc215dc586662731afe95724f932c4ffd2c
key-moon <kymn0116@gmail.com>
2025-08-24 17:02:27 +0900

    300

--- base_yu/task300.py
+++ base_yu/task300.py
@@ -3,4 +3,4 @@
 # ========================================= 87 ========================================
 # p=lambda g:(c:=max({*(u:=sum(g,[]))}-{0},key=u.count))and[[v for v,t in zip(s,zip(*g))if c in t]for s in g if c in s]
 # p=lambda g:[[v for v,t in zip(s,zip(*g))if c in t]for s in g if(c:=max({*(u:=sum(g,[]))}-{0},key=u.count))in s]
-p=lambda g,c=-1:c*g or p([[*s]for s in zip(*g)if max({*(u:=sum(g,[]))}-{0},key=u.count)in s],c+1)
+p=lambda g,c=-1:c*g or p([s for s in zip(*g)if max({*(u:=sum(g,[]))}-{0},key=u.count)in s],c+1)
