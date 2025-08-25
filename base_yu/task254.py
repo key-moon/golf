0840feba92a19a168aa481115c3bb07ab08d0763
Yu212 <yu212.mc@gmail.com>
2025-08-26 03:03:19 +0900

    254

--- base_yu/task254.py
+++ base_yu/task254.py
@@ -2,5 +2,6 @@
 # =============================================== 99 ==============================================
 # p=lambda g:[u:=sorted({*zip(*g)}-{(0,)*9},key=sum)]and[*zip(*[[v and(s==u[0])*2+(s==u[-1])for v in s]for s in zip(*g)])]
 def p(g):
- a,*_,b=sorted({*zip(*g)}-{(0,)*9},key=sum)
+ _,a,*_,b=sorted({*zip(*g)},key=sum)
  return[*zip(*[[v and(s==a)*2+(s==b)for v in s]for s in zip(*g)])]
+#  return[[x and(t==a)*2+(t==b)for x,t in zip(s,zip(*g))]for s in g]
