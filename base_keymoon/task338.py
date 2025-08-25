ab0302046192eeffe5da7f94fc3723714fc147ff
key-moon <kymn0116@gmail.com>
2025-08-26 03:56:20 +0900

    338

---
+++ base_keymoon/task338.py
@@ -0,0 +1,38 @@
+# best: 99(luke/sisyphus/Seek, 4atj, sisyphus) / others: 101(mukundan), 104(dbdr), 113(joking+MWI), 113(joking/MWI), 113(joking)
+# lambda g,R=[0]*99,S=[0]*99:eval(str([S:=[(c<1)*(D&3)+7*(c>0==C==D)for c,C,D in zip(r,R,S)if(R:=r)]for r in g]).replace(*"70"))
+# =============================================== 99 ==============================================
+
+# C c D
+# == 縁への侵入 == 侵入時点で色をつける 脱出前に色を消す
+# 0 2 0 -> ?
+# 0 2 3 -> 0
+# == 縁からの脱出 == 
+# 2 0 0 -> 0
+# 2 0 ? -> 3
+# == 通常 == 
+# 0 0 0 -> 0
+# 0 0 3 -> 3
+# == 縁 == 色を消さないと、縁からの脱出時点で判定が狂う
+# 2 2 0 -> 0
+# 2 2 ? -> 0
+
+# c==0 -> D&3
+# c==2 -> C==D==0 * 5
+# lambda g,R=[0]*99:(S:=R)and eval(str([S:=[(c<1<D)*3+7*(c>1>C+D)for c,C,D in zip(r,R,S)if(R:=r)]for r in g]).replace(*"70"))
+
+# -- or --
+
+# c D
+# == 縁への侵入 == 侵入時点で特殊色 脱出前に別の色
+# 2  0 ->  ?
+# 2  3 -> -?
+# 2  ? -> -?
+# 2 -? -> -?
+# == 縁からの脱出, 通常 ==
+# 0 0 -> 0
+# 0 ? -> (0<?)*3
+# lambda g,S=[0]*99:eval(str([S:=[0**c*D%4+c//~c*(3*(2<D)-7)for c,D in zip(r,S)]for r in g]).replace(*"70").replace(*"40"))
+# ?: 1
+# lambda g,S=[0]*99:eval(str([S:=[[3*(0<D),1-2*(0!=D)][1<c]for c,D in zip(r,S)]for r in g]).replace(*"10"))
+# ?: 0.5
+p=lambda g,S=[0]*99:eval(str([S:=[[3*(0<D),.5-(0!=D)][1<c]for c,D in zip(r,S)]for r in g]).replace(*"50"))
