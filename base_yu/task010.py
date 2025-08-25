ef6f0e4a50862a55422fd2250d6efa44b006f38c
Yu212 <yu212.mc@gmail.com>
2025-08-26 03:04:43 +0900

    comment

--- base_yu/task010.py
+++ base_yu/task010.py
@@ -1,3 +1,4 @@
+# 類題:254
 # best: 70(luke, luke/sisyphus/Seek) / others: 72(ovs), 72(4atj), 72(att), 73(mukundan), 78(Seek64)
 # ================================ 70 ================================
 # p=lambda g:[*zip(*[[x and 9-sorted(map(sum,zip(*g))).index(sum(s))for x in s]for s in zip(*g)])]
