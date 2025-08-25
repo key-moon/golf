91ea25d8832d2c2ce566e0b4c5928ecbc62b48ef
key-moon <kymn0116@gmail.com>
2025-08-25 15:02:36 +0900

    377

--- base_keymoon/task377.py
+++ base_keymoon/task377.py
@@ -2,5 +2,8 @@
 # lambda g:[p,list][g==(a:=[*zip(*[g:=r for r in g if g!=r])])](a)
 # lambda g:[list,p][g*0==[]]((*zip(*[g:=r for r in g if g!=r]),))
 # lambda g,c=-2:g*c or p([*zip(*[g:=r for r in g if g!=r])],c+1)
+# lambda g,f=lambda x:[x:=s for s in x if s!=x]:f(zip(*f(g)))
+# lambda g,c=2:c and[g:=s for s in p(zip(*g),c-1)if s!=g]or g
+# 類題: 218
 # ========================= 55 ========================
-p=lambda g,f=lambda x:[x:=s for s in x if s!=x]:f(zip(*f(g)))
+p=lambda g,c=-1:g*c or[g:=r for r in zip(*p(g,c+1))if g!=r]
