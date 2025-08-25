dea9b34c7f833b095b8a2056d05b8a9844168c3a
Yu212 <yu212.mc@gmail.com>
2025-08-26 03:25:19 +0900

    194

--- base_yu/task106.py
+++ base_yu/task106.py
@@ -4,5 +4,5 @@
 # p=lambda g:(t:=[g[i]+[s[i]for s in g[::-1]]for i in range(len(g))])and t+[s[::-1]for s in t[::-1]]
 
 # p=lambda g:(t:=[g[i]+[*v]for i,v in enumerate(zip(*g[::-1]))])and t+[s[::-1]for s in t[::-1]]
-p=lambda g:(t:=[*zip(*(g+[*zip(*g)][::-1]))])and[s[::-1]for s in t]+t[::-1]
+p=lambda g:(t:=[*zip(*g+[*zip(*g)][::-1])])and[s[::-1]for s in t]+t[::-1]
 # p=lambda g:((t:=[*zip(*(g+[*zip(*g)][::-1]))])+[s[::-1]for s in t[::-1]])[::-1]
---
+++ base_yu/task194.py
@@ -0,0 +1,3 @@
+# best: 73(luke, mukundan, 4atj, luke/sisyphus/Seek) / others: 74(joking+MWI), 74(joking/MWI), 74(joking), 75(natte), 75(sisyphus)
+# ================================== 73 =================================
+p=lambda g:(t:=[*zip(*g+[*zip(*g)][::-1])])and[s[::-1]for s in t]+t[::-1]
