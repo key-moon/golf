643ca84ac9067d836e4ad7dce34530a46413b857
Yu212 <yu212.mc@gmail.com>
2025-08-25 18:10:35 +0900

    dump

--- base_yu/task015.py
+++ base_yu/task015.py
@@ -1,3 +1,4 @@
 # best: 100(4atj) / others: 104(sisyphus), 107(joking+MWI), 107(joking), 117(duckyluuk), 117(biz)
 # ============================================== 100 ===============================================
 p=lambda g,c=-3:c*g or p([[(y or(0<x%8<3)and(40//x%11))%(9+(c<0))for x,y in zip((0,)+s,s)]for s in zip(*g[::-1])],c+1)
+# p=lambda g,c=-3:c*g or p([[(s[i]or(0<s[i-1]%8<3)and(40//s[i-1]%11))%(9+(c<0))for i in range(9)]for s in zip(*g[::-1])],c+1)
