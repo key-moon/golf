4ea1248d7e8ce9b32c4afcb82c900f12c655cff5
key-moon <kymn0116@gmail.com>
2025-08-25 14:14:33 +0900

    178

--- base_keymoon/task178.py
+++ base_keymoon/task178.py
@@ -3,5 +3,6 @@
 # lambda g:5>len(g)and[*map(list,zip(*g))]or []
 # lambda g,s=0:[(s:=r)*0!=r*0and p(r)or r for r in g if r!=s]
 # lambda g,s=0:[(s:=r)*0!=0and p(r)or r for r in g if r!=s]
+# lambda g:[[int,p][(g:=r)*0!=0](r)for r in g if r!=g]
 # ========================= 55 ========================
-p=lambda g:[[int,p][(g:=r)*0!=0](r)for r in g if r!=g]
+p=lambda g:g*0!=0and[p(g:=r)for r in g if r!=g]or g
