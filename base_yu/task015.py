eed14843f66f6340833fcfaa325c8f1102637973
Yu212 <yu212.mc@gmail.com>
2025-08-25 13:20:49 +0900

    015

---
+++ base_yu/task015.py
@@ -0,0 +1,3 @@
+# best: 100(4atj) / others: 104(sisyphus), 107(joking+MWI), 107(joking), 117(duckyluuk), 117(biz)
+# ============================================== 100 ===============================================
+p=lambda g,c=-3:c*g or p([[(y or(0<x%8<3)and(40//x%11))%(9+(c<0))for x,y in zip((0,)+s,s)]for s in zip(*g[::-1])],c+1)
