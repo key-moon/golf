eec62da1b8ba1956ca9ba3fe3f8f89c2c5c8dbf8
key-moon <kymn0116@gmail.com>
2025-08-25 12:29:25 +0900

    227

--- base_keymoon/task227.py
+++ base_keymoon/task227.py
@@ -2,5 +2,8 @@
 # lambda g:[*eval("[2*(x+y<1)for x,y in zip(g.pop(0),g[3])],"*4)]
 # lambda g:[*eval("[2-2*any(a)for a in zip(g.pop(0),g[3])],"*4)]
 # lambda g:[[2*~a&2for a in map(max,*c)]for c in zip(g,g[4:])]
+# lambda g:[[2-any(a)*2for a in zip(*c)]for c in zip(g,g[4:])]
+# lambda g:eval("[2-2*any(a)for a in zip(g.pop(0),g[3])],"*4)
+# lambda g:eval("[2*~a&2for a in map(max,g.pop(0),g[3])],"*4)
 # ========================= 55 ========================
-p=lambda g:[[2-any(a)*2for a in zip(*c)]for c in zip(g,g[4:])]
+p=lambda g:eval("[2*~a&2for a in map(max,g.pop(0),g[3])],"*4)
