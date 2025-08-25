a93d0316cbe520d859a6417c81d99b8a7d1852fd
key-moon <kymn0116@gmail.com>
2025-08-25 14:26:11 +0900

    218

---
+++ base_keymoon/task218.py
@@ -0,0 +1,3 @@
+# best: 60(luke) / others: 61(sisyphus), 67(joking+MWI), 72(Seek64), 72(joking), 74(mukundan)
+# =========================== 60 ===========================
+p=lambda g:[[int,p][(g:=r)*0!=0](r)for r in g if r!=g]
