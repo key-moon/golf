c8822fd16c5f9d8e02934a197a0d34c0dcc33844
Yu212 <yu212.mc@gmail.com>
2025-08-27 05:24:21 +0900

    109

---
+++ base_yu/task109.py
@@ -0,0 +1,3 @@
+# best: 90(natte) / others: 91(joking+MWI), 91(joking/MWI), 91(att), 91(joking MeWhenI), 92(4atj sisyphus luke Seek)
+# ========================================== 90 ==========================================
+p=lambda g:(n:=len(g)//2)and[[v and g[0][n]for v in s[:n]+s[n-1::-1]]for s in g[:n]+g[n-1::-1]]
