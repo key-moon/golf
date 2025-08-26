ddb5a11fc1a93a0c13913d6aff7cf858a4b17ce9
Yu212 <yu212.mc@gmail.com>
2025-08-26 17:33:18 +0900

    147

--- base_yu/task147.py
+++ base_yu/task147.py
@@ -1,3 +1,3 @@
 # best: 84(luke, luke/sisyphus/Seek) / others: 85(4atj), 88(joking+MWI), 88(joking/MWI), 88(joking), 88(sisyphus)
 # ======================================= 84 =======================================
-p=lambda g,E=enumerate:[[(s[j]and any([0,*s,0][j:j+3:2]+[0,*t,0][i:i+3:2]))*8or s[j]for j,t in E(zip(*g))]for i,s in E(g)]
+p=lambda g,E=enumerate:[[(s[j]and any([0,*s][j:j+3:2]+[0,*t][i:i+3:2]))*8or s[j]for j,t in E(zip(*g))]for i,s in E(g)]
