b1fba557fd29e9daeda8b281dc267d6037c454e3
Yu212 <yu212.mc@gmail.com>
2025-08-26 15:12:41 +0900

    091

--- base_yu/task091.py
+++ base_yu/task091.py
@@ -1,7 +1,12 @@
 # best: 78(att) / others: 92(luke), 92(luke/sisyphus/Seek), 96(mukundan), 111(joking+MWI), 111(natte)
 # ==================================== 78 ====================================
 
-p=lambda g:(a:=sum([(c:=0)or[*zip(*[(x,y)for x,y in zip(s,t)if c+(c:=c^(x==5 or y==5))])]for s,t in zip([[]]+g,g)],[]))and a[:1]+a[1::2]
+p=lambda g,c=-63:c*g or p([*zip(*g[1-(g[1].count(5)==2 and(8,5,5,8)!=g[1]or 5in g[0]):][::-1])],c+1)
+# p=lambda g,c=-63:c*g or p([*zip(*g[(g[1].count(5)!=2 or(8,5,5,8)==g[1])and 5not in g[0]:][::-1])],c+1)
+# p=lambda g,c=-63:c*g or p([*zip(*g[(g[1].count(5)!=2 or(8,5,5,8)==g[1])*(1-(5in g[0])):][::-1])],c+1)
+# p=lambda g,c=-63:c*g or p([*zip(*g[((g[-2].count(5)==2 and(8,5,5,8)!=g[-2])or 5in g[-1])-2::-1])],c+1)
+
+# p=lambda g:(a:=sum([(c:=0)or[*zip(*[(x,y)for x,y in zip(s,t)if c+(c:=c^(x==5 or y==5))])]for s,t in zip([[]]+g,g)],[]))and a[:1]+a[1::2]
 # p=lambda g:(a:=[*filter(len,[(c:=0)or[y for x,y,z in zip(s,t,u)if c+(c:=c^(x==5 or y==5 or z==5))]for s,t,u in zip([[]]+g,g,g[1:]+[[]])])])and a[:1]+a[1::2]
 
 # def p(g):
