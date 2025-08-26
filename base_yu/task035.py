f21635ca4c1e7febbf02137bc091d6d2ab40c35b
Yu212 <yu212.mc@gmail.com>
2025-08-27 05:19:42 +0900

    035

--- base_yu/task035.py
+++ base_yu/task035.py
@@ -5,5 +5,7 @@
 
 
 # p=lambda g,c=-3:c*g or p([(l:=s[0],1)and[[v,l or v][v==8 and ((l:=0) or 1)]for v in s]for s in zip(*g[::-1])],c+1)
-p=lambda g,c=-3:c*g or p([(l:=0)or[[v,(1-l)*s[0] or v][l:=l or v==8]for v in s]for s in zip(*g[::-1])],c+1)
-
+# p=lambda g,c=-3:c*g or p([(l:=0)or[[v,(1-l)*s[0]or v][l:=l or v==8]for v in s]for s in zip(*g[::-1])],c+1)
+# p=lambda g,c=-3:c*g or p([(l:=1)and[[l*s[0]or v,v][l:=l and v!=8]for v in s]for s in zip(*g[::-1])],c+1)
+# p=lambda g,c=-3:c*g or p([(l:=1)and[[l*s[0]or v,v][l:=l*(v!=8)]for v in s]for s in zip(*g[::-1])],c+1)
+p=lambda g,c=-3:c*g or p([[[l*s[0]or v,v][l:=l*(v!=8)]for v in s]for s in zip(*g[::-1])if(l:=1)],c+1)
