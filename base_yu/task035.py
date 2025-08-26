1baa4a5f4beb36ab74379e022756c8db104dd14e
Yu212 <yu212.mc@gmail.com>
2025-08-27 05:12:14 +0900

    035

--- base_yu/task035.py
+++ base_yu/task035.py
@@ -1,4 +1,9 @@
 # best: 88(mukundan) / others: 90(4atj sisyphus luke Seek), 90(4atj), 94(kabutack), 96(joking+MWI), 96(joking/MWI)
 # ========================================= 88 =========================================
-import re
-p=lambda g,c=-3:c*g or p([*zip(*eval(re.sub("([1-9])((, 0)+, )8","\\1\\2\\1",str(g)))[::-1])],c+1)
+# import re
+# p=lambda g,c=-3:c*g or p([*zip(*eval(re.sub("([1-9])((, 0)+, )8","\\1\\2\\1",str(g)))[::-1])],c+1)
+
+
+# p=lambda g,c=-3:c*g or p([(l:=s[0],1)and[[v,l or v][v==8 and ((l:=0) or 1)]for v in s]for s in zip(*g[::-1])],c+1)
+p=lambda g,c=-3:c*g or p([(l:=0)or[[v,(1-l)*s[0] or v][l:=l or v==8]for v in s]for s in zip(*g[::-1])],c+1)
+
