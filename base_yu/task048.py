4d980635a34be1b06782f6a54e070d2b41b43eca
Yu212 <yu212.mc@gmail.com>
2025-08-27 22:45:22 +0900

    048

---
+++ base_yu/task048.py
@@ -0,0 +1,8 @@
+# best: 98(4atj sisyphus luke Seek, 4atj) / others: 126(luke/sisyphus/Seek), 126(Seek64), 127(sisyphus), 131(duckyluuk), 136(joking+MWI)
+# ============================================== 98 ==============================================
+import re;s=re.sub
+def p(g):
+ d=s(*"21",s("[[ ,]","",str(g)),1)
+ for x in d:
+  d=s("[82](?=("+"."*len(g[0])+")?1)","1",d)[::-1]
+ return[[8-8*("2"in d)]]
