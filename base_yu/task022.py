79783664e1acdfa9c05ab3626bc19083ffc12bc6
Yu212 <yu212.mc@gmail.com>
2025-08-27 19:56:54 +0900

    022

--- base_yu/task022.py
+++ base_yu/task022.py
@@ -1,3 +1,4 @@
 # best: 91(4atj sisyphus luke Seek, 4atj) / others: 99(joking+MWI), 99(joking/MWI), 99(joking), 99(joking MeWhenI), 100(luke/sisyphus/Seek)
 # =========================================== 91 ==========================================
-p=lambda g,R=range:[[max(g[y+i][x+j]for y in R(9)for x in R(9)if g[y+1][x+1]==5)for j in R(3)]for i in R(3)]
+# p=lambda g,R=range:[[max(g[y+i][x+j]for y in R(9)for x in R(9)if g[y+1][x+1]==5)for j in R(3)]for i in R(3)]
+p=lambda g,R=range:[[max(g[k//9+i][k%9+j]for k in R(81)if g[k//9+1][k%9+1]==5)for j in R(3)]for i in R(3)]
