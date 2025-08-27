a02bea3d663fb181a18932c3fa7b3fbbfb8d3b88
key-moon <kymn0116@gmail.com>
2025-08-27 13:26:53 +0900

    fix regex

--- base_keymoon/task096.py
+++ base_keymoon/task096.py
@@ -50,13 +50,13 @@ def p(l):
  t=str(l)
  c=min(t,key=t.count)
  t=str(l+[*zip(*l)])
- t=re.sub('[([, ]','',t+t[::-1])
+ t=re.sub('[[(, ]','',t+t[::-1])
  b=max(t,key=t.count)
  c=[b,c][t.count(c)<9]
- s={*')]',b,c}
+ s={*'])',b,c}
  r=[]
  l=[]
- for s in [r for r in sorted([l+b*len(p)+l for t,p,l in re.findall(rf'([^{b}])((?:(?!\1|\]|\)).)+?)(\1\1+)',t)],key=len)[::-1]if s^(s:=s|{r[0]})]+[({*t}-s).pop()*3,c]:
+ for s in [r for r in sorted([l+b*len(p)+l for t,p,l in re.findall(rf'([^{b}])((?:(?!\)|]|\1).)+?)(\1\1+)',t)],key=len)[::-1]if s^(s:=s|{r[0]})]+[({*t}-s).pop()*3,c]:
   t=''.join(map(list.pop,l))
   l+=[[*s[:-1]]]
   r+=[[*map(int,t+s+t[::-1])]]
--- base_keymoon/task198.py
+++ base_keymoon/task198.py
@@ -9,7 +9,7 @@ import re;s=re.sub
 def p(g):
  a="."*len(g)
  d=a+"]"+s("[[ ,]","",str(g))+a
- v=f"[.{max(g)[0]}\]]"
+ v=f"[].{max(g)[0]}]"
  for x in d:
   d=s(f"0(?=({a})?4)|(?<={v+a}{v+a})0(?=({a}0)?({a}0)?{a+v})|(?<={v+a})0(?=({a}0)?{a+v})|(?<={v})0(?=0?{v})","4",d)[::-1]
  return[[*map(int,r)]for r in s("0","3",d).split("]")[1:-2]]
