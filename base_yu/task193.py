19cdfc3ba5bab155cb0a0ecd9500a11eafa4ab7e
Yu212 <yu212.mc@gmail.com>
2025-08-27 17:31:02 +0900

    193

--- base_yu/task193.py
+++ base_yu/task193.py
@@ -1,5 +1,8 @@
-R=range
-p=lambda g:(n:=len(g))and[[sum(-1<(y:=i+abs(k-2)-1)<n>(x:=j+abs(k-1)-1)>-1and g[y][x]>0for k in R(4))>1and g[i][j]for j in R(n)]for i in R(n)]
+# best: 86(4atj sisyphus luke Seek, luke/sisyphus/Seek, sisyphus) / others: 101(mukundan), 103(xsot ovs), 103(joking+MWI), 103(joking/MWI), 103(xsot)
+# ======================================== 86 ========================================
+# p=lambda g,c=-1:c*g or p([[([0,*s,0][i:i+3:2]>[0,0])*s[i] for i in range(len(s))]for s in zip(*g)],c+1)
+p=lambda g,c=-1:c*g or p([[any([0,*s][i:i+3:2])*s[i]for i in range(len(s))]for s in zip(*g)],c+1)
+# p=lambda g,R=range:(n:=len(g))and[[sum(-1<(y:=i+abs(k-2)-1)<n>(x:=j+abs(k-1)-1)>-1and g[y][x]>0for k in R(4))>1and g[i][j]for j in R(n)]for i in R(n)]
 
 # R=range
 # p=lambda g:(n:=len(g))and[[sum(-1<(y:=i+abs(k-2)-1)<n and-1<(x:=j+abs(k-1)-1)<n and g[y][x]>0for k in R(4))>1and g[i][j] or 0for j in R(n)]for i in R(n)]
