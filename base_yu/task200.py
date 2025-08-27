35f5b00a823dc9dffc1846c2c3f2c9ca765afec3
Yu212 <yu212.mc@gmail.com>
2025-08-27 23:20:24 +0900

    200

--- base_yu/task200.py
+++ base_yu/task200.py
@@ -4,7 +4,8 @@
 # p=lambda g:[([0]*g[9].index(c:=max(g[9]))+[c,5,c,0,c,0,c,5][((i>0)+(i>8))*2:][:4]*9)[:10]for i in range(10)]
 # p=lambda g,R=range(10):[([0]*g[9].index(c:=max(g[9]))+[[c,((i<j%4<2)+(i==j%4*3>8))*5][j%2] for j in R])[:10]for i in R]
 
-p=lambda g:[([0]*g[9].index(c:=max(g[9]))+[c,(i<1)*5,c,(i>8)*5]*9)[:10]for i in range(10)]
+# p=lambda g:[([0]*g[9].index(c:=max(g[9]))+[c,(i<1)*5,c,(i>8)*5]*9)[:10]for i in range(10)]
+p=lambda g:[([0]*g[9].index(c:=max(g[9]))+[c,0**i*5,c,(i>8)*5]*9)[:10]for i in range(10)]
 
 
 # i==0 2:6
