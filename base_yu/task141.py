396e2ef625e5745b9761ebb9425f1a8b35edb0a9
Yu212 <yu212.mc@gmail.com>
2025-08-27 16:12:11 +0900

    141

---
+++ base_yu/task141.py
@@ -0,0 +1,14 @@
+# best: 96(4atj sisyphus luke Seek) / others: 100(natte), 106(joking+MWI), 106(joking/MWI), 106(joking), 106(joking MeWhenI)
+# ============================================= 96 =============================================
+# p=lambda g:(R:=range(len(g)))and[[max((y-x==i-j or y+x==i+j)*(g[i][j]|g[y][x])for y in R for x in R)for j in R]for i in R]
+p=lambda g:(R:=range(len(g)))and[[max((y-i in(x-j,j-x))*g[y][x]for y in R for x in R)for j in R]for i in R]
+# p=lambda g:(R:=range(len(g)))and[[max(g[k][j+i-k]+g[k][j-i+k]for k in R)for j in R]for i in R]
+# p=lambda g:(R:=range(len(g)))and[[max(0<j+(i-k)*s<len(g)and g[k][j+(i-k)*s]for s in(1,-1)for k in R)for j in R]for i in R]
+
+
+
+# def p(g):
+#  u=sum(g,[])
+#  R=range(n:=len(g))
+#  y,x=divmod(u.index(c:=max(u)),n)
+#  return [[(y-x==i-j or y+x==i+j)*c for j in R]for i in R]
