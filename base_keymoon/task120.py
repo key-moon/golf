ba1cf769d9e24dd2f9bfdbc9c7e4f9254d555fe2
key-moon <kymn0116@gmail.com>
2025-08-27 14:17:28 +0900

    120

---
+++ base_keymoon/task120.py
@@ -0,0 +1,8 @@
+# best: 101(mukundan) / others: 104(joking+MWI), 104(joking/MWI), 104(joking), 104(joking MeWhenI), 104(natte)
+# =============================================== 101 ===============================================
+# port re;p=lambda g:(s:="[^0]"+"."*(len(g[0])*3+1)+"[^0]")and eval(re.sub(rf"(?<={s}, )[^0](?=, {s})","8",str(g)))
+# mport re;p=lambda g:(s:=(t:="[^0]")+"."*(len(g[0])*3+1)+t)and eval(re.sub(rf"(?<={s}, ){t}(?=, {s})","8",str(g)))
+# port re;p=lambda g:eval(re.sub(rf"(?<={(s:=(t:='[^0]')+'.'*(len(g[0])*3+1)+t)}, ){t}(?=, {s})","8",str(g)))
+# port re;p=lambda g:eval(re.sub(rf"(?<={(s:=(t:='[^0]')+'...'*len(g[0])+'.'+t)}, ){t}(?=, {s})","8",str(g)))
+# port re;p=lambda g:eval(re.sub(rf"(?<={(s:=(t:='[^0].')+'...'*len(g[0])+t)} ){t}(?= {s})","8,",str(g)))
+import re;p=lambda g:eval(re.sub(rf"(?<={(s:=(t:='.[^0]')+'...'*len(g[0])+t)},){t}(?=,{s})","8",str(g)))
