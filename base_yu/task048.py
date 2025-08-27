a647b631f442ce1c9d20eae46cb450ad826e3c98
Yu212 <yu212.mc@gmail.com>
2025-08-27 22:46:22 +0900

    048

--- base_yu/task048.py
+++ base_yu/task048.py
@@ -3,6 +3,5 @@
 import re;s=re.sub
 def p(g):
  d=s(*"21",s("[[ ,]","",str(g)),1)
- for x in d:
-  d=s("[82](?=("+"."*len(g[0])+")?1)","1",d)[::-1]
+ for x in d:d=s(f"[82](?=({'.'*len(g[0])})?1)","1",d)[::-1]
  return[[8-8*("2"in d)]]
