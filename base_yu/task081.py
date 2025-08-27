0f84be88290a16b64effc3c5023a29b5374b2f56
key-moon <kymn0116@gmail.com>
2025-08-27 15:17:22 +0900

    081

--- base_yu/task081.py
+++ base_yu/task081.py
@@ -1,4 +1,5 @@
 # best: 96(4atj sisyphus luke Seek, xsot ovs, luke/sisyphus/Seek, xsot, sisyphus) / others: 105(mukundan), 113(joking+MWI), 113(joking/MWI), 113(kg583), 113(joking MeWhenI)
 # ============================================= 96 =============================================
 # p=lambda g,c=-3:c*g or p([s for s in zip(*g[::-1])],c+1)
-import re;p=lambda g,c=-7:c*g or p(eval(re.sub("(8.{19}8, )0","\\1 1",str([*zip(*g[::-1])]))),c+1)
+# import re;p=lambda g,c=-7:c*g or p(eval(re.sub("(8.{19}8, )0","\\1 1",str([*zip(*g[::-1])]))),c+1)
+import re;p=lambda g,c=-7:c*g or p(eval(re.sub("0(, 8.{19}8)","1\\1",str([*zip(*g[::-1])]))),c+1)
