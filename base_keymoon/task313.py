b5a2853044e0a641b4f60fd8663cb0ece7507196
key-moon <kymn0116@gmail.com>
2025-08-27 23:10:40 +0900

    313

--- base_keymoon/task313.py
+++ base_keymoon/task313.py
@@ -1,4 +1,5 @@
 # best: 66(luke, 4atj sisyphus luke Seek) / others: 68(mukundan), 71(joking MeWhenI), 71(kabutack), 71(MeWhenI), 83(joking)
 # ============================== 66 ==============================
 # lambda g:([(r[1:3+(r[0]==r[3])][:3]*9)[:len(g)]for r in g[:2]]*99)[:len(g)]
-p=lambda g:([(r[1:3+(r[0]==r[3])]*9)[:(a:=len(g))]for r in g[:2]]*99)[:a]
+# lambda g:([(r[1:3+(r[0]==r[3])]*9)[:(a:=len(g))]for r in g[:2]]*99)[:a]
+p=lambda g:[(r[1:3+(r[0]==r[3])]*9)[:len(g)]for r,_ in zip(g[:2]*99,g)]
