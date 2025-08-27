2e71c844c4234c51fb8840bc005c62ba26095ffc
Yu212 <yu212.mc@gmail.com>
2025-08-27 16:30:26 +0900

    348

---
+++ base_yu/task348.py
@@ -0,0 +1,22 @@
+# best: 101(att) / others: 105(mukundan), 108(joking+MWI), 108(natte), 108(joking/MWI), 108(joking)
+# =============================================== 101 ===============================================
+
+# p=lambda g,c=-19,E=enumerate:c*g or p([[v or i+1<len(g) and j>0 and g[i+1][~(j-1)] and 15-g[i+1][~(j-1)] for j,v in E(s[::-1])] for i,s in E(g)],c+1)
+# p=lambda g,c=-19,E=enumerate:c*g or p([[v or j>0 and t[~(j-1)] and 15-t[~(j-1)] for j,v in E(s[::-1])] for s,t in zip(g,g[1:]+g[-1:])],c+1)
+# p=lambda g,c=-19:c*g or p([[v or w and 15-w for v,w in zip(s,[0]+t)][::-1]for s,t in zip(g,g[1:]+g[-1:])],c+1)
+# p=lambda g,c=-19:c*g or p([[v|(15-w)%15for v,w in zip(s,[0]+t)][::-1]for s,t in zip(g,g[1:]+g[-1:])],c+1)
+p=lambda g,c=-19:c*g or p([[v|-w%15for v,w in zip(s,[0]+t)][::-1]for s,t in zip(g,g[1:]+g[-1:])],c+1)
+
+# 7:8
+# 8:7
+# 0:0
+
+
+# def p(g):
+#  for _ in range(20):
+#   for i in range(len(g)):
+#    for j in range(len(g[i])):
+#     g[i][j]=g[i][j] or i+1<len(g) and j>0 and g[i+1][j-1] and 15-g[i+1][j-1]
+#   g=[s[::-1]for s in g]
+#  return g
+
