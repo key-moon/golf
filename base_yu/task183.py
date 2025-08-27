274a2b1cf95aa57aef14ea33f3a519fbc8f3ad17
Yu212 <yu212.mc@gmail.com>
2025-08-27 14:23:54 +0900

    183

---
+++ base_yu/task183.py
@@ -0,0 +1,7 @@
+# best: 94(joking+MWI, joking/MWI, joking, joking MeWhenI) / others: 98(Bulmenisaurus), 98(4atj sisyphus luke Seek), 98(luke/sisyphus/Seek), 98(Seek64), 98(MeWhenI)
+# ============================================ 94 ============================================
+# p=lambda g,E=enumerate:(n:=len(g))and[[v and g[[-1,0][i+2<n//2]][[-1,0][j+2<n//2]] for j,v in E(s[2:-2])]for i,s in E(g[2:-2])]
+# p=lambda g,E=enumerate:(n:=len(g)//2-2)and[[v and g[[-1,0][i<n]][[-1,0][j<n]]for j,v in E(s[2:-2])]for i,s in E(g[2:-2])]
+# p=lambda g:(R:=range(n:=len(g)-4))and[[g[i+2][j+2]and g[[-1,0][i*2<n]][[-1,0][j*2<n]]for j in R]for i in R]
+# p=lambda g:(R:=range(n:=len(g)-4))and[[g[i+2][j+2]and g[-(i*2>=n)][-(j*2>=n)]for j in R]for i in R]
+p=lambda g:(R:=range(2,n:=len(g)-2))and[[g[i][j]and g[-(i*2>n)][-(j*2>n)]for j in R]for i in R]
