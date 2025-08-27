aff97d789f835edee979414f6c5567fc848d1820
Yu212 <yu212.mc@gmail.com>
2025-08-27 23:25:28 +0900

    123

--- base_yu/task123.py
+++ base_yu/task123.py
@@ -1,5 +1,8 @@
-R=range(10)
-p=lambda g:[[g[0][max(i,j)%(4+(g[0][4]>0))]for j in R]for i in R]
+# R=range(10)
+# p=lambda g:[[g[0][max(i,j)%(4+(g[0][4]>0))]for j in R]for i in R]
+
+
+p=lambda g,R=range(10):[[g[0][max(i,j)%(5-0**g[0][4])]for j in R]for i in R]
 
 # def p(g):
 #  return[[g[0][max(i,j)%(4+(g[0][4]>0))] for j in range(10)]for i in range(10)]
