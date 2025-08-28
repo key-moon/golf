5986963862e9a01f87e1b847a078113539d9e72f
key-moon <kymn0116@gmail.com>
2025-08-28 14:43:06 +0900

    tag

--- base_yu/task291.py
+++ base_yu/task291.py
@@ -1,6 +1,4 @@
 # best: 62(duckyluuk) / others: 69(Bulmenisaurus), 70(att), 71(luke), 71(joking), 71(xsot)
 # ============================ 62 ============================
-# import re
-# p=lambda g:[[int(re.search("([1-9])(, 0)+, \\1",str(g)).group(1))]]
-
+# import re;p=lambda g:[[int(re.search("([^0]), 0[^]]+\\1",str(g))[1])]]
 p=lambda g:[[c for c in range(1,10)if len({s.count(c) for s in g})>2]]
