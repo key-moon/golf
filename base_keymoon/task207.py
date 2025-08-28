550255d3745b7de18fb2626a57b5216abfe9cd8f
key-moon <kymn0116@gmail.com>
2025-08-28 14:32:22 +0900

    346

---
+++ base_keymoon/task207.py
@@ -0,0 +1,6 @@
+# best: 81(4atj sisyphus luke Seek) / others: 84(Seek64), 84(biz), 84(luke), 84(4atj), 84(kabutack)
+# lambda g:[[(a:=sorted(g[i][j::3]+g[i+3][j::3]))[0]^a[1]^a[3]for j in(0,1)]for i in(0,1)]
+# ====================================== 81 =====================================
+p=lambda g:[[sum(u:=g[i][j::3]+g[i+3][j::3])%(9**max(u))for j in(0,1)]for i in(0,1)]
+# lambda g:[eval("[sum(u:=g.pop(0)[j::3]+g[2][j::3])%(9**max(u))for j in(0,1)],"*3)]
+# lambda g:min(p:=[[r[j:j+2]for r in g[i:i+2]]for i in(0,3)for j in(0,3)],key=p.count)
--- base_keymoon/task227.py
+++ base_keymoon/task227.py
@@ -3,7 +3,10 @@
 # lambda g:[*eval("[2-2*any(a)for a in zip(g.pop(0),g[3])],"*4)]
 # lambda g:[[2*~a&2for a in map(max,*c)]for c in zip(g,g[4:])]
 # lambda g:[[2-any(a)*2for a in zip(*c)]for c in zip(g,g[4:])]
+# { 0: 1, 3: 0 }
 # lambda g:eval("[2-2*any(a)for a in zip(g.pop(0),g[3])],"*4)
+# lambda g:eval("[~-any(a)%3for a in zip(g.pop(0),g[3])],"*4)
+# { 0: 2, 3: 0 }
 # lambda g:eval("[2*~a&2for a in map(max,g.pop(0),g[3])],"*4)
 # ========================= 55 ========================
 p=lambda g:eval("[2*~a&2for a in map(max,g.pop(0),g[3])],"*4)
