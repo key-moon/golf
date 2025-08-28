047bccb4d6c7d7500c4f8e55a49a05733a3bcc35
key-moon <kymn0116@gmail.com>
2025-08-28 14:22:54 +0900

    098

--- base_keymoon/task098.py
+++ base_keymoon/task098.py
@@ -1,4 +1,9 @@
 # best: 98(mukundan) / others: 100(luke/sisyphus/Seek), 100(4atj sisyphus luke Seek), 100(natte), 100(sisyphus), 101(xsot ovs)
 # 類題: 120 なんだけど 120 のが 3 長い 同じ色の長方形がないことを使う?
+# port re;p=lambda g:eval(re.sub(f"(?<={(s:=(t:='.[^0]')+'...'*len(g[0])+t)},){t}(?=,{s})",'0',str(g)))
 # ============================================== 98 ==============================================
-import re;p=lambda g:eval(re.sub(f"(?<={(s:=(t:='.[^0]')+'...'*len(g[0])+t)},){t}(?=,{s})",'0',str(g)))
+# port re;p=lambda g:eval(re.sub(f"(?<={(s:=(t:='.[^0]')+'...'*len(g[0])+t)},)..(?=,{s})",'0',str(g)))
+# .[^0].......[^0]
+# port re;p=lambda g:eval(re.sub("(?<="+(s:='[^0]..{%d}[^0]'%len(g[0]*3))+", ).(?=, %s)"%s,'0',str(g)))
+# port re;p=lambda g:eval(re.sub("(?<=%s, ).(?=, %s)"%(s:='[^0]..{%d}[^0]'%len(g[0]*3),s),'0',str(g)))
+import re;p=lambda g:eval(re.sub(f"(?<={(s:='[^0]..{%d}[^0]'%len(g[0]*3))}, ).(?=, {s})",'0',str(g)))
