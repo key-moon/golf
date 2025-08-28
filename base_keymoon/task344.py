c241048c980083b9dd36a5167b828b61074c0729
key-moon <kymn0116@gmail.com>
2025-08-28 14:41:18 +0900

    merge 344

--- base_keymoon/task344.py
+++ base_keymoon/task344.py
@@ -1,3 +1,5 @@
 # best: 78(sisyphus) / others: 87(mukundan), 87(luke), 95(natte), 99(Seek64), 116(joking+MWI)
 # ==================================== 78 ====================================
+# p=lambda g,c=4:c and p([[y or(x==1)for x,y in zip((0,)+s,s)]for s in zip(*g[::-1])],c-1)or g
+# import re;p=lambda g,c=4:c and p([*zip(*eval(re.sub("3, 2","8,0",str(g)))[::-1])],c-1)or g
 p=lambda g,c=-3:c*g or p([*zip(*eval(str(g).replace("3, 2","8,0"))[::-1])],c+1)
--- base_yu/task344.py
+++
@@ -1,6 +0,0 @@
-# best: 78(sisyphus) / others: 87(luke), 87(mukundan), 95(natte), 99(Seek64), 116(joking+MWI)
-# ==================================== 78 ====================================
-# p=lambda g,c=4:c and p([[y or(x==1)for x,y in zip((0,)+s,s)]for s in zip(*g[::-1])],c-1)or g
-
-# import re;p=lambda g,c=4:c and p([*zip(*eval(re.sub("3, 2","8,0",str(g)))[::-1])],c-1)or g
-import re;p=lambda g,c=-3:c*g or p([*zip(*eval(re.sub("3, 2","8,0",str(g)))[::-1])],c+1)
