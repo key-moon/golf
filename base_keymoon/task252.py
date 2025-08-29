b8790b537f9ce62a3d8faa81e9619c9883be2863
key-moon <kymn0116@gmail.com>
2025-08-29 21:38:23 +0900

    252

--- base_keymoon/task252.py
+++ base_keymoon/task252.py
@@ -2,4 +2,4 @@
 # lambda g,R=[0]*99:[R:=[[c,4][4!=C!=0]for c,C in zip(r,[0]+R)]for r in g]
 # lambda g:[g:=[[c,4][C*0==0and 4!=C>0]for c,C in zip(r,[0]+g)]for r in g]
 # ========================== 57 =========================
-p=lambda g:[g:=[[c,4][C*0==0<C!=4]for c,C in zip(r,[0]+g)]for r in g]
+p=lambda g:[g:=[[c,4][0<c==C]for c,C in zip(r,[0]+g)]for r in g]
