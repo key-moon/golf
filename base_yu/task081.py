4661e5dedc50f0fdb638941eb5d127d214d00c8f
key-moon <kymn0116@gmail.com>
2025-08-30 03:00:11 +0900

    081

--- base_yu/task081.py
+++ base_yu/task081.py
@@ -3,4 +3,4 @@
 # ============================================= 96 =============================================
 # p=lambda g,c=-3:c*g or p([s for s in zip(*g[::-1])],c+1)
 # import re;p=lambda g,c=-7:c*g or p(eval(re.sub("(8.{19}8, )0","\\1 1",str([*zip(*g[::-1])]))),c+1)
-import re;p=lambda g,c=-7:c*g or p(eval(re.sub("0(, 8.{19}8)","1\\1",str([*zip(*g[::-1])]))),c+1)
+import re;p=lambda g,c=-7:c*g or p(eval(re.sub("0(?=, 8.{19}8)","1",str([*zip(*g[::-1])]))),c+1)
