eff178a6ded3a21e061a7e020faccb4bcf9407b8
Yu212 <yu212.mc@gmail.com>
2025-08-30 23:21:06 +0900

    216,365

--- base_yu/task216.py
+++ base_yu/task216.py
@@ -1,7 +1,10 @@
+# best: 114(jailctf merger) / others: 117(mukundan), 125(xsot ovs att joking mewheni), 125(xsot ovs att), 126(biz), 141(joking)
+# ===================================================== 114 ======================================================
 # p=lambda g:max([[s[l%20:[*g[l//20],0].index(0,l%20)]for s in g[l//20:[*[g[k][l%20]for k in range(20)],0].index(0,l//20)]]for l in range(400)],key=lambda t:sum(t,[]).count(2))
 # p=lambda g:max([(sum(t:=[s[l%20:[*g[l//20],0].index(0,l%20)]for s in g[l//20:[*[g[k][l%20]for k in range(20)],0].index(0,l//20)]],[]).count(2),~l,t)for l in range(400)])[2]
-p=lambda g:max([(sum(t:=[s[l%20:[*g[l//20],0].index(0,l%20)]for s in g[l//20:[*[*[*zip(*g)][l%20]],0].index(0,l//20)]],[]).count(2),~l,t)for l in range(400)])[2]
-
+# p=lambda g:max([(sum(t:=[s[l%20:[*g[l//20],0].index(0,l%20)]for s in g[l//20:[*[*[*zip(*g)][l%20]],0].index(0,l//20)]],[]).count(2),~l,t)for l in range(400)])[2]
+# p=lambda g:max((all(y:=sum(x:=[s[l:r]for s in g[u:d]],[])),y.count(2),len(y),x)for r in range(21)for l in range(r)for d in range(21)for u in range(d))[3]
+p=lambda g:max((all(y:=sum(x:=[s[l:t%21]for s in g[u:t//21]],[])),y.count(2),len(y),x)for t in range(441)for l in range(t%21)for u in range(t//21))[3]
 
 # def p(g):
 #  V=0
