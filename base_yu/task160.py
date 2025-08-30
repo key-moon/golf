70b22732474c95ef8b0bd546bcf9d518638ad2cc
Yu212 <yu212.mc@gmail.com>
2025-08-31 03:29:38 +0900

    160

---
+++ base_yu/task160.py
@@ -0,0 +1,11 @@
+# best: 115(xsot ovs att joking mewheni) / others: 116(jailctf merger), 117(4atj sisyphus luke Seek), 117(joking), 117(joking MeWhenI), 117(sisyphus)
+# ====================================================== 115 ======================================================
+import re
+p=lambda g:eval(r'(g:=eval(re.sub("1, 1, 1(.{25})1, 0, 1(.{25})1, 1, 1",r"0,2,0\1 2,2,2\2 0,2,0",str(g)))),'*2)[1]
+
+
+# import re
+# def p(g):
+#  g=eval(re.sub("1, 1, 1(.{25})1, 0, 1(.{25})1, 1, 1","0,2,0\\1 2,2,2\\2 0,2,0",str(g)))
+#  g=eval(re.sub("1, 1, 1(.{25})1, 0, 1(.{25})1, 1, 1","0,2,0\\1 2,2,2\\2 0,2,0",str(g)))
+#  return g
