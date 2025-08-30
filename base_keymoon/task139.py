39e4c6d9ca7d4815fe622668120ae48a71c2826a
key-moon <kymn0116@gmail.com>
2025-08-30 11:32:51 +0900

    139

--- base_keymoon/task139.py
+++ base_keymoon/task139.py
@@ -1,5 +1,5 @@
-# best: 100(joking+MWI, joking/MWI, joking, joking MeWhenI) / others: 102(luke/sisyphus/Seek), 102(4atj sisyphus luke Seek), 102(sisyphus), 114(Seek64), 116(mukundan)
+# best: 94(jailctf merger) / others: 100(joking), 100(joking MeWhenI), 102(4atj sisyphus luke Seek), 102(sisyphus), 114(Seek64)
 # 類題: 081
-# ============================================== 100 ===============================================
-import re;p=lambda g,c=-63:c*g or p(eval(re.sub("0(, [47].{25}[47])","7\\1",str([*zip(*g[::-1])]))),c+1)
-
+# port re;p=lambda g,c=-63:c*g or p(eval(re.sub("0(, [47].{25}[47])","7\\1",str([*zip(*g[::-1])]))),c+1)
+# ============================================ 94 ============================================
+import re;p=lambda g,c=-11:c*g or p(eval(re.sub("0(?=, [47].{25}[47])","7",str([*zip(*g[::-1])]))),c+1)
