59f13a078654d00d4e2071c2d3a5b3c96e39bbbd
Yu212 <yu212.mc@gmail.com>
2025-08-30 20:21:23 +0900

    112

---
+++ base_yu/task112.py
@@ -0,0 +1,16 @@
+# best: 109(jailctf merger) / others: 110(4atj sisyphus luke Seek), 110(sisyphus), 112(joking), 112(joking MeWhenI), 114(mukundan)
+# =================================================== 109 ===================================================
+
+# p=lambda g,c=-1:c*g or p([*zip(*(g[:(i:=[i for i,s in enumerate(g)if 3in s][0])+1]+g[i::-1]+[g[-1]]*99)[:len(g)])],c+1)
+# p=lambda g,c=-1:c*g or p([*zip(*(g[:(i:=g.index(max(g,key=max)))+1]+g[i::-1]+[g[-1]]*99)[:len(g)])],c+1)
+p=lambda g,c=-1:c*g or p([*zip(*((a:=g[:g.index(max(g,key=max))+1])+a[::-1]+[g[-1]]*99)[:len(g)])],c+1)
+
+
+# def p(g):
+#  i=[i for i,s in enumerate(g)if 3in s][0]
+#  print(g.index(max(g,key=max)))
+#  print(i)
+#  g=[*zip(*(g[:i+1]+g[i::-1]+[g[-1]]*99)[:len(g)])]
+#  i=[i for i,s in enumerate(g)if 3in s][0]
+#  g=[*zip(*(g[:i+1]+g[i::-1]+[g[-1]]*99)[:len(g)])]
+#  return g
