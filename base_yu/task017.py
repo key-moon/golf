46fb14b564ca216ba6488ef0179f83e4fbcde4d5
Yu212 <yu212.mc@gmail.com>
2025-08-30 18:18:08 +0900

    017

--- base_yu/task017.py
+++ base_yu/task017.py
@@ -3,7 +3,9 @@
 # p=lambda g,R=range(21):[[[u[(i%d,j%d)]for j in R]for i in R]for d in range(4,10)if len(u:=dict(m:=[((i%21%d,i//21%d),v)for i,v in enumerate(sum(g,[]))if v]))==len({*m})][0]
 
 
-p=lambda g:[u for d in range(4,10)if(u:=[[max(max(g[i%d::d])[j%d::d])for j in range(21)]for i in range(21)])if sum(s in g for s in u)>9][0]
+# p=lambda g:[u for d in range(4,10)if(u:=[[max(max(g[i%d::d])[j%d::d])for j in range(21)]for i in range(21)])if sum(s in g for s in u)>9][0]
+p=lambda g,R=range(21):[u for d in range(4,10)if(u:=[[max(max(g[i%d::d])[j%d::d])for j in R]for i in R])[0]in g or u[1]in g][0]
+
 
 # def p(g):
 #  cor = [d for d in range(4,10)if len(u:=dict(m:=[((i%21%d,i//21%d),v)for i,v in enumerate(sum(g,[]))if v]))==len({*m})][0]
