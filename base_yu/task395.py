ccca5d6b16c9917db8761a15b242a87fd9526fa9
key-moon <kymn0116@gmail.com>
2025-08-30 18:39:03 +0900

    395

--- base_yu/task395.py
+++ base_yu/task395.py
@@ -1,4 +1,5 @@
 # def p(g):n=len(g)//2;return[[2*(a==b==0)for a,b in zip(g[i],g[i+n])]for i in range(n)]
 # def p(g):return[[2*(a==b)for a,b in zip(*c)]for c in zip(g,g[len(g)//2:])]
 # lambda g:[[2*(a==b)for a,b in zip(*c)]for c in zip(g,g[3:])]
-p=lambda g:eval("[2-any(a)*2for a in zip(g.pop(0),g[2])],"*3)
+# lambda g:eval("[2-any(a)*2for a in zip(g.pop(0),g[2])],"*3)
+p=lambda g:[[~-a&2for a in map(max,g.pop(3),s)] for s in g]
