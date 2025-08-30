45c79b4a505ef9f89b0300a9afcbb77f590e5c13
key-moon <kymn0116@gmail.com>
2025-08-30 18:56:47 +0900

    mapping

--- base_yu/task395.py
+++ base_yu/task395.py
@@ -3,3 +3,5 @@
 # lambda g:[[2*(a==b)for a,b in zip(*c)]for c in zip(g,g[3:])]
 # lambda g:eval("[2-any(a)*2for a in zip(g.pop(0),g[2])],"*3)
 p=lambda g:[[~-a&2for a in map(max,g.pop(3),s)] for s in g]
+
+# mapping = { 0: 2, 9: 0, 1: 0 }
