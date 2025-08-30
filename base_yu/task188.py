8570a044a108ae99180b2ed6ad1bb280b6bdd8f5
Yu212 <yu212.mc@gmail.com>
2025-08-30 12:30:29 +0900

    188

--- base_yu/task188.py
+++ base_yu/task188.py
@@ -1,3 +1,5 @@
 # 3456789012345678901234567890123456789012345678901234567890123456789012
-p=lambda g:[g[:(h:=len(g)//2)],[s[:(w:=len(s)//2)]for s in g]][g[0]!=g[h]or h<w]
+# p=lambda g:[g[:(h:=len(g)//2)],[s[:(w:=len(s)//2)]for s in g]][g[0]!=g[h]or h<w]
 # p=lambda g:[g[:(h:=len(g)//2)],[s[:(w:=len(g[0])//2)]for s in g]][g[0]!=g[h]]
+# p=lambda g:(g==(a:=g[:53%~-len(g)])*2)*a or[*map(p,g)]
+p=lambda g:(a:=g[:53%~-len(g)])*(g==a*2)or[*map(p,g)]
