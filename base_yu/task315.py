fe37b777a9f80eb541e290cc67395a088fb36d72
key-moon <kymn0116@gmail.com>
2025-08-31 13:19:02 +0900

    315(failed)

--- base_yu/task315.py
+++ base_yu/task315.py
@@ -2,5 +2,10 @@
 # ============================= 63 ============================
 # p=lambda g,R=range(9):[[(g[i//3][j//3]>1)*g[i%3][j%3]for j in R]for i in R]
 # p=lambda g:[sum([[[0,0,0],s][v>1]for v in t],[])for t in g for s in g]
+# lambda g:[[x//2*y for x in t for y in s]for t in g for s in g]
 p=lambda g:[[y*(x>1)for x in t for y in s]for t in g for s in g]
-
+# {
+#   (0,0):0, (0,1):0, (0,2):0,
+#   (1,0):0, (1,1):0, (1,2):0,
+#   (2,0):0, (2,1):1, (2,2):2
+# }[(x,y)]
