ba0a5a57366cc97c84095ba0cc19738a1ddc3721
Yu212 <yu212.mc@gmail.com>
2025-09-01 01:09:35 +0900

    124

--- base_yu/task124.py
+++ base_yu/task124.py
@@ -1,9 +1,14 @@
-R=range
-def p(g):
- for w in R(8,16):
-  x,y,h=w%4,w//4,len(g)
-  if all(g[i][j]==g[i-y][j-x]for i in R(y,h)for j in R(x,10)):
-   return g+[[0]*(v:=i//y*x)+g[i%y][:10-v]for i in R(h,10)]
+# best: 123(mukundan) / others: 130(xsot ovs att joking mewheni), 141(jailctf merger), 196(Jonas), 223(jacekw), 224(jacekwl)
+# ========================================================== 123 ==========================================================
+# def p(g):
+#  for w in range(6):
+#   x,y=w%3,w//3+2
+#   u=[([0]*(i//y*x)+g[i%y])[:10]for i in range(10)]
+#   if g==u[:len(g)]:
+#    return u
+
+p=lambda g,c=5:((u:=[([0]*(i//(y:=c%2+2)*(c//2))+g[i%y])[:10]for i in range(10)])[:len(g)]==g)*u or p(g,c-1)
+
 
 # def p(g):
 #  h=len(g)
