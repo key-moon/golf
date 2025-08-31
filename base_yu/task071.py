5389f31ee2e4ff81e0449c7e46bea1fabb7e2d1b
Yu212 <yu212.mc@gmail.com>
2025-08-31 18:38:27 +0900

    071

--- base_yu/task071.py
+++ base_yu/task071.py
@@ -1,12 +1,23 @@
 # best: 122(jailctf merger) / others: 135(4atj sisyphus luke Seek), 182(jacekwl), 187(jacekw), 224(Potatoman), 235(xsot ovs att joking mewheni)
 # ========================================================= 122 ==========================================================
-def p(g):
- c=[*filter(int,sum(g,[]))][0]
- for p in range(32):
-  x=[[{s[j],(s+g[0])[p-j]}&{0,c}for j in range(16)]for s in g]
-  if{0,c}>max(max(x)):
-   return[[*map(sum,s)]for s in x]
-   
+
+p=lambda g:[x for p in range(32)if min(map(len,(x:=[[sum(t)for j in range(16)if len(t:={s[j],(s+g[0])[p-j]}&{0,[*filter(int,sum(g,[]))][0]})<2]for s in g])))>15][0]
+
+# def p(g):
+#  c=[*filter(int,sum(g,[]))][0]
+#  for p in range(32):
+#   x=[[sum(t)for j in range(16)if(t:={s[j],(s+g[0])[p-j]}&{0,c})<{0,c}]for s in g]
+#   if min(map(len,x))>15:
+#    return x
+
+
+# def p(g):
+#  c=[*filter(int,sum(g,[]))][0]
+#  for p in range(32):
+#   x=[[{s[j],(s+g[0])[p-j]}&{0,c}for j in range(16)]for s in g]
+#   if{0,c}>max(max(x)):
+#    return[[*map(sum,s)]for s in x]
+
 
 # R=range
 # def p(g):
