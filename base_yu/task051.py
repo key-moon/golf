104f086674a51be06b66a8305904caed92d4e064
key-moon <kymn0116@gmail.com>
2025-08-31 18:41:48 +0900

    51

--- base_yu/task051.py
+++ base_yu/task051.py
@@ -3,7 +3,10 @@
 # p=lambda g,c=-3:c*g or p([*zip(*[(u:=0)or[[u:=u|(v>0)*(len({*s[i-3:i]})>2)*s[i-2],v][v>0]for i,v in enumerate(s)]for s in g[::-1]])],c+1)
 # p=lambda g,c=-3:c*g or p([*zip(*[(u:=0)or[[u:=u|(len({*s[i-3:i]})>2>0<v)*s[i-2],v][v>0]for i,v in enumerate(s)]for s in g[::-1]])],c+1)
 
-import re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub(r"0(?=(, 0)*(, [1-9])+(?!\2), ([1-9]), 0)",r"\3",str(p(g,c+1)))))][::-1]
+# port re;p=lambda g,c=-63:c*g or[*zip(*eval(re.sub(r"(0, ([^0]), ((?!\2|0)., )+(0, )*)0",r"\1\2",str(p(g,c+1)))))][::-1]
+# port re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub(r"0(?=[, 0]*(, [^0])+(?!\1), ([^0]), 0)",r"\2",str(p(g,c+1)))))][::-1]
+# port re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub(r"0(?=[^[(]*(, [^0])+(?!\1), ([^0]), 0)",r"\2",str(p(g,c+1)))))][::-1]
+import re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub(r"0(?=(, 0)*(, [^0])+(?!\2), ([^0]), 0)",r"\3",str(p(g,c+1)))))][::-1]
 
 # def p(g):
 #  for _ in range(4):
