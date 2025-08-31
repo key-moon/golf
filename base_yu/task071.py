e4106e9ac35a8576640147a7c16ab179784d75e6
Yu212 <yu212.mc@gmail.com>
2025-08-31 18:32:25 +0900

    071

--- base_yu/task071.py
+++ base_yu/task071.py
@@ -1,6 +1,16 @@
-R=range
+# best: 122(jailctf merger) / others: 135(4atj sisyphus luke Seek), 182(jacekwl), 187(jacekw), 224(Potatoman), 235(xsot ovs att joking mewheni)
+# ========================================================= 122 ==========================================================
 def p(g):
- c=[v for v in sum(g,[])if v][0]
- for p in R(32):
-  if all((0<=p-j<16)and g[i][p-j]for I in R(256)if g[i:=I%16][j:=I>>4]==c):
-   return [[~(g[i][j]not in(0,c)and~g[i][p-j]or~g[i][j])for j in R(16)]for i in R(16)]
+ c=[*filter(int,sum(g,[]))][0]
+ for p in range(32):
+  x=[[{s[j],(s+g[0])[p-j]}&{0,c}for j in range(16)]for s in g]
+  if{0,c}>max(max(x)):
+   return[[*map(sum,s)]for s in x]
+   
+
+# R=range
+# def p(g):
+#  c=[v for v in sum(g,[])if v][0]
+#  for p in R(32):
+#   if all((0<=p-j<16)and g[i][p-j]for I in R(256)if g[i:=I%16][j:=I>>4]==c):
+#    return [[~(g[i][j]not in(0,c)and~g[i][p-j]or~g[i][j])for j in R(16)]for i in R(16)]
