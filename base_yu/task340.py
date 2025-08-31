f891581460be08d29cf4dff7292038228e610eec
Yu212 <yu212.mc@gmail.com>
2025-08-31 15:35:10 +0900

    340

--- base_yu/task340.py
+++ base_yu/task340.py
@@ -1,13 +1,24 @@
+# best: 120(mukundan) / others: 133(xsot ovs att joking mewheni), 133(joking MeWhenI), 136(joking), 137(jailctf merger), 152(MeWhenI)
+# ======================================================== 120 =========================================================
+
 def p(g):
- for _ in(u:=[0])*4:
-  for s in g[1:-1]:
-   if s[0]in s[1:]:s[s.index(s[0],1)],s[1]=0,s[0]
-   u+=[s[0]]
-  *g,=map(list,zip(*g[::-1]))
- for s in g:
-  for j in range(len(s)):
-   if s[j]not in u:s[j]=0
- return g
+ for _ in range(4):
+  # g=g[:1]+[[v or max(g[0])*(max(g[0])in s)for*s,v in zip(*g[1:],g[1])]]+g[2:]
+  # g=g[:1]+[[v or(t:=max(g[0]))*(t in s)for*s,v in zip(*g[1:],g[1])]]+g[2:]
+  # g=g[:1]+[[v|max({*g[0]}&{*s})for*s,v in zip(*g[1:],g[1])]]+g[2:]
+  # g=[[s[0],s[1]|(s[0]in s[2:])*s[0],*s[2:]]for s in zip(*g[::-1])]
+  g=[[s[0],max({*s[:2]}&{*s[2:]}),*s[2:]]for s in zip(*g[::-1])]
+ return g[:2]+[s[:2]+[0]*(len(s)-4)+s[-2:] for s in g[2:-2]]+g[-2:]
+# def p(g):
+#  for _ in(u:=[0])*4:
+#   for s in g[1:-1]:
+#    if s[0]in s[1:]:s[s.index(s[0],1)],s[1]=0,s[0]
+#    u+=[s[0]]
+#   *g,=map(list,zip(*g[::-1]))
+#  for s in g:
+#   for j in range(len(s)):
+#    if s[j]not in u:s[j]=0
+#  return g
 
 
 # def p(g):
