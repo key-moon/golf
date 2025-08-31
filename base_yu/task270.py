8b7a3ab5e42d420864b287024d25c75c6719d52e
Yu212 <yu212.mc@gmail.com>
2025-09-01 01:32:13 +0900

    270

--- base_yu/task270.py
+++ base_yu/task270.py
@@ -1,6 +1,7 @@
 # best: 119(xsot ovs att joking mewheni) / others: 130(jailctf merger), 131(mukundan), 135(4atj sisyphus luke Seek), 168(MasukenSamba), 222(jacekwl)
 # ======================================================== 119 ========================================================
-import re;p=lambda g,c=-7:c*g or p([*zip(*eval(r'(g:=eval(re.sub("%d, ((\d, )*)0, %d",r"0,\1 %d,%d",str(g)))),'*2%(3,2,3,2,7,1,7,1))[1][::-1])],c+1)
+# import re;p=lambda g,c=-7:c*g or p([*zip(*eval(r'(g:=eval(re.sub("%d, ((\d, )*)0, %d",r"0,\1 %d,%d",str(g)))),'*2%(3,2,3,2,7,1,7,1))[1][::-1])],c+1)
+import re;p=lambda g,c=-7:c*g or p([*zip(*eval(r'(g:=eval(re.sub("(%d), ((\d, )*)0, (%d)",r"0,\2 \1,\4",str(g)))),'*2%(3,2,7,1))[1][::-1])],c+1)
 
 # import re
 # def p(g):
