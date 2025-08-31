95b1ef54991d1bec14804529d9f08a2b3319887b
key-moon <kymn0116@gmail.com>
2025-08-31 22:56:44 +0900

    014(failed to improve)

--- base_yu/task014.py
+++ base_yu/task014.py
@@ -1,3 +1,5 @@
 # 34567890123456789012345678901234567890123456789012345678901234567890
 # p=lambda g:[[v for v,t in zip(s,zip(*g))if len({*t})>2]for s in g if len({*s})>2]
-p=lambda g:[[v for*t,v in zip(*g,s)if len({*t})>2]for s in g if len({*s})>2]
+# lambda g:[[v for*t,v in zip(*g,s)if len({*t})>2]for s in g if len({*s})>2]
+# lambda g:[[t[0]for t in zip(s,*g)if len({*t})>2]for s in g if len({*s})>2]
+p=lambda g:[R for s in g if(R:=[t[0]for t in zip(s,*g)if len({*t}&{*s})>2])]
