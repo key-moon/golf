938462be5039833fbaff8884cd7d80bc9544a190
key-moon <kymn0116@gmail.com>
2025-08-31 14:57:35 +0900

    095

--- base_yu/task095.py
+++ base_yu/task095.py
@@ -1,4 +1,5 @@
-# best: 80(sisyphus) / others: 81(luke), 86(4atj), 87(mukundan), 90(natte), 92(joking+MWI)
-# ===================================== 80 =====================================
+# best: 77(mukundan, jailctf merger) / others: 80(4atj sisyphus luke Seek), 90(natte), 91(kabutack), 92(xsot ovs att joking mewheni), 109(duckyluuk)
 # p=lambda g,c=-3:c*g or p([[y or x>0for x,y in zip((0,)+s,s)]for s in zip(*g[::-1])],c+1)
-p=lambda g,c=-3,l=0:c*g or p([[(l>0)|(l:=y)for y in s]for s in zip(*g[::-1])],c+1)
+# ==================================== 77 ===================================
+p=lambda g,c=-3:c*g or[[(c>0)|(c:=y)for y in s]for s in zip(*p(g,c+1)[::-1])]
+
