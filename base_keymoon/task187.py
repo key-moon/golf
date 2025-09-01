5b1e701037ef243bf7f5757357bc5af38c339ec7
key-moon <kymn0116@gmail.com>
2025-09-01 12:07:50 +0900

    187

--- base_keymoon/task187.py
+++ base_keymoon/task187.py
@@ -1,4 +1,10 @@
 # best: 101(jailctf merger) / others: 104(4atj sisyphus luke Seek), 104(sisyphus), 106(mukundan), 107(joking), 107(joking MeWhenI)
 # =============================================== 101 ===============================================
+# port re;p=lambda g,c=-63:g*c or[*zip(*eval(re.sub(*['2(?=, 3|\))',*'032'][c>=0::2],str(p(g,c+1)))))][::-1]
 # port re;S=re.sub;p=lambda g,c=-63:g*c or eval(S("2(?=, 3|\))","3",S(*"02",str([*zip(*p(g,c+1))][::-1]))))
-import re;S=re.sub;p=lambda g,c=-63:g*c or eval(S("2(?=, 3|\))","3",S(*"02",str([*zip(*p(g,c+1))][::-1]))))
+# port re;S=re.sub;p=lambda g,c=-127:S(*"02",str(g))*c or[*zip(*eval(S("2(?=, 3|\))","3",str(p(g,c+1)))))][::-1]
+import re;S=re.sub;p=lambda g,c=-63:g*c or[*zip(*eval(S("2(?=, 3|\))","3",S(*"02",str(p(g,c+1))))))][::-1]
+
+# import re
+# S=re.sub
+# p=lambda g,c=-63:g*c or[*zip(*eval(S(*['2(?=, 3|\))',*'032'][::2],S(*'02',str(p(g,c+1))))))][::-1]
