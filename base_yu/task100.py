9a5bd6d3c0db3653cbe3024241ae74b6f3edbffb
key-moon <kymn0116@gmail.com>
2025-09-02 02:09:36 +0900

    100

--- base_yu/task100.py
+++ base_yu/task100.py
@@ -1,5 +1,5 @@
-# best: 88(xsot ovs att joking mewheni, 4atj sisyphus luke Seek, jailctf merger) / others: 90(mukundan), 100(nauti), 105(jacekwl), 105(jacekw), 109(Bulmenisaurus)
+# best: 88(xsot ovs att joking mewheni, 4atj sisyphus luke Seek, jailctf merger) / others: 90(mukundan), 100(nauti), 105(jacekw), 105(jacekwl), 109(Bulmenisaurus)
 # ========================================= 88 =========================================
 # p=lambda g:[sorted(set(a:=sum(g,[])),key=a.count)[1:2]*2]*2
 # p=lambda g:[sorted(set(a:=sum(g,[])),key=lambda c:(x:=max(s.count(c)for s in g))*(a.count(c)-2*x+4))[1:2]*2]*2
-p=lambda g:[[max([(s.count(v)*t.count(v),v)for s in g for*t,v in zip(*g,s)if v])[1]]*2]*2
+p=lambda g:[max([[s.count(v)*t.count(v),v]for s in g for*t,v in zip(*g,s)if v])[1:]*2]*2
