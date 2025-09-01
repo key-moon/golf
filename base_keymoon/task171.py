7a1c19f2b51bd3ce18d8c175965b25e0f2fa3c09
key-moon <kymn0116@gmail.com>
2025-09-01 12:06:41 +0900

    171

--- base_keymoon/task171.py
+++ base_keymoon/task171.py
@@ -2,9 +2,11 @@
 # lambda g:(a:=[[8]*len(g[0])])+[[8,*[0]*(len(g[0])-2),8]]*(len(g)-2)+a
 # lambda g,c=-1:g*c or p([*zip(*(a:=[[8]*len(g[0])])+g[1:-1]+a)],c+1)
 # lambda g:(a:=[[8]*len(g[0])])+[[8,*[0]*(len(a)-2),8]]*(len(g)-2)+a
+# lambda g,c=-1:g*c or p([*zip(*[a:=[8]*len(g[0]),*g[2:],a])],c+1)
+# lambda g,c=-3:g*c or p([*zip(*[[8]*len(g[0])]+g[1:])][::-1],c+1)
 # lambda g,c=-3:g*c or p([r+[8]for*r,_ in zip(*g[::-1])],c+1)
 # lambda g,c=-1:g*c or p([[8,*r,8]for _,*r,_ in zip(*g)],c+1)
 # lambda g,c=-1:g*c or p([[8,*r[2:],8]for r in zip(*g)],c+1)
 # lambda g,c=-1:g*c or p([[8,*r,8]for*r,_,_ in zip(*g)],c+1)
 # ======================== 54 ========================
-p=lambda g,c=-1:g*c or p([[8,*r[2:],8]for r in zip(*g)],c+1)
+p=lambda g,c=-1:g*c or[[8,*r[2:],8]for r in zip(*p(g,c+1))]
