677a124d8c2663cc2ac5bf316a65627274db607d
key-moon <kymn0116@gmail.com>
2025-09-01 20:54:32 +0900

    332

--- base_keymoon/task332.py
+++ base_keymoon/task332.py
@@ -1,4 +1,4 @@
-# best: 58(luke) / others: 64(mukundan), 64(nauti), 65(4atj), 65(sisyphus), 67(kg583)
+# best: 58(4atj sisyphus luke Seek, jailctf merger) / others: 61(xsot ovs att joking mewheni), 64(mukundan), 64(nauti), 67(kg583), 67(duckyluuk)
 # lambda g:[(a:=[1,.6])and[c*(a:=a[::-1])[0]for c in r[::-1]][::-1]for r in g]
 # lambda g:[[c*[1,.6][i+len(r)&1]for i,c in enumerate(r)]for r in g]
 # lambda g:[(a:=5)and[(0<c)*(a:=a^6)for c in r[::-1]][::-1]for r in g]
@@ -6,4 +6,21 @@
 # lambda g:[(a:=.6+.8*len(r)%1.6)and[c*(a:=1.6-a)for c in r]for r in g]
 # lambda g:[[v*.6**(len(s)-i&1)for i,v in enumerate(s)]for s in g]
 # ========================== 58 ==========================
-p=lambda g:[eval(".6**(len(r)%2)*r.pop(0),"*len(r))for r in g]
+p=lambda g:[eval("7**len(r)*r.pop(0)%8"*len(r))for r in g]
+
+# b*7**a%8
+# b*7**a%8
+# mappings = {
+#     (0,0): 0,
+#     (1,0): 0,
+#     (2,0): 0,
+#     (3,0): 0,
+#     (4,0): 0,
+#     (5,0): 0,
+#     (0,5): 5,
+#     (2,5): 5,
+#     (4,5): 5,
+#     (1,5): 3,
+#     (3,5): 3,
+#     (5,5): 3,
+# }
