db1c6d831440bd4e71074835c4fd90d92fcedd2b
key-moon <kymn0116@gmail.com>
2025-09-03 05:02:12 +0900

    168 gc

--- base_keymoon/task168.py
+++ base_keymoon/task168.py
@@ -1,7 +1,9 @@
 # best: 115(jailctf merger) / others: 116(4atj sisyphus luke Seek), 116(sisyphus), 117(mukundan), 117(xsot ovs att joking mewheni), 117(joking)
 # ====================================================== 115 ======================================================
 # port re;p=lambda g:eval(r'(g:=[*zip(*eval(re.sub(r"(([1-9]), \2.{31}\2.{25}(\2.{28})*)0",r"\1\2",str(g)))[::-1])]),'*32)[-1]
-# import re;p=lambda g:eval(r'(g:=[*zip(*eval(re.sub(r"(([1-9]), \2.{31}\2.{25}(\2.{28})*)0",r"\1\2",str(g)))[::-1])]),'*32)[-1]
+# port re;p=lambda g:eval(r'(g:=[*zip(*eval(re.sub(r"(([1-9]), \2.{31}\2.{25}(\2.{28})*)0",r"\1\2",str(g)))[::-1])]),'*32)[-1]
 # port re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub(r"0(?=(.{35})+, ([^0]).{28}(\2, ){2})",r"\2",str(p(g,c+1)))))][::-1]
 # port re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub(r"0(?=(.{35})+, ([^0]).{28}(\2, ){2})",r"\2",str(p(g,c+1)))))][::-1]
 import re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub(r"0(?=(.{35})+(, ([^0])).{26}\2\2)",r"\3",str(p(g,c+1)))))][::-1]
+
+# port re;# p=lambda g:eval(r'(g:=eval(re.sub(r"(([1-9]), \2.{31}\2.{25}(\2.{28})*)0",r"\1 \2",str(g)))),')[0]
--- base_yu/task168.py
+++
@@ -1,7 +0,0 @@
-# best: 115(jailctf merger) / others: 116(4atj sisyphus luke Seek), 116(sisyphus), 117(xsot ovs att joking mewheni), 117(mukundan), 117(joking)
-# ====================================================== 115 ======================================================
-import re
-p=lambda g:eval(r'(g:=[*zip(*eval(re.sub(r"(([1-9]), \2.{31}\2.{25}(\2.{28})*)0",r"\1\2",str(g)))[::-1])]),'*32)[-1]
-
-# import re
-# p=lambda g:eval(r'(g:=eval(re.sub(r"(([1-9]), \2.{31}\2.{25}(\2.{28})*)0",r"\1 \2",str(g)))),')[0]
