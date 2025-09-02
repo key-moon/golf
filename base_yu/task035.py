ee838350d551393481f1a25bddc0ac2a3bd61015
key-moon <kymn0116@gmail.com>
2025-09-03 05:10:46 +0900

    35

--- base_yu/task035.py
+++ base_yu/task035.py
@@ -1,11 +1,11 @@
 # best: 88(mukundan) / others: 90(4atj sisyphus luke Seek), 90(4atj), 94(kabutack), 96(joking+MWI), 96(joking/MWI)
 # ========================================= 88 =========================================
-# import re
-# p=lambda g,c=-3:c*g or p([*zip(*eval(re.sub("([1-9])((, 0)+, )8","\\1\\2\\1",str(g)))[::-1])],c+1)
+# lambda g,c=-3:c*g or p([(l:=s[0],1)and[[v,l or v][v==8 and ((l:=0) or 1)]for v in s]for s in zip(*g[::-1])],c+1)
+# lambda g,c=-3:c*g or p([(l:=0)or[[v,(1-l)*s[0]or v][l:=l or v==8]for v in s]for s in zip(*g[::-1])],c+1)
+# lambda g,c=-3:c*g or p([(l:=1)and[[l*s[0]or v,v][l:=l and v!=8]for v in s]for s in zip(*g[::-1])],c+1)
+# lambda g,c=-3:c*g or p([(l:=1)and[[l*s[0]or v,v][l:=l*(v!=8)]for v in s]for s in zip(*g[::-1])],c+1)
+p=lambda g,c=-3:c*g or[[[l*s[0]or v,v][l:=l*(v!=8)]for v in s]for s in zip(*p(g,c+1)[::-1])if(l:=1)]
 
-
-# p=lambda g,c=-3:c*g or p([(l:=s[0],1)and[[v,l or v][v==8 and ((l:=0) or 1)]for v in s]for s in zip(*g[::-1])],c+1)
-# p=lambda g,c=-3:c*g or p([(l:=0)or[[v,(1-l)*s[0]or v][l:=l or v==8]for v in s]for s in zip(*g[::-1])],c+1)
-# p=lambda g,c=-3:c*g or p([(l:=1)and[[l*s[0]or v,v][l:=l and v!=8]for v in s]for s in zip(*g[::-1])],c+1)
-# p=lambda g,c=-3:c*g or p([(l:=1)and[[l*s[0]or v,v][l:=l*(v!=8)]for v in s]for s in zip(*g[::-1])],c+1)
-p=lambda g,c=-3:c*g or p([[[l*s[0]or v,v][l:=l*(v!=8)]for v in s]for s in zip(*g[::-1])if(l:=1)],c+1)
+# port re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub("([1-9])((, 0)+, )8",r"\1\2\1",str(p(g,c+1))))[::-1])]
+# port re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub("8(?=[ ,0]{4,}([1-9]))",r"\1",str(p(g,c+1))))[::-1])]
+# port re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub("8(?=, 0[^[(]*([1-9]))",r"\1",str(p(g,c+1))))[::-1])]
