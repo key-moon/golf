b32c0a46b3eb5938fbfd2dc594a917d86cf9ca24
key-moon <kymn0116@gmail.com>
2025-09-04 03:42:30 +0900

    271

--- base_keymoon/task271.py
+++ base_keymoon/task271.py
@@ -1,4 +1,4 @@
-# best: 94
+# best: 86(xsot ovs att joking mewheni) / others: 93(jailctf merger), 96(4atj sisyphus luke Seek), 102(mukundan), 105(kabutack), 115(cg)
 # 9x9グリッド 背景: 0
 # 要素が{1,8}の 3x3のグリッドが4つ
 # 8が一番少ない or 1 が一番多いグリッドを抽出
@@ -9,4 +9,13 @@
 # p=lambda g:min((sum(sum(a,[])),a)for x in range(49)if 0<min(min(a:=[r[x%7:][:3]for r in g[x//7:][:3]])))[1]
 # p=lambda g:min((sum(sum(a,[])),a)for x in range(49)if 0<min(min(a:=[r[x%7:][:3]for r in g[x//7:][:3]])))[1]
 # ============================================================================================
-p=lambda g:min((sum(sum(a,[])),a)for x in range(49)if 0<min(min(a:=[r[x%7:][:3]for r in g[x//7:][:3]])))[1]
+
+# lambda g:min((sum(sum(a,[])),a)for x in range(49)if 0<min(min(a:=[r[x%7:][:3]for r in g[x//7:][:3]])))[1]
+# lambda g:len(g)==3and(sum(a:=sum(g,()))+0**min(a)*99,g)or min([p([*zip(*g[i:i+3])])for i in range(7)])[1]
+# lambda g:min((sum(b),a)for x in range(49)if 0<min(b:=sum(a:=[r[x%7:][:3]for r in g[x//7:][:3]],[])))[1]
+# lambda g:min((sum(b:=sum(a:=[r[x%7:][:3]for r in g[x//7:][:3]],[]))+(0in b)*99,a)for x in range(49))[1]
+p=lambda g:g*(len(g)<4)or min((sum(a:=sum(b:=p([*zip(*g[i:i+3])]),()))+(0in a)*99,b)for i in range(7))[1]
+
+# ↓keyをlambdaでごまかし系列 カス
+# lambda g:(sum(b:=sum(g,[]))+(0in b)*99)*(len(g)<4)or min(([r[x%7:][:3]for r in g[x//7:][:3]]for x in range(49)),key=p)[1]
+# lambda g:min(([r[x%7:][:3]for r in g[x//7:][:3]]for x in range(49)),key=lambda g:sum(b:=sum(g,[]))+(0in b)*99)[1]
