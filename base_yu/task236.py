b8a84f3ef58a765b954d14ea90956b33bfad820f
Yu212 <yu212.mc@gmail.com>
2025-09-04 03:31:47 +0900

    236

--- base_yu/task236.py
+++ base_yu/task236.py
@@ -1,8 +1,9 @@
-# best: 60(4atj) / others: 61(luke), 62(mukundan), 62(Seek64), 62(sisyphus), 63(kg583)
+# best: 54(jailctf merger) / others: 60(4atj sisyphus luke Seek), 62(xsot ovs att joking mewheni), 62(mukundan), 63(kg583), 65(natte)
+# ======================== 54 ========================
 # lambda g:[[sum(a)*1.5%2//1for a in zip(*c)]for c in zip(g,g[5:])]
-# =========================== 60 ===========================
 # p=lambda g:[[3*(x^y//2)for x,y in zip(*c)]for c in zip(g,g[5:])]
-p=lambda g:eval("[3*(x^y//2)for x,y in zip(g.pop(0),g[4])],"*4)
+# p=lambda g:eval("[3*(x^y//2)for x,y in zip(g.pop(0),g[4])],"*4)
+p=lambda g:eval("[x*3^-y%5for x,y in zip(g.pop(0),g[4])],"*4)
 
 # 00 0
 # 01 3
