692501bf542aceb76e09d9c966f4cd50db6a18c6
key-moon <kymn0116@gmail.com>
2025-09-04 00:08:12 +0900

    49

--- base_keymoon/task049.py
+++ base_keymoon/task049.py
@@ -1,3 +1,4 @@
+# best: 81(4atj sisyphus luke Seek, jailctf merger) / others: 82(mukundan), 86(kg583), 86(kabutack), 87(xsot ovs att joking mewheni), 91(Bulmenisaurus)
 # p=lambda g:eval(min([str(a) for i in range(10) if 1==len(set(a:=[[c for c in r if c==i]for r in g if i in r]))],key=len))
 # p=lambda g:eval(min([str([[i]*r.count(i)for r in g if i in r])for i in range(1,10) if str(i)in str(g)],key=len))
 # p=lambda g:eval(min([str([[i]*r.count(i)for r in g if i in r]or"1,"*999)for i in range(1,10)],key=len))
@@ -11,5 +12,7 @@
 # range(1,10)
 # [i]*r.count(i)
 # [i for c in r if i==c]
-# =============================== best: 81 by 4atj ==============================
-p=lambda g:eval(min([str([a for r in g if(a:=[i]*r.count(i))])for(i)in sum(g,[])if i],key=len))
+# ====================================== 81 =====================================
+# lambda g:[R for r in g if(R:=[v for v in r if v==min(u:=[0]*99+sum(g,[]),key=u.count)])]
+# lambda g:[R for r in g if(R:=r.count(v:=min(u:=[0]*99+sum(g,[]),key=u.count))*[v])]
+p=lambda g:[c*[v]for r in g if(c:=r.count(v:=min(u:=sum(g,[0]*99),key=u.count)))]
