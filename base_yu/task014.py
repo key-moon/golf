8d20a8e81e19dd550af248cba2f4be8626c3a1a1
key-moon <kymn0116@gmail.com>
2025-09-03 20:37:24 +0900

    banner

--- base_yu/task014.py
+++ base_yu/task014.py
@@ -1,5 +1,8 @@
+# best: 70(4atj sisyphus luke Seek) / others: 73(jailctf merger), 74(mukundan), 76(xsot ovs att joking mewheni), 76(natte), 94(kabutack)
+# ================================ 70 ================================
 # 34567890123456789012345678901234567890123456789012345678901234567890
 # p=lambda g:[[v for v,t in zip(s,zip(*g))if len({*t})>2]for s in g if len({*s})>2]
 # lambda g:[[v for*t,v in zip(*g,s)if len({*t})>2]for s in g if len({*s})>2]
 # lambda g:[[t[0]for t in zip(s,*g)if len({*t})>2]for s in g if len({*s})>2]
 p=lambda g:[R for s in g if(R:=[t[0]for t in zip(s,*g)if len({*t}&{*s})>2])]
+
--- base_yu/task167.py
+++ base_yu/task167.py
@@ -1,19 +1,17 @@
-# best: 77(luke, 4atj, luke/sisyphus/Seek, Seek64) / others: 79(sisyphus), 80(mukundan), 85(nauti), 85(joking+MWI), 85(joking/MWI)
-# ==================================== 77 ===================================
-# p=lambda g:[[(c:=len({*sum(g,[])}))<3,c<2,c!=2],[0,c>1,0],[c>2,0,c==2]]
-# p=lambda g:[[((c:=len({*sum(g,[])}))<3)*5,(c<2)*5,(c!=2)*5],[0,(c>1)*5,0],[(c>2)*5,0,(c==2)*5]]
-# p=lambda g:[[((c:=len({*sum(g,[])}))<3)*5,(c<2)*5,c%2*5],[0,1%c*5,0],[4%c*5,0,3%c*5]]
+# best: 72(4atj sisyphus luke Seek) / others: 76(jailctf merger), 80(mukundan), 85(xsot ovs att joking mewheni), 85(Yuchen20), 85(kabutack)
+# ================================= 72 =================================
+# lambda g:[[(c:=len({*sum(g,[])}))<3,c<2,c!=2],[0,c>1,0],[c>2,0,c==2]]
+# lambda g:[[((c:=len({*sum(g,[])}))<3)*5,(c<2)*5,(c!=2)*5],[0,(c>1)*5,0],[(c>2)*5,0,(c==2)*5]]
+# lambda g:[[((c:=len({*sum(g,[])}))<3)*5,(c<2)*5,c%2*5],[0,1%c*5,0],[4%c*5,0,3%c*5]]
 
-# p=lambda g:[[(v>>len({*sum(g,[])})&1)*5for v in s]for s in[[6,2,10],[0,12,0],[8,0,4]]]
-p=lambda g:[[(v>>len({*sum(g,[])})&1)*5for v in b""[i-3:i]]for i in(3,6,9)]
-# p=lambda g:[[(v>>len({*sum(g,[])})&1)*5for v in b"000000000"[i-3:i]]for i in(3,6,9)]
-# p=lambda g:[[(17314875942>>i+j+len({*sum(g,[])})&1)*5for j in(0,4,8)]for i in(0,12,24)]
-# p=lambda g:[[(69259503768>>i+j+len({*sum(g,[])})&1)*5for j in(0,4,8)]for i in b"000"]
-# p=lambda g:[[(69259503768>>i+j+len({*sum(g,[])})&1)*5for j in(0,4,8)]for i in b""]
+# lambda g:[[(v>>len({*sum(g,[])})&1)*5for v in s]for s in[[6,2,10],[0,12,0],[8,0,4]]]
+# lambda g:[[(v>>len({*sum(g,[])})&1)*5for v in b"000000000"[i-3:i]]for i in(3,6,9)]
+# lambda g:[[(17314875942>>i+j+len({*sum(g,[])})&1)*5for j in(0,4,8)]for i in(0,12,24)]
+# lambda g:[[(69259503768>>i+j+len({*sum(g,[])})&1)*5for j in(0,4,8)]for i in b"000"]
+# lambda g:[[(69259503768>>i+j+len({*sum(g,[])})&1)*5for j in(0,4,8)]for i in b""]
+# lambda g:[[(v>>len({*sum(g,[])})&1)*5for v in b"         "[i-3:i]]for i in(3,6,9)]
+p=lambda g:[[(v>>len({*sum(g,[])})&1)*5for v in b"\x06\x02\x0b\x01\x0c\x01\x08\x01\x04"[i-3:i]]for i in(3,6,9)]
 
-
-
-# [6,2,10,0,12,0,8,0,4]
 
 
 # oox 6
