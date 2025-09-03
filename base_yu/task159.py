df9290b1abf44c32d2a28b03c5dbe6c245a47332
Yu212 <yu212.mc@gmail.com>
2025-09-03 18:55:27 +0900

    159

---
+++ base_yu/task159.py
@@ -0,0 +1,24 @@
+# best: 139(4atj sisyphus luke Seek) / others: 149(natte), 151(jailctf merger), 152(mukundan), 185(xsot ovs att joking mewheni), 188(kg583)
+# ================================================================== 139 ==================================================================
+
+# def p(g,k=0,u=[[2]*99]):
+#  g=g*(k>0)or p(g,k:=sum(g,[]).count(2)//12)
+#  return[*zip(*u+sum([k*[s]for s in g if{*s}-{0,2}],[])+u)]
+
+def p(g,k=0,u=[[2]*99]):
+ if k<1:
+  g=p(g,k:=str(g).count("2")//12)
+ return[*zip(*u+sum([k*[s]for s in g if{*s}-{0,2}],[])+u)]
+
+# def p(g):
+#  k=sum(g,[]).count(2)//12
+#  u=[[2]*99]
+#  t=[*zip(*u+sum([k*[s]for s in g if{*s}-{0,2}],[])+u)]
+#  t=[*zip(*u+sum([k*[s]for s in t if{*s}-{0,2}],[])+u)]
+#  return t
+
+# def p(g):
+#  k=sum(g,[]).count(2)//12
+#  t=[*zip(*[s for s in g if {*s}-{0,2}])]
+#  t=[*zip(*[s for s in t if {*s}-{0,2}])]
+#  return [[2]*(k*3+2)]+sum([[[2]+sum([[v]*k for v in s],[])+[2]]*k for s in t],[])+[[2]*(k*3+2)]
