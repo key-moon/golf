18afc06f8d5414d685e9f8b38a31cb3cd30ac8c0
Yu212 <yu212.mc@gmail.com>
2025-09-04 00:24:17 +0900

    281

---
+++ base_yu/task281.py
@@ -0,0 +1,33 @@
+# best: 146(xsot ovs att joking mewheni) / others: 152(natte), 169(4atj sisyphus luke Seek), 169(jailctf merger), 234(cg), 241(nauti)
+# ===================================================================== 146 ======================================================================
+# p=lambda g,c=-3:c*g or p([*zip(*(g*({*g[x:=(t:=[i for i,s in enumerate(g)if any(s)])[0]]}!={0,8})or g[:x]+[g[y:=t[1]]]+[g[y+1]]*(y-x)+g[y+1:])[::-1])],c+1)
+
+
+def p(g,c=-3):
+  x,y,*_=[i for i,s in enumerate(g)if any(s)]
+  return c*g or p([*zip(*(g*({*g[x]}!={0,8})or g[:x]+[g[y]]+[g[y+1]]*(y-x)+g[y+1:])[::-1])],c+1)
+
+# def p(g,c=-3):
+#   x,y,*_=[i for i,s in enumerate(g)if any(s)]
+#   if{*g[x]}=={0,8}:
+#    g=g[:x]+[g[y]]+[g[y+1]]*(y-x)+g[y+1:]
+#   return c*g or p([*zip(*g[::-1])],c+1)
+
+# def p(g):
+#  for _ in range(4):
+#   x,y,*_=[i for i,s in enumerate(g)if any(s)]
+#   if{*g[x]}=={0,8}:
+#    g=g[:x]+[g[y]]+[g[y+1]]*(y-x)+g[y+1:]
+#   g=[*zip(*g[::-1])]
+#  return g
+
+
+# def p(g):
+#  for _ in range(4):
+#   u=max(g,key=any)
+#   if{*u}=={0,8}:
+#    x=g.index(u)
+#    y=g.index(max(g[x+1:],key=any))
+#    g=g[:x]+[g[y]]+[g[y+1]]*(y-x)+g[y+1:]
+#   g=[*zip(*g[::-1])]
+#  return g
