0def2a8f8d9be68262b2bdb2dddfc33a6c833c7f
key-moon <kymn0116@gmail.com>
2025-09-03 12:55:05 +0900

    344

--- base_keymoon/task344.py
+++ base_keymoon/task344.py
@@ -2,4 +2,4 @@
 # ==================================== 78 ====================================
 # p=lambda g,c=4:c and p([[y or(x==1)for x,y in zip((0,)+s,s)]for s in zip(*g[::-1])],c-1)or g
 # import re;p=lambda g,c=4:c and p([*zip(*eval(re.sub("3, 2","8,0",str(g)))[::-1])],c-1)or g
-p=lambda g,c=-3:c*g or p([*zip(*eval(str(g).replace("3, 2","8,0"))[::-1])],c+1)
+p=lambda g,c=-3:c*g or[*zip(*eval(str(p(g,c+1)).replace("3, 2","8,0"))[::-1])]
