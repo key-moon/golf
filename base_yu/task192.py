5d6925df16faef158af7485aa658a5d8623a00cb
Yu212 <yu212.mc@gmail.com>
2025-09-04 16:26:48 +0900

    192

--- base_yu/task192.py
+++ base_yu/task192.py
@@ -1,15 +1,35 @@
-def p(g):
- c,b,a=sorted({*sum(g,[])},key=sum(g,[]).count)
- for i in range(len(g)):
-  for j in range(len(g[0])):
-   if g[i][j]==c:
-    u=0
-    for k in range(9):
-     if len(g)>(y:=i+k//3-1)>-1<(x:=j+k%3-1)<len(g[0]) and g[y][x]!=a:
-      u+=k%2+1
-      u|=(k%6==1)*16|(k in(3,5))*32
-    g[i][j]=[a,b][u>52]
- return g
+# best: 111(jailctf merger) / others: 117(4atj sisyphus luke Seek), 120(xsot ovs att joking mewheni), 154(mukundan), 197(natte), 199(MasukenSamba)
+# ==================================================== 111 ====================================================
+
+# p=lambda g,c=-7:c*g or (d:=min(u:=sum(g,g[0]),key=u.count))and p([*zip(*[(l:=0)or[((x+(d==x>0<l==y)*(l-x))*(c<0 or x!=d),l:=x)[0]for x,y in zip(s,t)]for s,t in zip(g,g[:1]+g)][::-1])],c+1)
+# p=lambda g,c=-7:c*g or (d:=min(u:=sum(g,g[0]),key=u.count))and p([*zip(*[(l:=0)or[((c<0 or x!=d)*(x+(d==x>0<l==y)*(l-x)),l:=x)[0]for x,y in zip(s,t)]for s,t in zip(g,g[:1]+g)][::-1])],c+1)
+# p=lambda g,c=-7:c*g or (d:=min(u:=sum(g,g[0]),key=u.count))and p([*zip(*[(l:=0)or[((d==x>0<l==y)*(l-x)+(l:=x))*(c<0 or x!=d)for x,y in zip(s,t)]for s,t in zip(g,g[:1]+g)][::-1])],c+1)
+
+# p=lambda g,c=-7:c*g or (d:=min(u:=sum(g,g[0]),key=u.count))and p([*zip(*[(l:=0)or[((x+(0<l==y!=x>0)*(l-x))*(c<0 or x!=d),l:=x)[0]for x,y in zip(s,t)]for s,t in zip(g,g[:1]+g)][::-1])],c+1)
+# p=lambda g,c=-7:c*g or (d:=min(u:=sum(g,g[0]),key=u.count))and p([*zip(*[(l:=0)or[((x+(0<l==y!=x>0)*(l-x))*(c<0 or x!=d),l:=(x==y)*x)[0]for x,y in zip(s,t)]for s,t in zip(g,g[:1]+g)][::-1])],c+1)
+# p=lambda g,c=-7:c*g or p([*zip(*[(l:=0)or[(((0<l==t[i]!=s[i]>0)*l or s[i])*(c+1<0 or max([0,*s,0][i:i+3:2])>0),l:=(s[i]==t[i])*s[i])[0]for i in range(len(s))]for s,t in zip(g,g[:1]+g)][::-1])],c+1)
+p=lambda g,c=-7:c*g or p([*zip(*[[((s[i]>0<t[i-1]==t[i])*t[i]or s[i])*(c<-1 or max([0,*s,0][i:i+3:2])>0)for i in range(len(s))]for s,t in zip(g,g[:1]+g)][::-1])],c+1)
+
+# import re
+# def p(g):
+#  c,b,a=sorted({*sum(g,[])},key=sum(g,[]).count)
+#  for _ in range(4):
+#   g=eval(re.sub(f"{c}(?=, {b}.{{{len(g)*3-2}}}{b})",str(b),str([*zip(*g[::-1])])))
+#  g=eval(str(g).replace(str(c),"0"))
+#  return g
+
+# def p(g):
+#  c,b,a=sorted({*sum(g,[])},key=sum(g,[]).count)
+#  for i in range(len(g)):
+#   for j in range(len(g[0])):
+#    if g[i][j]==c:
+#     u=0
+#     for k in range(9):
+#      if len(g)>(y:=i+k//3-1)>-1<(x:=j+k%3-1)<len(g[0]) and g[y][x]!=a:
+#       u+=k%2+1
+#       u|=(k%6==1)*16|(k in(3,5))*32
+#     g[i][j]=[a,b][u>52]
+#  return g
 
 
 # no compress
