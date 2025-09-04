e7ae24db2e9c275ecbedc6e50abff7ea1efcc3cc
Yu212 <yu212.mc@gmail.com>
2025-09-04 17:22:34 +0900

    266

--- base_keymoon/task266.py
+++ base_keymoon/task266.py
@@ -6,7 +6,11 @@
 # lambda g,a=[0]*3:[r[4-max(g).index(2):][:5]for r in[a*3,a+[3,0,6]+a,a*3,a+[8,0,7]+a,a*3][2-g.index(max(g)):][:3]]
 # f p(g,a=[0]*3):m=max(g);return[r[4-m.index(2):][:5]for r in[a*3,a+[3,0,6]+a,a*3,a+[8,0,7]+a,a*3][2-g.index(m):][:3]]
 # lambda g,a=[0]*3:[r[4-max(g).index(2):][:5]for r in[a*3,a+[3,0,6]+a,a*3,a+[8,0,7]+a,a*3][2-g.index(max(g)):][:3]]
-p=lambda g,A=(a:=[0]*17)+[3,0,6]+a+[8,0,7]+a:[A[i-(a:=sum(g,[]).index(2))-a//5*5:][:5]for i in b"\x1c\x26\x30"]
+# p=lambda g,A=(a:=[0]*17)+[3,0,6]+a+[8,0,7]+a:[A[i-(a:=sum(g,[]).index(2))-a//5*5:][:5]for i in b"\x1c\x26\x30"]
+# p=lambda g,A=(a:=[0]*17)+[3,0,6]+a+[8,0,7]+a:[A[i-(a:=sum(g,[]).index(2))*2+a%5:][:5]for i in b"\x1c\x26\x30"]
+a=[0]*17
+A=a+[3,0,6]+a+[8,0,7]+a
+p=lambda g:[A[i-(a:=sum(g,[]).index(2))*2+a%5:][:5]for i in b"\x1c\x26\x30"]
 
 # mapping = { 0: 3, 1: 6, 2: 7, 3: 8, 4: 0 }
 # mapping = { 0: 0, 1: 8, 2: 7, 3: 6, 4: 3 } a*8%29%9
