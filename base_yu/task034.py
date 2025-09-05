9e31223f42a5000cee8f59a734c03a765f93d793
Yu212 <yu212.mc@gmail.com>
2025-09-05 16:04:25 +0900

    034

--- base_yu/task034.py
+++ base_yu/task034.py
@@ -4,4 +4,4 @@
 def p(g):
  u=sum(g,[])
  Y,X=divmod(u.index(max(u,key=bool)),9)
- return[[sum({*sum(g,[])}-{2})*(v>0 or g[Y+(r>Y)][X+(c>X)]==2>min(abs(r-c-Y+X),abs(r+c-Y-X-1)))for c,v in enumerate(R)]for r,R in enumerate(g)]
+ return[[sum({*u}-{2})*(v>0 or g[Y+(r>Y)][X+(c>X)]==2>min(abs(r-c-Y+X),abs(r+c-Y-X-1)))for c,v in enumerate(R)]for r,R in enumerate(g)]
