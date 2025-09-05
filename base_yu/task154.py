653950fefb7307beb4ec3d5d3373ad67c5bfb331
key-moon <kymn0116@gmail.com>
2025-09-05 16:37:26 +0900

    154

--- base_yu/task154.py
+++ base_yu/task154.py
@@ -1,6 +1,6 @@
 # best: 100(mukundan, 4atj sisyphus luke Seek, jailctf merger) / others: 133(xsot ovs att joking mewheni), 180(MasukenSamba), 227(nauti), 230(Bulmenisaurus), 244(cg)
 # ============================================== 100 ===============================================
-p=lambda g,c=-29,k=3:c*g or exec(g[i:=c%15].count(2)//5*2*"g[i-k],g[i+k]=g[i+k],g[i-k];k=2;")or p([*zip(*g)],c+1)
+p=lambda g,c=-29,k=3:c*g or p(exec(g[i:=c%15].count(2)//5*2*"g[i-k],g[i+k]=g[i+k],g[i-k];k=2;")or[*zip(*g)],c+1)
 
 # def p(g):
 #  for i in range(30):
