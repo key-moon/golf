0824b7e5c2502dbefe6df1ad51a39440a66070f2
Yu212 <yu212.mc@gmail.com>
2025-09-05 15:32:41 +0900

    390

--- base_yu/task390.py
+++ base_yu/task390.py
@@ -1,9 +1,21 @@
+# best: 100(mukundan, 4atj sisyphus luke Seek, jailctf merger) / others: 122(xsot ovs att joking mewheni), 129(biz), 147(jacekwl), 147(jacekw), 162(MasukenSamba)
+# ============================================== 100 ===============================================
+
 def p(g):
  for _ in 0,1:
   for i in range(15):
-   if g[i].count(2)>4:g[i+2],g[i-2],g[i+3],g[i-3]=g[i-2],g[i+2],g[i-3],g[i+3]
+   if g[i].count(2)>4:
+    # g[i-3:i+4]=[g[i+k-4]for k in(7,6,3,4,5,2,1)]
+    g[i-3:i+4]=[g[i+k-4]for k in b""]
   *g,=zip(*g)
  return g
+
+# def p(g):
+#  for _ in 0,1:
+#   for i in range(15):
+#    if g[i].count(2)>4:g[i+2],g[i-2],g[i+3],g[i-3]=g[i-2],g[i+2],g[i-3],g[i+3]
+#   *g,=zip(*g)
+#  return g
 
 
 # def p(g):
