25d06786bd0c2a11491f3a2d0f5e444dd7095d5d
kq5y <contact@kq5.jp>
2025-09-05 17:45:16 +0900

    337

--- base_yu/task337.py
+++ base_yu/task337.py
@@ -1,3 +1,6 @@
+# best: 46(4atj sisyphus luke Seek, rucin93, jailctf merger, xsot ovs att joking mewheni, mukundan) / others: 48(natte), 48(HETHAT), 48(HashPanda), 48(MasukenSamba), 48(kabutack)
+# ==================== 46 ====================
 # 345678901234567890123456789012345678901234567890
 # p=lambda g:[[v^13*(v in(5,8))for v in s]for s in g]
-p=lambda g:[[v^13*(v>v%3>1)for v in s]for s in g]
+# p=lambda g:[[v^13*(v>v%3>1)for v in s]for s in g]
+p=lambda g:eval(str(g).translate({53:56,56:53}))
