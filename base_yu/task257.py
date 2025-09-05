b66c0bc4f06a362bc8566ddbd701f733666b8e8e
kq5y <contact@kq5.jp>
2025-09-05 23:45:56 +0900

    257

--- base_yu/task257.py
+++ base_yu/task257.py
@@ -1,13 +1,13 @@
 # 類題:180
-# best: 81(luke) / others: 82(mukundan), 82(sisyphus), 83(kabutack), 84(natte), 84(att)
-# ====================================== 81 =====================================
+# best: 75(jailctf merger, xsot ovs att joking mewheni) / others: 81(4atj sisyphus luke Seek), 82(mukundan), 83(kabutack), 84(natte), 89(jacekw)
+# =================================== 75 ==================================
 # lambda g:[[g[i][j]or g[i][j+5]or g[i+5][j]or g[i+5][j+5] for j in range(4)]for i in range(4)]
 # lambda g,R=range(4):[[g[i][j]or g[i][j+5]or g[i+5][j]or g[i+5][j+5] for j in R]for i in R]
 # lambda g:[[s[j]or s[j+5]or t[j]or t[j+5] for j in range(4)]for s,t in zip(g,g[5:])]
 # lambda g:[[s[j]or s[j+5]or t[j]or t[j+5] for j in(0,1,2,3)]for s,t in zip(g,g[5:])]
 # lambda g:[[s[j-5]or s[j]or t[j-5]or t[j]for j in b""]for s,t in zip(g,g[5:])]
-p=lambda g:[[max(s[j::5])or max(t[j::5])for j in(0,1,2,3)]for s,t in zip(g,g[5:])]
-
+#p=lambda g:[[max(s[j::5])or max(t[j::5])for j in(0,1,2,3)]for s,t in zip(g,g[5:])]
+p=lambda g,r=range(4):[[max(g[i][j::5])or max(g[i+5][j::5])for j in r]for i in r]
 #  b"\0123"
 # (0,1,2,3)
 
