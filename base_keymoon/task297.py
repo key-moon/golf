d3ecb0f198a3a9c1feaaf198ede709705a39ac7d
kq5y <contact@kq5.jp>
2025-09-07 00:57:23 +0900

    297

--- base_keymoon/task297.py
+++ base_keymoon/task297.py
@@ -1,5 +1,8 @@
-# best: 43(4atj sisyphus luke Seek, jailctf merger) / others: 44(xsot ovs att joking mewheni), 44(MasukenSamba), 47(kg583), 47(mukundan), 47(cg)
+# best: 43(4atj sisyphus luke Seek, jailctf merger, 4atj sisyphus luke Seek mukundan) / others: 44(xsot ovs att joking mewheni), 44(MasukenSamba), 47(Potatoman), 47(jacekwl Potatoman), 47(Yuchen20)
 # lambda g:g[:2]+[*zip(*[2*(a:=g[0])]*len(a))]
 # lambda g:g[:2]+[*zip(*[g[0]*2]*len(g[0]))]
 # =================== 43 ==================
-p=lambda g:g[:2]+[*zip(*[g[0]*2]*len(g[0]))]
+#p=lambda g:g[:2]+[*zip(*[g[0]*2]*len(g[0]))]
+#p=lambda g:g[:2]+[*zip(*[g[0]]*len(g[0]))]*2
+#p=lambda g:g[:2]+[*zip(*[a:=g[0]]*len(a))]*2
+p=lambda g:g[:2]+[*zip(*g[:1]*len(g[0]))]*2
