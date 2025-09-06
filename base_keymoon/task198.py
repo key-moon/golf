c4e34e398680558388b404d3382c06e3c049beec
key-moon <kymn0116@gmail.com>
2025-09-07 01:49:24 +0900

    198

--- base_keymoon/task198.py
+++ base_keymoon/task198.py
@@ -1,23 +1,7 @@
-# best: 127 信じられねえ
-# 連結成分は 4、無は3
-# マスのサイズは3か4
-# (偶然) max(g[5]) で仕切りを検出できる 冷静になると max(g)[0] でもできる
-# 穴は2ます以上の場合もある 縦に3ます穴が空いている場合もある（ケース148）
-
-# regex golfの残骸（コーナーケースに対処できずに伸びに伸びたゴミ、勝ち方針はregexではないと思う）
-import re;s=re.sub
-def p(g):
- a="."*len(g)
- d=a+"]"+s("[[ ,]","",str(g))+a
- v=f"[].{max(g)[0]}]"
- for x in d:
-  d=s(f"0(?=({a})?4)|(?<={v+a}{v+a})0(?=({a}0)?({a}0)?{a+v})|(?<={v+a})0(?=({a}0)?{a+v})|(?<={v})0(?=0?{v})","4",d)[::-1]
- return[[*map(int,r)]for r in s("0","3",d).split("]")[1:-2]]
-
-# def p(g):
-#  h=len(g);r=range(h-1);a=max(g)[0]
-#  for _ in g[1:]:
-#   [for c in r]for 
-#   g=eval(str(g)[::-1])
- 
-#  return eval(str(g).replace("0","3"))
+# best: 122(4atj sisyphus luke Seek mukundan, xsot ovs att joking mewheni, 4atj sisyphus luke Seek, jailctf merger) / others: 127(mukundan), 239(MasukenSamba), 256(kdmitrie), 256(J&R), 259(JRK)
+# ========================================================= 122 ==========================================================
+# port re;S=re.sub;p=lambda g,c=-63:eval(S(*"03",str(g)))*c or p(eval(S("0(?=([^(]+[^(04]{9}|, 4))","4",str([*zip(*g)][::-1]))),c+1)
+# port re;p=lambda g,c=-63:g*c or[*zip(*eval(re.sub(*["0","3(?=([^[(]+[^[(34]{9}|, 4))","3","4"][c%2::2],str(p(g,c+1)))))][::-1]
+# port re;p=lambda g,c=-63:g*c or[*zip(*eval(re.sub(*["0","3(?=([^[(]+[^[(34]{9}|, 4))","3","4"][c%2::2],str(p(g,c+1)))))][::-1]
+# port re;S=re.sub;p=lambda g,c=-63:S(*"03",str(g))*c or[*zip(*eval(S("3(?=([^[(]+[^[(34]{9}|, 4))","4",str(p(g,c+1)))))][::-1]
+import re;S=re.sub;p=lambda g,c=-63:g*c or[*zip(*eval(S("3(?=([^[(]+[^[(34]{9}|, 4))","4",S(*"03",str(p(g,c+1))))))][::-1]
