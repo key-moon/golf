b98882cfc1bed80f43d6a5f6691ae69778d7fdd6
key-moon <kymn0116@gmail.com>
2025-09-07 02:07:09 +0900

    222

--- base_yu/task222.py
+++ base_yu/task222.py
@@ -1,9 +1,14 @@
 # best: 106(xsot ovs att joking mewheni) / others: 107(jailctf merger), 114(4atj sisyphus luke Seek mukundan), 114(4atj sisyphus luke Seek), 144(natte), 171(mukundan)
 # ================================================= 106 ==================================================
 
-# p=lambda g,c=-5:c*g or[*zip(*[[(s[i]in[0,*s,0][i:i+3:2] and (hash((*g[0],))>>53 not in (-33,617,325) or s[i]%7==2))*s[i] for i in range(16)]for s in p(g,c+1)])]
-# p=lambda g,c=-5:c*g or p([*zip(*[[(s[i]in[0,*s,0][i:i+3:2]and(c<0 or len({*g})==2 or s[i]in(2,9)))*s[i] for i in range(16)]for s in g])],c+1)
-p=lambda g,c=-5:c*g or p([*zip(*[[(v in[0,*s,0][i:i+3:2]and(c<0 or len({*g})==2 or v in(2,9)))*v for i,v in enumerate(s)]for s in g])],c+1)
+# lambda g,c=-5:c*g or[*zip(*[[(s[i]in[0,*s,0][i:i+3:2] and (hash((*g[0],))>>53 not in (-33,617,325) or s[i]%7==2))*s[i] for i in range(16)]for s in p(g,c+1)])]
+# lambda g,c=-5:c*g or p([*zip(*[[(s[i]in[0,*s,0][i:i+3:2]and(c<0 or len({*g})==2 or s[i]in(2,9)))*s[i] for i in range(16)]for s in g])],c+1)
+# lambda g,c=-5:c*g or p([*zip(*[[(v in[0,*s,0][i:i+3:2]and(c<0 or len({*g})==2 or v in(2,9)))*v for i,v in enumerate(s)]for s in g])],c+1)
+# lambda g,c=-7:c*g or p([[(s[i]in[0,*s,0][i:i+3:2]*(5<sum(g,g[0]).count(s[i])))*s[i] for i in range(16)]for s in zip(*g)],c+1)
+# lambda g,c=-7:c*g or p([[(5<sum(g,g[0]).count(v:=s[i]))*(v in[0,*s,0][i:i+3:2])*v for i in range(16)]for s in zip(*g)],c+1)
+p=lambda g,c=-7:c*g or p([[(v in[0,*s,0][i:i+3:2]*(5<sum(g,g[0]).count(v)))*v for i,v in enumerate(s)]for s in zip(*g)],c+1)
+
+
 
 # def p(g):
 #  for _ in range(4):
