c9856ca39f62465591b67f84979d2d626aa961c7
Yu212 <yu212.mc@gmail.com>
2025-09-07 01:19:28 +0900

    198

---
+++ base_yu/task198.py
@@ -0,0 +1,28 @@
+# best: 122(xsot ovs att joking mewheni, 4atj sisyphus luke Seek mukundan, 4atj sisyphus luke Seek, jailctf merger) / others: 127(mukundan), 239(MasukenSamba), 256(kdmitrie), 256(J&R), 265(Jonas)
+# ========================================================= 122 ==========================================================
+def p(g):
+ c=max(g)[0]
+ k=g.index(max(g[3:6]))+1
+ for _ in range(4):
+#   g=[[((-~i%k)*(-~j%k)<1) and (v or 4) or (((i<k or {*g[i//k*k-1][j//k*k:][:k-1]}=={c}) and v<4 and 3 or 4))for j,v in enumerate(s)]for i,s in enumerate(g)]
+#   g=[[((-~i%k)*(-~j%k)<1) and (v or 4) or (((i<k or {*g[i//k*k-1][j//k*k:][:k-1]}=={c}) and v<4 and 3 or 4))for j,v in enumerate(s)]for i,s in enumerate(g)]
+  g=[[-~i%k*(-~j%k)<1and(v or 4)or 4-({*[[],*g][i//k*k][j//k*k:][:k-1]}<={c}!=4>v)for j,v in enumerate(s)]for i,s in enumerate(g)]
+  g=[*zip(*g[::-1])]
+ return g
+
+# def p(g):
+#  c=max(g)[0]
+#  k=g.index(max(g[3:6]))
+#  k2=k+1
+#  for _ in range(4):
+#   g=[[((-~i%k2)*(-~j%k2)<1) and (v==c and c or 4) or (i<k2 and (v or 3) or ({*g[i//k2*k2-1][j//k2*k2:][:k]}=={c} and (v<4 and 3 or 4) or 4))for j,v in enumerate(s)]for i,s in enumerate(g)]
+#   g=[*map(list,zip(*g[::-1]))]
+#  return g
+
+
+# stripped:
+# A=enumerate
+# def p(g):
+#  c=max(g)[0];k=g.index(max(g[3:6]))+1
+#  for _ in range(4):g=[[-~i%k*(-~j%k)<1and(v or 4)or 4-({*[[],*g][i//k*k][j//k*k:][:k-1]}<={c}!=4>v)for(j,v)in A(s)]for(i,s)in A(g)];g=[*zip(*g[::-1])]
+#  return g
