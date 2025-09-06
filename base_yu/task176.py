4f99cb2c836d84db237237ba0398a2c5760b5e8f
Yu212 <yu212.mc@gmail.com>
2025-09-06 16:19:42 +0900

    176

--- base_yu/task176.py
+++ base_yu/task176.py
@@ -1,5 +1,5 @@
-# best: 65(jailctf merger) / others: 68(4atj sisyphus luke Seek), 74(xsot ovs att joking mewheni), 76(natte), 78(mukundan), 80(dbdr)
-# ============================== 65 =============================
+# best: 64(4atj sisyphus luke Seek mukundan, 4atj sisyphus luke Seek, jailctf merger) / others: 74(xsot ovs att joking mewheni), 76(natte), 78(mukundan), 80(dbdr), 85(Yuchen20)
+# ============================= 64 =============================
 # p=lambda g:[[v or(abs(j%12-6)+i<2 or abs((j+6)%12-6)<i)*4 for j,v in enumerate(g[i])]for i in range(3)]
 
 # p=lambda g:[(s*9)[:len(g[0])]for s in[[2,0,0,0,2,4,4,4,2,0,0,0],[4,2,0,2,0,2],[4,4,2,0,0,0,2,0,0,0,2,4]]]
@@ -12,7 +12,16 @@
 # a=[2,0,0,0,2,4,4,4,2,0,0,0]*2
 # p=lambda g:[(s*9)[:len(g[0])]for s in[a,[4,2,0,2,0,2],a[6:18]]]
 
-p=lambda g:[[v+(w>>i%12&1)*4for i,v in enumerate(s)]for s,w in zip(g,[224,65,2051])]
+# p=lambda g:[[v+(w>>i%12&1)*4for i,v in enumerate(s)]for s,w in zip(g,[224,65,2051])]
+# p=lambda g:[(u:=0)or[((u:=(u+v)%3)==d)*(4-v*2)+v for v in s]for s,d in zip(g,[1,0,0])]
+# p=lambda g,d=2:[(u:=d,d:=0)and[((u:=u+v)%3==d)*(4-v*2)+v for v in s]for s in g]
+# p=lambda g,d=2:[(u:=d,d:=0)and[(v^-((u:=u+v)%3==d))%5for v in s]for s in g]
+# p=lambda g,d=2:[([(v^-((d:=d+v)%3<1))%5for v in s],d:=0)[0]for s in g]
+p=lambda g,d=2:[[(v^-((d:=d+v)%3<1))%5for v in s]*-~(d:=0)for s in g]
+
+# 2,5,...
+# 0,3,...
+# 0,3,...
 
 # 1,0
 # 2,0
