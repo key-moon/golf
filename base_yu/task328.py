97574f646d8d168dc8b8fc9be5ba28a43d33f511
Yu212 <yu212.mc@gmail.com>
2025-09-06 23:50:39 +0900

    328

--- base_yu/task328.py
+++ base_yu/task328.py
@@ -4,4 +4,4 @@ def p(g):
  R=range(n:=len(g))
 #  return[[(u:=sorted((X+Y,-~max(X,Y)%2*v)for y,x,Y,X in((0,0,i,j),(0,-1,i,n+~j),(-1,0,n+~i,j),(-1,-1,n+~i,n+~j))if(v:=g[y][x])))and(u[0][0]<u[1][0])*u[0][1]for j in R]for i in R]
 #  return[[(u:=sorted((X+Y,-~max(X,Y)%2*v)for k in range(4)if(v:=g[-(1<k)][k%-2])))and(u[0][0]<u[1][0])*u[0][1]for j in R]for i in R]
- return[[(u:=sorted(((X:=(i,n+~i)[k//2])+(Y:=(j,n+~j)[k%2]),-~max(X,Y)%2*v)for k in range(4)if(v:=g[-(1<k)][k%-2])))and(u[0][0]<u[1][0])*u[0][1]for j in R]for i in R]
+ return[[((u:=sorted(((X:=(i,n+~i)[k//2])+(Y:=(j,n+~j)[k%2]),~max(X,Y)%2*v)for k in range(4)if(v:=g[-(1<k)][k%-2])))[0][0]<u[1][0])*u[0][1]for j in R]for i in R]
