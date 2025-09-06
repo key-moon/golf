ea679b3b14055e7c5c707897b9d6fff95e76dd28
Yu212 <yu212.mc@gmail.com>
2025-09-06 19:52:28 +0900

    273

--- base_kq5y/task273.py
+++ base_kq5y/task273.py
@@ -1,4 +1,4 @@
-# best: 116(4atj sisyphus luke Seek, xsot ovs att joking mewheni, 4atj sisyphus luke Seek mukundan) / others: 120(jailctf merger), 121(mukundan), 132(natte), 138(kdmitrie), 138(J&R)
+# best: 116(xsot ovs att joking mewheni, 4atj sisyphus luke Seek mukundan, 4atj sisyphus luke Seek) / others: 120(jailctf merger), 121(mukundan), 132(natte), 138(kdmitrie), 138(J&R)
 # ====================================================== 116 =======================================================
 
 # p=lambda g,t=[0,0]:[
@@ -14,4 +14,6 @@
 #p=lambda g,t=[0,0]:[any(c)and[t:=(0,0)if t[1]else[i for i,v in enumerate(c)if v>3],c][1]or[c,[0]*(t[0]+1)+[2]*(t[1]-t[0]-1)+[0]*(10-t[1])][any(t)]for c in g]
 #p=lambda g,t=[0,0]:[any(c)and[t:=(0,0)if t[1]else[i for i,v in enumerate(c)if v>3],c][1]or[c,[2*(t[0]<i<t[1])for i in range(10)]][any(t)]for c in g]
 #p=lambda g,t=0:[any(c)and[t:=0if t else[i for i,v in enumerate(c)if v],c][1]or([2*(t[0]<i<t[1])for i in range(10)]if t else c)for c in g]
-p=lambda g,t=[]:[sum(c)and[t:=[i for i,v in enumerate(c)if v]*(t==[]),c][1]or([2*(t[0]<i<t[1])for i in range(10)]if t else c)for c in g]
+# p=lambda g,t=[]:[sum(c)and[t:=[i for i,v in enumerate(c)if v]*(t==[]),c][1]or([2*(t[0]<i<t[1])for i in range(10)]if t else c)for c in g]
+# p=lambda g,t=[],R=range(10):[sum(c)and[t:=[i for i in R if c[i]]*(t==[]),c][1]or([2*(t[0]<i<t[1])for i in R]if t else c)for c in g]
+p=lambda g,t=[],R=range(10):[sum(c)and[t:=[i for i in R if c[i]if[]==t],c][1]or([2*(t[0]<i<t[1])for i in R]if t else c)for c in g]
