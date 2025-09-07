7ea087089c2b4eae9199123651b03f76cd43a1be
Hiroki Shibata <shibata.hiroki.753@s.kyushu-u.ac.jp>
2025-09-07 12:04:48 +0900

    118 zlib golf

---
+++ base_zatsu/task118.py
@@ -0,0 +1,10 @@
+# == begin zlib golf ==
+def	p(l):
+	v={(i,j)for	i	in	range(len(l))for	j	in	range(len(l[0]))if	l[i][j]&2};y=3
+	while	1:
+		c,k,i,j=max((sum((i+~-abs(q-1)*g,j+~-abs(q-2)*g)in	v	for	q	in	range(4)for	g	in	range(q>0,k)),-k,i,j)for	k	in	range(y,5)for	i	in	range(len(l))for	j	in	range(len(l[0])));y=-k
+		if	c<1:return	l
+		for	q	in	range(4):
+			for	g	in	range(q>0,-k):
+				if(i+~-abs(q-1)*g,j+~-abs(q-2)*g)in	v:v-={(i+~-abs(q-1)*g,j+~-abs(q-2)*g)}
+				elif	len(l)>i+~-abs(q-1)*g>-1<j+~-abs(q-2)*g<len(l[0]):l[i+~-abs(q-1)*g][j+~-abs(q-2)*g]=8
