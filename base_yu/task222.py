0f73ab76467416e805d55552f23a5e931f4a432b
Yu212 <yu212.mc@gmail.com>
2025-09-07 10:01:48 +0900

    222

--- base_yu/task222.py
+++ base_yu/task222.py
@@ -6,8 +6,9 @@
 # lambda g,c=-5:c*g or p([*zip(*[[(v in[0,*s,0][i:i+3:2]and(c<0 or len({*g})==2 or v in(2,9)))*v for i,v in enumerate(s)]for s in g])],c+1)
 # lambda g,c=-7:c*g or p([[(s[i]in[0,*s,0][i:i+3:2]*(5<sum(g,g[0]).count(s[i])))*s[i] for i in range(16)]for s in zip(*g)],c+1)
 # lambda g,c=-7:c*g or p([[(5<sum(g,g[0]).count(v:=s[i]))*(v in[0,*s,0][i:i+3:2])*v for i in range(16)]for s in zip(*g)],c+1)
-p=lambda g,c=-7:c*g or p([[(v in[0,*s,0][i:i+3:2]*(5<sum(g,g[0]).count(v)))*v for i,v in enumerate(s)]for s in zip(*g)],c+1)
-
+# p=lambda g,c=-7:c*g or p([[(v in[0,*s,0][i:i+3:2]*(5<sum(g,g[0]).count(v)))*v for i,v in enumerate(s)]for s in zip(*g)],c+1)
+p=lambda g,c=-7:c*g or p([[v*(85<sum(g,[0,*s][i:i+3]*40).count(v))for i,v in enumerate(s)]for s in zip(*g)],c+1)
+# p=lambda g,c=-7:c*g or p([[sum(g,[0,*s][i:i+3]*40).count(s[i])//85*s[i]for i in range(16)]for s in zip(*g)],c+1)
 
 
 # def p(g):
