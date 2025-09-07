4a77508d00507a9d86d7c9c5d00fb822e8ed39ae
key-moon <kymn0116@gmail.com>
2025-09-07 19:07:50 +0900

    036

--- base_yu/task036.py
+++ base_yu/task036.py
@@ -1,8 +1,11 @@
-# best: 103(4atj sisyphus luke Seek) / others: 106(joking), 106(joking MeWhenI), 110(sisyphus), 113(4atj), 115(ovs)
-# ================================================ 103 ================================================
+# best: 90(jailctf merger) / others: 91(4atj sisyphus luke Seek mukundan), 91(4atj sisyphus luke Seek), 98(xsot ovs att joking mewheni), 117(mukundan), 163(kg583)
+# ========================================== 90 ==========================================
 # f=lambda g,d,c=-1:c*g or f([[*s]for s in zip(*g)if d in s],d,c+1)
 # f=lambda g,d:[[v for*t,v in zip(*g,s) if d in t]for s in g if d in s]
 # p=lambda g:min((s:=f(g,i))and(sum(t.count(0)for t in s),s)for i in sum(g,[]))[1]
 # p=lambda g:min((s:=[[v for*t,v in zip(*g,s)if i in t]for s in g if i in s])and(sum(t.count(0)for t in s),s)for i in sum(g,[]))[1]
 # p=lambda g:min((k:=1,s:=[[v for*t,v in zip(*g,s)if i in t if(k:=k+(v<1))]for s in g if i in s])and(k,s)for i in sum(g,g))[1]
-p=lambda g:min((k:=1,[[v for*t,v in zip(*g,s)if i in t if(k:=k+(v<1))]for s in g if i in s],k)[::-1]for i in sum(g,[]))[1]
+# p=lambda g:min((k:=1,[[v for*t,v in zip(*g,s)if i in t if(k:=k+(v<1))]for s in g if i in s],k)[::-1]for i in sum(g,[]))[1]
+# lambda g,c=1:(a:=[[v for*t,v in zip(*g,s)if c in t]for s in g if c in s])*(1<len(a)<6)or p(g,c+1)
+# lambda g,c=0:DUMP((c%3-1)*g or(a:=[s for s in zip(*p(g,c+1))if c//3in s])*(1<len(a)<6)or p(g,c+1))
+p=lambda g,c=1,d=1:-c*g or(a:=[s for s in zip(*p(g,c-1,d))if d in s])*(1<len(a)<6)or p(g,1,d+1)
