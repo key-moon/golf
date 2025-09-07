778b183987af751f49ad691b54755640073213cc
key-moon <kymn0116@gmail.com>
2025-09-07 19:10:39 +0900

    272

--- base_yu/task272.py
+++ base_yu/task272.py
@@ -1,5 +1,6 @@
-# best: 101(ovs) / others: 103(mukundan), 108(natte), 109(luke/sisyphus/Seek), 109(sisyphus), 113(Seek64)
-# =============================================== 101 ===============================================
+# best: 89(jailctf merger) / others: 95(xsot ovs att joking mewheni), 99(4atj sisyphus luke Seek mukundan), 99(4atj sisyphus luke Seek), 103(mukundan), 108(natte)
+# ========================================== 89 =========================================
 # p=lambda g,E=enumerate:[[(sum([0,*s,0][j:j+3]+[0,*t,0][i:i+3])==2*s[j]>0)or s[j]for j,t in E(zip(*g))]for i,s in E(g)]
 # p=lambda g,E=enumerate:[[sum([0,*s][j:j+3:2]+[0,*t][i:i+3:2])<s[j]or s[j]for j,t in E(zip(*g))]for i,s in E(g)]
-p=lambda g,E=enumerate:[[sum([0,*s][j:j+3]+[0,*t][i:i+3])<3*s[j]or s[j]for j,t in E(zip(*g))]for i,s in E(g)]
+# lambda g,E=enumerate:[[sum([0,*s][j:j+3]+[0,*t][i:i+3])<3*s[j]or s[j]for j,t in E(zip(*g))]for i,s in E(g)]
+p=lambda g,c=35:-c*g or[*zip(*eval(str(p(g,c-1)).replace(*[f"1, {1+c%3}","2","2,2","1"][c<1::2])))][::-1]
