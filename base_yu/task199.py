9c1b6f1d203220569d10bb60dc724e33a73fc5cc
key-moon <kymn0116@gmail.com>
2025-09-09 03:07:30 +0900

    199

--- base_yu/task199.py
+++ base_yu/task199.py
@@ -2,4 +2,4 @@
 # ======================================= 84 =======================================
 # p=lambda g:(l:=[*map(max,*g)])and[([4,0]*9)[l.index(max(l))%2:][:len(g)]]*-~(k:=g.index(l))+g[k:-1]
 # p=lambda g:[[t:=(l:=[*map(max,*g)]).index(max(l))%2*4]*0+[(t:=4-t)for _ in g]]*-~(k:=g.index(l))+g[k:-1]
-p=lambda g:[([4,0]*9)[(l:=[*map(max,*g)]).index(max(l))%2:][:len(g)]]*-~(k:=g.index(l))+g[k:-1]
+p=lambda g:[([4,0]*9)[(l:=max(g)).index(max(l))%2:][:len(g)]]*-~(k:=g.index(l))+g[k:-1]
