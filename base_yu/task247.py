d69d871941cd2655beaa6a35a104ed6d71e16bbd
key-moon <kymn0116@gmail.com>
2025-09-09 05:14:31 +0900

    247

--- base_yu/task247.py
+++ base_yu/task247.py
@@ -1,9 +1,13 @@
 # best: 95(4atj sisyphus luke Seek mukundan, 4atj sisyphus luke Seek, jailctf merger) / others: 96(xsot ovs att joking mewheni), 101(jonas ryno kg583), 112(kabutack), 112(kg583), 112(JRK)
-# ============================================= 95 ============================================
 # p=lambda g:(G:=sum(g,[]),u:=sorted((G.count(c),G.index(c)%10,c)for c in{*G}-{0}))and[[c for k,_,c in u if k==u[-1][0]]]*u[-1][0]
 # p=lambda g:(G:=sum(zip(*g),()),u:=[[c]*G.count(c)for c in G if c>0])and[*zip(*[s for s in u if len(s)==max(map(len,u))])]
 
-def p(g):
- G=sum(g,[])
- u=sorted((G.count(c),G.index(c)%10,c)for c in{*G}-{0})
- return[[c for k,_,c in u if k==u[-1][0]]]*u[-1][0]
+# f p(g):G=sum(g,[]);u=sorted((G.count(c),G.index(c)%10,c)for c in{*G}-{0});return[[c for k,_,c in u if k==u[-1][0]]]*u[-1][0]
+# ============================================= 95 ============================================
+# lambda g,c=9:(a:=[*sum(zip(*g),())])and(X:=[*filter(int,eval("(a.count(Y:=a.pop(0))==c)*Y,"*100))])and-~c*[X]or p(g,c-1)
+# f p(g,c=9):a=[*sum(zip(*g),())];X=eval("(a.count(Y:=a.pop(0))==c)*Y,"*100);return-~c*any(X)*[*filter(int,X)]or p(g,c-1)
+# lambda g,c=9:(a:=[*sum(zip(*g),())])and-~c*[X:=[v for v in a*1if(a.count(a.pop(0))==c)*v]]*any(X)or p(g,c-1)
+# lambda g,c=9:(a:=[*sum(zip(*g),())])and(X:=[v for v in a*1if(a.count(a.pop(0))==c-1)*v])and c*[X]or p(g,c-1)
+# f p(g,c=9):a=[*sum(zip(*g),())];X=[v for v in a*1if(a.count(a.pop(0))==c)*v];return-~c*any(X)*[X]or p(g,c-1)
+# lambda g,c=9:(a:=[*sum(zip(*g),())])and(X:=[v for v in a*1if(a.count(a.pop(0))==c)*v])and-~c*[X]or p(g,c-1)
+def p(g,c=9):a=[*sum(zip(*g),())];return(X:=[v for v in a*1if(a.count(a.pop(0))==c)*v])and-~c*[X]or p(g,c-1)
