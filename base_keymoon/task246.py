9e1b7711fe80888e8268a8ca53226f4d1bba346a
key-moon <kymn0116@gmail.com>
2025-09-09 01:44:54 +0900

    335, 246

---
+++ base_keymoon/task246.py
@@ -0,0 +1,3 @@
+# best: 105(4atj sisyphus luke Seek mukundan, jailctf merger) / others: 130(xsot ovs att joking mewheni), 143(4atj sisyphus luke Seek), 167(intgrah jimboko awu macaque sammyuri), 170(natte), 194(jacekwl Potatoman)
+# ================================================= 105 =================================================
+p=lambda g,c=-1,a=0:g*c or p([*zip(*[[(v or(c+3in s)*8)*(a|(a:=a^any(t)))for*t,v in zip(*g,s)]for s in g])],c+1)
---
+++ base_keymoon/task335.py
@@ -0,0 +1,3 @@
+# best: 107(4atj sisyphus luke Seek mukundan) / others: 110(jailctf merger), 131(mukundan), 132(xsot ovs att joking mewheni), 143(4atj sisyphus luke Seek), 166(MasukenSamba)
+# ================================================== 107 ==================================================
+p=lambda g,c=-1,a=0:g*c or p([*zip(*[[(v or(c*6+8in s)*4)*(a|(a:=a^any(t)))for*t,v in zip(*g,s)]for s in g])],c+1)
