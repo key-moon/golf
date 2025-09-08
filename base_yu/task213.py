f1d8129771cc1d046053c47158532b68eba5a4c8
Yu212 <yu212.mc@gmail.com>
2025-09-09 08:38:37 +0900

    213

--- base_yu/task213.py
+++ base_yu/task213.py
@@ -4,6 +4,6 @@
 # p=lambda g:any(map(all,g))and[*zip(*p([*zip(*g)]))]or(a:=[[*filter(int,map(max,zip(g[0],g[-1])))]])*len(a[0])
 # p=lambda g:any(map(all,g))and[*zip(*p([*zip(*g)]))]or(a:=[[v for s in zip(*g)if(v:=max({*s}-{5}))]])*len(a[0])
 # lambda g:any(min(g))and(a:=[[x|y for x,y in zip(g[0],g[-1])if x|y]])*len(a[0])or[*zip(*p([*zip(*g)]))]
-p=lambda g:any(min(g))and[a:=[*filter(int,max(g[0],g[-1]))]]*len(a)or[*zip(*p([*zip(*g)]))]
+p=lambda g:any(min(g))*[a:=[*filter(int,max(g[0],g[-1]))]]*len(a)or[*zip(*p([*zip(*g)]))]
 
 # lambda g:5in(a:=max(g,key=set))and[*zip(*p([*zip(*g[::-1])]))][::-1]or[a:=[*filter(int,a)]]*len(b)
