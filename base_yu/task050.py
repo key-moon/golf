04ccdfade7a824e3dc8bac19d00e2e536abb2354
key-moon <kymn0116@gmail.com>
2025-09-09 02:55:34 +0900

    050

--- base_yu/task050.py
+++ base_yu/task050.py
@@ -2,4 +2,6 @@
 # ======================================== 86 ========================================
 # p=lambda g,c=-1:c*g or p([[s[i]or(12<sum(s[:i])+6<sum(s))*3for i in range(len(s))]for s in zip(*g)],c+1)
 # lambda g,c=-1:c*g or p([[s[i]or(8in s[:i])*(8in s[i:])*3for i in range(len(s))]for s in zip(*g)],c+1)
-p=lambda g,c=-1:c*g or p([[v or(8in s[:i])*(8in s[i:])*3for i,v in enumerate(s)]for s in zip(*g)],c+1)
+# p=lambda g,c=-1:c*g or p([[v or(8in s[:i])*(8in s[i:])*3for i,v in enumerate(s)]for s in zip(*g)],c+1)
+# lambda g,c=-1,a=0:c*g or[*zip(*[[v+(v<1)*(a:=a^(23<sum(s)+v))*3for v in s]for s in p(g,c+1)])]
+p=lambda g,c=-1,a=0:c*g or[*zip(*[[v+(v<(a:=a^(23<sum(s)+v)))*3for v in s]for s in p(g,c+1)])]
