d289bae91f389446a7c948d024aeb91323e3e082
key-moon <kymn0116@gmail.com>
2025-09-09 13:19:17 +0900

    374

--- base_yu/task374.py
+++ base_yu/task374.py
@@ -1,7 +1,12 @@
 # best: 112(jailctf merger) / others: 119(4atj sisyphus luke Seek mukundan), 123(4atj sisyphus luke Seek), 130(mukundan), 150(xsot ovs att joking mewheni), 155(MasukenSamba)
 # ==================================================== 112 =====================================================
-p=lambda g,c=23:-c*g or p([*zip(*eval(str(g).replace((c//2*", 5")[2:],(c//2*f",{len({*sum(g,g[0])})*3%5}")[1:])))],c-1)
+# lambda g,c=23:-c*g or p([*zip(*eval(str(g).replace((c//2*", 5")[2:],(c//2*f",{len({*sum(g,g[0])})*3%5}")[1:])))],c-1)
+# lambda g,c=23:-c*g or p([*zip(*eval(str(g).replace("5"+c//2*", 5",(a:="len({*sum(g,g[0])})*3%5")+c//2*f",{a}")))],c-1)
+# lambda g,c=23:-c*g or p(eval(str([*zip(*g)]).replace((c//2*", 5")[2:],(c//2*",len({*sum(g,())})*3%5")[1:])),c-1)
+# lambda g,c=23:-c*g or p(eval(str([*zip(*g)]).replace((c//2*", 5")[2:],(c//2*",len({*str(g)})**2%7")[1:])),c-1)
+p=lambda g,c=47:-c*g or p(eval(str([*zip(*g)][::-1]).replace(c//4*", 5",c//4*",len({*str(g)})**2%7")),c-1)
 
+# mapping = {8: 1, 9: 4, 10: 2}
 
 # def p(g):
 #  for c in range(4,24)[::-1]:
