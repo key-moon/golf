6b4e7f33268be2218e3140d3a2681cbba21bea7b
key-moon <kymn0116@gmail.com>
2025-09-09 13:38:33 +0900

    marge 016

--- base_yu/task016.py
+++
@@ -1,3 +0,0 @@
-# best: 43(4atj sisyphus luke Seek mukundan, 4atj sisyphus luke Seek, jailctf merger, mukundan) / others: 44(xsot ovs att joking mewheni), 46(intgrah jimboko awu macaque sammyuri), 46(HETHAT), 47(cefqrn), 48(rucin93)
-# =================== 43 ==================
-p=lambda g:[[*map(b"A	".find,g[0])]]*3
