10c1586b1686c874b38358efa221116b54447c26
Yu212 <yu212.mc@gmail.com>
2025-09-10 23:21:22 +0900

    012

---
+++ base_yu/task012.py
@@ -0,0 +1,12 @@
+# best: 127(jailctf merger) / others: 133(xsot ovs att joking mewheni), 133(4atj sisyphus luke Seek mukundan), 133(4atj sisyphus luke Seek), 138(biz), 147(mukundan)
+# ============================================================ 127 ============================================================
+import re
+p=lambda g,c=-7:c*g or[*zip(*eval(re.sub(r"(([^0]), ([^0]), \2, )0(.{31}\2, )0(.{40})0",r"\1\2\4\3\5\3",str(p(g,c+1))))[::-1])]
+
+
+# import re
+# def p(g):
+#  for _ in range(8):
+#   g=eval(re.sub(r"(([^0]), ([^0]), \2, )0(.{31}\2, )0(.{40})0",r"\1\2\4\3\5\3",str(g)))
+#   g=[*zip(*g[::-1])]
+#  return g
