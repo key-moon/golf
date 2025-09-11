5e38aafb52b6f6251e2aa7d1e71699b2996b1471
kq5y <contact@kq5.jp>
2025-09-11 06:37:17 +0000

    258

---
+++ base_kq5y/task258.py
@@ -0,0 +1,3 @@
+# best: 61(4atj sisyphus luke Seek mukundan, intgrah jimboko awu macaque sammyuri, JRK, jonas ryno kg583, xsot ovs att joking mewheni, jailctf merger) / others: 64(Bulmenisaurus), 67(MasukenSamba), 67(natte), 67(HETHAT), 69(jacekwl Potatoman nauti)
+# ============================ 61 ===========================
+import re;p=lambda g:eval(re.sub("1, 0(?=, 1)","1,2",str(g)))
