44bb53d1af4d1399521e351e964b2f8214061960
key-moon <kymn0116@gmail.com>
2025-09-12 05:07:03 +0900

    336

--- base_yu/task336.py
+++ base_yu/task336.py
@@ -4,7 +4,8 @@
 # p=lambda g,c=-3:c*g or p((l:=[0]*len(g[0]))and[(l:=[s[i]or(any(s[:i])*any(s[i:])or(l.count(8),v)==(1,8))*8for i,v in enumerate(l)])for s in zip(*g[::-1])],c+1)
 
 # p=lambda g,c=-3:c*g or p([[s[i]or([*s,5].index(5)<i<[*s,5].index(5,4))*8for i in range(10)]for s in zip(*g[::-1])],c+1)
-p=lambda g,c=-3:c*g or p([[s[i]or((f:=[*s,5].index)(5)<i<f(5,4))*8for i in range(10)]for s in zip(*g[::-1])],c+1)
+p=lambda g,c=-3:c*g or[[s[i]or((f:=[*s,5].index)(5)<i<f(5,4))*8for i in range(10)]for s in zip(*p(g,c+1)[::-1])]
+
 
 # def p(g):
 #  for _ in range(4):
