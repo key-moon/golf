6317a867075ea1bf976e1b47c8d42898f0c1f2f2
key-moon <kymn0116@gmail.com>
2025-09-12 06:11:17 +0900

    169, 330

--- base_keymoon/task169.py
+++ base_keymoon/task169.py
@@ -3,7 +3,7 @@
 # lambda g,c=2**26:-c*[[-v.bit_count()%7for v in s]for s in g]or p([(l:=0)or[l:=v and v|l|(v<9)*(c:=c//2)for v in s]for s in zip(*g)][::-1],min(c-1,6))
 # lambda g,c=7,d=8**40:-c*[[-v.bit_count()%7for v in s]for s in g]or p([(l:=0)or[l:=(v|l|(v<9)*(d:=d//2))&-(0<v)for v in s]for s in zip(*g)][::-1],c-1)
 # lambda g,c=7,d=8**9:-c*[[-v.bit_count()%7for v in s]for s in g]or p([(l:=0)or[l:=v and v|l|(v<9)*(d:=d//2)for v in s]for s in zip(*g)][::-1],c-1)
-p=lambda g,c=11,d=8**9:-c*g or[(l:=0)or[l:=v and[-v.bit_count()%7,v|l|(v<9)*(d:=d//2)][c<11]for v in s]for s in zip(*p(g,c-1))][::-1]
+p=lambda g,c=11,d=8:-c*g or[(l:=0)or[l:=v and[-v.bit_count()%7,v|l|(v<9)*(d:=d*2)][c<11]for v in s]for s in zip(*p(g,c-1))][::-1]
 
 # mapping = { 0: 0, 2: 3, 3: 2, 4: 1 } -a%5
 # mapping = { 0: 0, 4: 3, 5: 2, 6: 1 } -a%7
