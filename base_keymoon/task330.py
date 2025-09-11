edfb4792b1d3d84f4489a9e5a90539b8c1a7ed62
Yu212 <yu212.mc@gmail.com>
2025-09-12 08:19:19 +0900

    330

--- base_keymoon/task330.py
+++ base_keymoon/task330.py
@@ -1,3 +1,4 @@
 # best: 134(4atj sisyphus luke Seek mukundan) / others: 135(xsot ovs att joking mewheni), 145(jailctf merger), 203(kdmitrie), 213(MasukenSamba), 215(intgrah jimboko awu macaque sammyuri)
 # =============================================================== 134 ================================================================
-p=lambda g,c=15,d=8:-c*g or[(l:=0)or[l:=v and[1+(v.bit_count()==8),v|l|(v<9)*(d:=d*2)][c<15]for v in s]for s in zip(*p(g,c-1))][::-1]
+# p=lambda g,c=15,d=8:-c*g or[(l:=0)or[l:=v and[1+(v.bit_count()==8),v|l|(v<9)*(d:=d*2)][c<15]for v in s]for s in zip(*p(g,c-1))][::-1]
+p=lambda g,c=15,d=8:-c*g or[(l:=0)or[l:=v and[-59%v.bit_count()%3,v|l|(v<9)*(d:=d*2)][c<15]for v in s]for s in zip(*p(g,c-1))][::-1]
