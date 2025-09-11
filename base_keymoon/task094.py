ab53081d28dedc4c5a33d54ee521dcc1715d8070
key-moon <kymn0116@gmail.com>
2025-09-12 02:54:56 +0900

    094

---
+++ base_keymoon/task094.py
@@ -0,0 +1,4 @@
+# best: 102(4atj sisyphus luke Seek mukundan) / others: 106(xsot ovs att joking mewheni), 114(jailctf merger), 165(natte), 169(intgrah jimboko awu macaque sammyuri), 170(MasukenSamba)
+# =============================================== 102 ================================================
+# port re;p=lambda g,c=-3:g*c or[*zip(*eval(re.sub(r"8(?=[^(]+\(.{,40}1.{43}1, 1)","6",str(p(g,c+1)))))]
+import re;p=lambda g,c=-3:g*c or[*zip(*eval(re.sub(r"8(?=[^(]*\([^(]*1.{43}1, 1)","6",str(p(g,c+1)))))]
