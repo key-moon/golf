5359c31e7c753b64a5e7e7df4a9628dd38df760e
key-moon <kymn0116@gmail.com>
2025-09-12 05:06:33 +0900

    169

---
+++ base_keymoon/task169.py
@@ -0,0 +1,15 @@
+# best: 129(4atj sisyphus luke Seek mukundan) / others: 134(xsot ovs att joking mewheni), 139(jailctf merger), 195(jonas ryno kg583), 201(MasukenSamba), 203(JRK)
+# ============================================================= 129 =============================================================
+# lambda g,c=2**26:-c*[[-v.bit_count()%7for v in s]for s in g]or p([(l:=0)or[l:=v and v|l|(v<9)*(c:=c//2)for v in s]for s in zip(*g)][::-1],min(c-1,6))
+# lambda g,c=7,d=8**40:-c*[[-v.bit_count()%7for v in s]for s in g]or p([(l:=0)or[l:=(v|l|(v<9)*(d:=d//2))&-(0<v)for v in s]for s in zip(*g)][::-1],c-1)
+p=lambda g,c=7,d=8**9:-c*[[-v.bit_count()%7for v in s]for s in g]or p([(l:=0)or[l:=v and v|l|(v<9)*(d:=d//2)for v in s]for s in zip(*g)][::-1],c-1)
+
+# mapping = { 0: 0, 2: 3, 3: 2, 4: 1 } -a%5
+# mapping = { 0: 0, 4: 3, 5: 2, 6: 1 } -a%7
+
+# (v|l|(v<9)*(d:=d//2))*v%2
+# v and v|l|(v<9)*(d:=d//2)
+
+# v.bit_count()
+# bin(v).count("1")
+# 
