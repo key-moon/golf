b9f1f66050716fb8f9738f6d75cb47a2548c39b7
Yu212 <yu212.mc@gmail.com>
2025-09-13 00:10:56 +0900

    239

--- base_yu/task239.py
+++ base_yu/task239.py
@@ -2,4 +2,6 @@
 # =============================================== 99 ==============================================
 # f p(g):u=sum(g,[]);C=sorted([(-u.count(v),v)for v in{*u}]);return[*zip(*[[c]*-v+[0]*(v-min(C)[0])for v,c in C])]
 # f p(g):u=sum(g,[]);C=sorted({*u},key=(k:=u.count))[::-1];return[*zip(*[[c]*k(c)+[0]*(k(C[0])-k(c))for c in C])]
-def p(g):u=sum(g,[]);C=sorted({*u},key=(k:=u.count))[::-1];return[[c*(i<k(c))for c in C]for i in range(k(C[0]))]
+# def p(g):u=sum(g,[]);C=sorted({*u},key=(k:=u.count))[::-1];return[[c*(i<k(c))for c in C]for i in range(k(C[0]))]
+# def p(g):u=sum(g,[]);C=sorted([[c:=u.count(v)]+[v]*c+[0]*99for v in{*u}])[::-1];return[*zip(*C)][1:C[0][0]+1]
+def p(g):u=sum(g,[]);C=sorted([c:=-u.count(v)]+[v]*-c+[0]*99for v in{*u});return[*zip(*C)][1:1-C[0][0]]
