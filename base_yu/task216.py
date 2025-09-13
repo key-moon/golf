debb7abbcf0945065425971bdeb8ba42e12989e8
Yu212 <yu212.mc@gmail.com>
2025-09-13 19:08:08 +0900

    216,365

--- base_yu/task216.py
+++ base_yu/task216.py
@@ -4,7 +4,9 @@
 # p=lambda g:max([(sum(t:=[s[l%20:[*g[l//20],0].index(0,l%20)]for s in g[l//20:[*[g[k][l%20]for k in range(20)],0].index(0,l//20)]],[]).count(2),~l,t)for l in range(400)])[2]
 # p=lambda g:max([(sum(t:=[s[l%20:[*g[l//20],0].index(0,l%20)]for s in g[l//20:[*[*[*zip(*g)][l%20]],0].index(0,l//20)]],[]).count(2),~l,t)for l in range(400)])[2]
 # p=lambda g:max((all(y:=sum(x:=[s[l:r]for s in g[u:d]],[])),y.count(2),len(y),x)for r in range(21)for l in range(r)for d in range(21)for u in range(d))[3]
-p=lambda g:max((all(y:=sum(x:=[s[l:t%21]for s in g[u:t//21]],[])),y.count(2),len(y),x)for t in range(441)for l in range(t%21)for u in range(t//21))[3]
+# p=lambda g:max((all(y:=sum(x:=[s[l:t%21]for s in g[u:t//21]],[])),y.count(2),len(y),x)for t in range(441)for l in range(t%21)for u in range(t//21))[3]
+p=lambda g,c=-3:c*g or min(("0"in(y:=str(x:=p([*zip(*g[l::-1])],c+1))),-y.count("2"),-len(y),x)for l in range(len(g)))[3]
+
 
 # def p(g):
 #  V=0
--- base_yu/task365.py
+++ base_yu/task365.py
@@ -7,7 +7,8 @@
 # p=lambda g,A=range:max((all(y:=sum(x:=[s[l:r]for s in g[u:d]],[])),y.count(2),len(y),x)for r in A(11)for l in A(r)for d in A(11)for u in A(d))[3]
 # p=lambda g,c=-1:c*g or max([p([*zip(*g[l:r])],c+1)for r in range(11)for l in range(r)],key=lambda g:(all(y:=sum(g,g[0]*0)),y.count(2),len(y),g))
 # p=lambda g,c=-1:c*g or min([p([*zip(*g[l:r])],c+1)for r in range(11)for l in range(r)],key=lambda g:("0"in(y:=str(g)),-y.count("2"),-len(y),g))
-p=lambda g,c=-1:c*g or min([("0"in(y:=str(u:=p([*zip(*g[l:r])],c+1))),-y.count("2"),-len(y),u)for r in range(11)for l in range(r)])[3]
+# p=lambda g,c=-1:c*g or min([("0"in(y:=str(u:=p([*zip(*g[l:r])],c+1))),-y.count("2"),-len(y),u)for r in range(11)for l in range(r)])[3]
+p=lambda g,c=-3:c*g or min(("0"in(y:=str(u:=p([*zip(*g[l::-1])],c+1))),-y.count("2"),-len(y),u)for l in range(len(g)))[3]
 
 # def p(g):
 #  for r in range(10):
