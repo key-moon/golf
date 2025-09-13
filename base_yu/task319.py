ff9769860dd3072e1251c2787891f32c3be01340
Yu212 <yu212.mc@gmail.com>
2025-09-13 17:23:09 +0900

    319

--- base_yu/task319.py
+++ base_yu/task319.py
@@ -1,25 +1,47 @@
 # best: 194(jailctf merger) / others: 207(4atj sisyphus luke Seek mukundan), 243(xsot ovs att joking mewheni), 327(MasukenSamba), 396(jacekwl), 396(jacekwl Potatoman nauti)
 # ============================================================================================= 194 ==============================================================================================
+# def p(g):
+#  *C,b=sorted({*sum(g,[])},key=sum(g,[]).count)
+#  for c in C:
+#   for y in range(-32,14):
+#    for x in range(-32,14):
+#     u=[len(g)>(Y:=(i+y)//2)>-1<(X:=(j+x)//2)<len(g[i]) and g[Y][X] for i,s in enumerate(g)for j,v in enumerate(s)if v==c]
+#     if len({*u})==1 and u[0] not in (0,c,b):
+#      print(CASE,u)
+#      return[[v for*t,v in zip(*g,s)if u[0]in t]for s in g if u[0]in s]
+#  return g
+
+
 def p(g):
  *C,b=sorted({*sum(g,[])},key=sum(g,[]).count)
  for c in C:
   for y in range(-32,14):
    for x in range(-32,14):
-    U=[]
-    L=[]
-    d=[]
-    for i in range(len(g)):
-     for j in range(len(g[i])):
-      if g[i][j]==c:
-       U+=[i]
-       L+=[j]
-       for k in range(2):
-        for l in range(2):
-         Y=i*2+y+k
-         X=j*2+x+l
-         if len(g)>Y>-1<X<len(g[i]):
-          d+=[g[Y][X]]
-    L,*_,R=sorted(L)
-    U,*_,D=sorted(U)
-    if any({*d}=={C[k]}and len(d)==sum(g,[]).count(C[k])for k in range(3)):
-     return[[[b,c][v==c]for v in s[L:R+1]]for s in g[U:D+1]]
+    d=[g[i*2+y+k][j*2+x+l]for i in range(len(g))for j in range(len(g[i]))for k in range(2)for l in range(2)if g[i][j]==c if len(g)>i*2+y+k>-1<j*2+x+l<len(g[i])]
+    if []<d==[d[0]]*sum(g,[]).count(d[0]):
+     return [[[b,v][v==c] for*t,v in zip(*g,s)if c in t]for s in g if c in s]
+
+
+# def p(g):
+#  *C,b=sorted({*sum(g,[])},key=sum(g,[]).count)
+#  for c in C:
+#   for y in range(-32,14):
+#    for x in range(-32,14):
+#     U=[]
+#     L=[]
+#     d=[]
+#     for i in range(len(g)):
+#      for j in range(len(g[i])):
+#       if g[i][j]==c:
+#        U+=[i]
+#        L+=[j]
+#        for k in range(2):
+#         for l in range(2):
+#          Y=i*2+y+k
+#          X=j*2+x+l
+#          if len(g)>Y>-1<X<len(g[i]):
+#           d+=[g[Y][X]]
+#     L,*_,R=sorted(L)
+#     U,*_,D=sorted(U)
+#     if any({*d}=={C[k]}and len(d)==sum(g,[]).count(C[k])for k in range(3)):
+#      return[[[b,c][v==c]for v in s[L:R+1]]for s in g[U:D+1]]
