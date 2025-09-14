f8b3b556d0d3b0b120adc65c4d8556f53ceda64f
Yu212 <yu212.mc@gmail.com>
2025-09-14 13:49:36 +0900

    054

--- base_yu/task054.py
+++ base_yu/task054.py
@@ -5,15 +5,26 @@ def p(g):
   if len(z)>3 and z[-1][0]-z[0][0]<5:
    y=z[0][0]+z[-1][0]>>1
    x=z[0][1]+z[-1][1]>>1
-   for l in range(1,30):
-    for u in range(1,30):
-     r=g[u].index(g[0][0],l)
-     d=[*zip(*g)][l].index(g[0][0],u)
-     if g[0][0]==g[u-1][l]==g[u][l-1] and r-l>5:
-      g[u:d]=[[(l<=j<r)*max(g[k][w]==g[y][x]!=g[i][j]!=g[0][0]!=(v:=g[y+max(min(i-k,2),-2)][x+max(min(j-w,2),-2)])and v for k in range(u,d)for w in range(l,r)) or g[i][j] for j in range(30)]for i in range(u,d)]
+   g=[[max((g[0][0]not in g[i][min(j,w):max(j,w)] and g[0][0]not in [*zip(*g)][w][min(i,k):max(i,k)])and g[k][w]==g[y][x]!=g[i][j]!=g[0][0]!=(v:=g[y+max(min(i-k,2),-2)][x+max(min(j-w,2),-2)])and v for k in range(30)for w in range(30)) or g[i][j] for j in range(30)]for i in range(30)]
    for s in g[y-2:y+3]:
     s[x-2:x+3]=[g[0][0]]*5
    return g
+
+# def p(g):
+#  for c in range(10):
+#   z=[(i,j)for i in range(30)for j in range(30)if g[i][j]==c]
+#   if len(z)>3 and z[-1][0]-z[0][0]<5:
+#    y=z[0][0]+z[-1][0]>>1
+#    x=z[0][1]+z[-1][1]>>1
+#    for l in range(1,30):
+#     for u in range(1,30):
+#      r=g[u].index(g[0][0],l)
+#      d=[*zip(*g)][l].index(g[0][0],u)
+#      if g[0][0]==g[u-1][l]==g[u][l-1] and r-l>5:
+#       g[u:d]=[[(l<=j<r)*max(g[k][w]==g[y][x]!=g[i][j]!=g[0][0]!=(v:=g[y+max(min(i-k,2),-2)][x+max(min(j-w,2),-2)])and v for k in range(u,d)for w in range(l,r)) or g[i][j] for j in range(30)]for i in range(u,d)]
+#    for s in g[y-2:y+3]:
+#     s[x-2:x+3]=[g[0][0]]*5
+#    return g
 
 
 # def p(g):
