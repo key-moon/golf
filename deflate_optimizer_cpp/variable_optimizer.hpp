4da37217ad43f06330855ced067ecb2c158e786f
Hiroki Shibata <shibata.hiroki.753@s.kyushu-u.ac.jp>
2025-09-15 13:35:01 +0900

    hoge~

--- deflate_optimizer_cpp/variable_optimizer.hpp
+++ deflate_optimizer_cpp/variable_optimizer.hpp
@@ -4,16 +4,38 @@
 #include "optimizer.hpp"
 #include "variable.hpp"
 
+
+
+// only signed char can be variable name
+void rename_variables(std::vector<Token>& tokens, const std::vector<Variable>& variables, const std::vector<int>& assigned_literal_code) {
+    std::vector<int> text_position_to_variable_id(-1); // text_position -> variable id
+    for (int var_id = 0; var_id < variables.size(); ++var_id) {
+        for (auto pos : variables[var_id].occurrences) {
+            text_position_to_variable_id[pos] = var_id;
+        }
+    }
+    // TODO!!!!!!!!!!!!!!!!!!!!!!
+}
+
+
 void optimize_variables(DynamicHuffmanBlock& block, const std::vector<Variable>& variables, const std::vector<int>& context) {
 
     auto text = block.get_string(context);
+
+    std::cout << "-------- Code --------\n";
+    for (auto c : text) {
+        std::cout << static_cast<char>(c);
+    }
+    std::cout << "\n\n";
+
+
 
     std::vector<bool> is_lieral_position(text.size());
 
     std::vector<int> literal_freq(256, 0);
 
     int ptr = 0;
-    for(auto tok : block.tokens) {
+    for (auto tok : block.tokens) {
         if (tok.type == Token::LITERAL) {
             is_lieral_position[ptr] = true;
             literal_freq[tok.literal]++;
@@ -37,30 +59,117 @@ void optimize_variables(DynamicHuffmanBlock& block, const std::vector<Variable>&
             }
         }
     }
-    std::vector<std::pair<int,int>> considered_vars; // only single character variables, (char, lit_freq)
+    struct CharStat {
+        bool var_candidate = false;
+        int num_var_occurrences_as_literal;
+        int num_nonvar_occurrences_as_literal;
+        int lit_code_length;
+    };
+    std::vector<CharStat> char_stats(256);
+    for (int i = 'A'; i <= 'Z'; ++i) char_stats[i].var_candidate = true;
+    for (int i = 'a'; i <= 'z'; ++i) char_stats[i].var_candidate = true;
+    char_stats['_'].var_candidate = true;
+
+    for (int i = 0; i < 256; ++i) {
+        char_stats[i].num_var_occurrences_as_literal = 0;
+        char_stats[i].num_nonvar_occurrences_as_literal = literal_freq[i];
+        char_stats[i].lit_code_length = block.literal_code_lengths[i];
+    }
 
     for (int i = 0; i < variables.size(); ++i) {
         if (variables[i].name.size() != 1) continue;
         int char_val = static_cast<int>(variables[i].name[0]);
-        considered_vars.emplace_back(char_val, num_lit_occurrences_of_vars[i]);
-        literal_freq[char_val] -= num_lit_occurrences_of_vars[i]; // literal_freq only consider non-variable occurrences
+        if (!char_stats[char_val].var_candidate) continue;
+        char_stats[char_val].num_var_occurrences_as_literal = num_lit_occurrences_of_vars[i];
+        char_stats[char_val].num_nonvar_occurrences_as_literal -= num_lit_occurrences_of_vars[i];
     }
-    std::sort(considered_vars.begin(), considered_vars.end(), [](const auto& a, const auto& b) {
-        return a.second > b.second;
-    });
-    std::cout << "Considered variables: ";
-    for (const auto& p : considered_vars) {
-        std::cout << "'" << static_cast<char>(p.first) << "'(" << p.second << ") ";
+
+    std::cout << "-------- Variable Candidates (var_occ, nonvar_occ, lit_code_length) --------\n";
+    for (int i = 0; i < 256; ++i) {
+        if (!char_stats[i].var_candidate) continue;
+        std::cout << "Char '" << static_cast<char>(i) << "' (" << i << "): ";
+        std::cout << char_stats[i].num_var_occurrences_as_literal << ", ";
+        std::cout << char_stats[i].num_nonvar_occurrences_as_literal << ", ";
+        std::cout << char_stats[i].lit_code_length 
+         << "\n";
     }
-    std::cout << "\n";
-    std::cout << "Literal frequencies after removing variable occurrences: ";
-    for (int i = 0; i < literal_freq.size(); ++i) {
-        if (literal_freq[i] > 0) {
-            std::cout << "'" << static_cast<char>(i) << "'(" << literal_freq[i] << ")\n";
+
+    // とりあえず、ビット数が少ないところに貪欲に当てはめていく
+    // ビット数が同じ場合、既に埋めたところに近いものを優先的に割り当てる感じで
+    struct VarStat {
+        int original_char;
+        int num_var_occurrences_as_literal;
+    };
+
+    std::vector<VarStat> vars; // 変数をliteralとしての出現回数でソート
+    for (int i = 0; i < 256; ++i) {
+        if (!char_stats[i].var_candidate) continue;
+        if (char_stats[i].num_var_occurrences_as_literal == 0) continue;
+        std::cout << "Variable candidate: '" << static_cast<char>(i) << "' (" << i << ") with " << char_stats[i].num_var_occurrences_as_literal << " literal occurrences\n";
+        vars.push_back({i, char_stats[i].num_var_occurrences_as_literal});
+    }
+    std::sort(vars.begin(), vars.end(), [](const VarStat& a, const VarStat& b) {
+        if (a.num_var_occurrences_as_literal != b.num_var_occurrences_as_literal) {
+            return a.num_var_occurrences_as_literal > b.num_var_occurrences_as_literal;
         }
+        return a.original_char < b.original_char;
+    });
+
+    std::vector<int> assigned_literal_code(256, -1); // 変数文字 -> 割り当てたliteralコード
+
+    std::vector<std::vector<int>> code_length_symbol_map(17);
+    for (int i = 0; i < 256; ++i) {
+        if (!char_stats[i].var_candidate) continue;
+        code_length_symbol_map[char_stats[i].lit_code_length].push_back(i);
     }
-    std::vector<int> varid_variable_chars;
-    for (int i = 'a'; i <= 'z'; ++i) varid_variable_chars.push_back(i);
-    for (int i = 'A'; i <= 'Z'; ++i) varid_variable_chars.push_back(i);
-    varid_variable_chars.push_back('_');
+    ptr = 0;
+    for (int len = 1; len <= 16; ++len) {
+        std::cout << "Length " << len << ": " << code_length_symbol_map[len].size() << " candidates\n";
+        if (code_length_symbol_map[len].size() == 0) continue;
+        std::vector<int> traverse_vars_list;
+        std::vector<int> distance_vec(256, 1e9);
+        std::queue<int> que;
+
+
+        if (ptr == 0) {
+            // 最初の変数はlit codeの出現頻度で決める
+            std::sort(code_length_symbol_map[len].begin(), code_length_symbol_map[len].end(), [&](int a, int b) {
+                return char_stats[a].num_nonvar_occurrences_as_literal > char_stats[b].num_nonvar_occurrences_as_literal;
+            });
+            assigned_literal_code[vars[ptr].original_char] = code_length_symbol_map[len][0];
+            ++ptr;
+        }
+
+        for (int j = 0; j < 256; ++j) {
+            if (assigned_literal_code[j] != -1) {
+                distance_vec[j] = 0;
+                que.push(j);
+            }
+        }
+        while (!que.empty()) {
+            int v = que.front();
+            que.pop();
+            if (char_stats[v].lit_code_length == len) {
+                traverse_vars_list.push_back(v);
+            }
+            for (int u : code_length_symbol_map[len]) {
+                if (distance_vec[u] > distance_vec[v] + 1) {
+                    distance_vec[u] = distance_vec[v] + 1;
+                    que.push(u);
+                }
+            }
+        }
+        std::cout << "traverse_vars_list size: " << traverse_vars_list.size() << "\n";
+        for(auto var : traverse_vars_list) {
+            assigned_literal_code[vars[ptr].original_char] = var;
+            ++ptr;
+            if (ptr >= vars.size()) break;
+        }
+        if (ptr >= vars.size()) break;
+    }
+    std::cout << ptr << " / " << vars.size() << " variables assigned\n";
+    for (int i = 0; i < 256; ++i) {
+        if (assigned_literal_code[i] == -1) continue;
+        std::cout << "Assigned literal code " << static_cast<char>(assigned_literal_code[i]) << " to variable '" << static_cast<char>(i) << "' (" << i << ")\n";
+    }
 }
