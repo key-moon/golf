ee00bd1a32724e6884d37960d4ba101ac03f3198
Yu212 <yu212.mc@gmail.com>
2025-09-16 02:16:02 +0900

    018

---
+++ base_yu/task018.py
@@ -0,0 +1,21 @@
+# best: 323(xsot ovs att joking mewheni, jailctf merger) / others: 371(natte), 411(MasukenSamba), 416(jacekwl), 425(jacekw), 427(jacekwl Potatoman nauti)
+def p(g):
+ C=max({*sum(g,[])}-{0},key=sum(g,[]).count)
+ for y,x,c in [(y,x,g[y][x])for y in range(len(g))for x in range(len(g[0]))if g[y][x]==C]:
+  v=[(y,x,g[y][x])]
+  v=[(y,x,g[y][x])for y in range(len(g))for x in range(len(g[0]))if g[y][x]if any(2>i-y>-2<j-x<2 for i,j,c in v)]
+  v=[(y,x,g[y][x])for y in range(len(g))for x in range(len(g[0]))if g[y][x]if any(2>i-y>-2<j-x<2 for i,j,c in v)]
+  v=[(y,x,g[y][x])for y in range(len(g))for x in range(len(g[0]))if g[y][x]if any(2>i-y>-2<j-x<2 for i,j,c in v)]
+  v=[(y,x,g[y][x])for y in range(len(g))for x in range(len(g[0]))if g[y][x]if any(2>i-y>-2<j-x<2 for i,j,c in v)]
+  v=[(y,x,g[y][x])for y in range(len(g))for x in range(len(g[0]))if g[y][x]if any(2>i-y>-2<j-x<2 for i,j,c in v)]
+  v=[(y,x,g[y][x])for y in range(len(g))for x in range(len(g[0]))if g[y][x]if any(2>i-y>-2<j-x<2 for i,j,c in v)]
+  for d in [-1,-1,-1,1,-1,-1,-1,1]:
+   for y in range(-22,22):
+    for x in range(-22,22):
+     if all(len(g)>i+y>-1<j+x<len(g[0]) and g[i+y][j+x]==c for i,j,c in v if c!=C):
+      for i,j,c in v:
+       g[i+y][j+x]=c
+   g=[*map(list,zip(*g[::d]))]
+  for i,j,c in v:
+   g[i][j]=0
+ return g
