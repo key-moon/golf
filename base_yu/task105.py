8315e0fecd4148168b9b9f1c5507711be4764dcc
Yu212 <yu212.mc@gmail.com>
2025-09-16 19:06:05 +0900

    105

---
+++ base_yu/task105.py
@@ -0,0 +1,15 @@
+# best: 148(4atj sisyphus luke Seek mukundan) / others: 153(xsot ovs att joking mewheni), 181(jailctf merger), 185(intgrah jimboko awu macaque sammyuri), 217(2F), 217(biz)
+# ====================================================================== 148 =======================================================================
+p=lambda g,c=-31:(c-2)*g or c>0 and p([*zip(*[[v or (s[1:-1].count(1)>1 or i%(len(g)-1)<1)*2 for v in s]for i,s in enumerate(g)])],c+1)or[*zip(*p([*zip(*g[any(g[-1])-2::-1])],c+1))][::-1]+g[-1:]
+
+# def p(g,c=-31):
+#  if c==3:
+#   return g
+#  if c==1 or c==2:
+#   return p([*zip(*[[v or (s[1:-1].count(1)>1 or i%(len(g)-1)<1)*2 for v in s]for i,s in enumerate(g)])],c+1)
+#  return [*zip(*p([*zip(*g[any(g[-1])-2::-1])],c+1))][::-1]+g[-1:]
+
+# def p(g,c=-31):
+#  if c>0:
+#   return [[s[j] or (sum(s[1:-1])>1 or sum(t[1:-1])>1 or i%(len(g)-1)==0 or j%(len(s)-1)==0)*2 for j,t in enumerate(zip(*g))]for i,s in enumerate(g)]
+#  return [*zip(*p([*zip(*g[any(g[-1])-2::-1])],c+1))][::-1]+g[-1:]
