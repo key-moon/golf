a46d28d6d0f567ebdc860113880ffc282f9d6dfd
key-moon <kymn0116@gmail.com>
2025-09-17 08:39:14 +0900

    036

---
+++ base_keymoon/task036.py
@@ -0,0 +1,4 @@
+# best: 75(4atj sisyphus luke Seek mukundan, biz) / others: 84(2F), 86(jailctf merger), 90(xsot ovs att joking mewheni), 109(jacekwl Potatoman nauti), 145(intgrah jimboko awu macaque sammyuri)
+# =================================== 75 ==================================
+def p(g):
+ return g
