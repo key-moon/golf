e0365ef04a43d98d82f46bb803ad11946c9e7927
kq5y <contact@kq5.jp>
2025-09-16 12:57:03 +0000

    193 (not worth)

--- base_yu/task193.py
+++ base_yu/task193.py
@@ -4,6 +4,8 @@
 p=lambda g,c=-1:c*g or p([[any([0,*s][i:i+3:2])*s[i]for i in range(len(s))]for s in zip(*g)],c+1)
 # p=lambda g,R=range:(n:=len(g))and[[sum(-1<(y:=i+abs(k-2)-1)<n>(x:=j+abs(k-1)-1)>-1and g[y][x]>0for k in R(4))>1and g[i][j]for j in R(n)]for i in R(n)]
 
+# import re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub('(0, |\()[^0](, 0|\))',r'\1 0\2',str(p(g,c+1)))))]
+
 # R=range
 # p=lambda g:(n:=len(g))and[[sum(-1<(y:=i+abs(k-2)-1)<n and-1<(x:=j+abs(k-1)-1)<n and g[y][x]>0for k in R(4))>1and g[i][j] or 0for j in R(n)]for i in R(n)]
 
