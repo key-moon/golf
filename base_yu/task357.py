7471a7a45e9960a9f4a620f712fb8df42eccce1b
kq5y <contact@kq5.jp>
2025-09-16 14:00:10 +0000

    357

--- base_yu/task357.py
+++ base_yu/task357.py
@@ -1,3 +1,5 @@
 # best: 86(jailctf merger) / others: 90(xsot ovs att joking mewheni), 92(4atj sisyphus luke Seek mukundan), 94(jonas ryno kg583), 97(Yuchen20), 100(natte)
 # ======================================== 86 ========================================
-p=lambda g:(((v:=[u:=[1]+[8]*~-len(g[0])]+[u:=[8]+u[:-1]for _ in u[1:]])+v[-2:0:-1])*9)[len(g)-1::-1]
+p=lambda g:(((v:=[u:=[1]+[8]*~-len(g[0])]+[u:=[8]+u[:-1]for _ in u[1:]])+v[-2:0:-1])*9)[9::-1]
+
+# p=lambda g:[[8-7*(i==5-abs(j-4))for i in range(len(g[0]))]for j in range(10)] これはダメだけどこんな感じで短くできるかも
