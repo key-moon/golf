75bb8bff4ea59f632d118d804d55d59ba3783e38
key-moon <kymn0116@gmail.com>
2025-09-16 15:55:39 +0900

    050

--- base_yu/task050.py
+++ base_yu/task050.py
@@ -1,7 +1,8 @@
 # best: 85(4atj sisyphus luke Seek mukundan) / others: 88(jailctf merger), 91(xsot ovs att joking mewheni), 99(intgrah jimboko awu macaque sammyuri), 101(duckyluuk), 101(MasukenSamba)
 # ======================================== 85 =======================================
-# p=lambda g,c=-1:c*g or p([[s[i]or(12<sum(s[:i])+6<sum(s))*3for i in range(len(s))]for s in zip(*g)],c+1)
+# lambda g,c=-1:c*g or p([[s[i]or(12<sum(s[:i])+6<sum(s))*3for i in range(len(s))]for s in zip(*g)],c+1)
 # lambda g,c=-1:c*g or p([[s[i]or(8in s[:i])*(8in s[i:])*3for i in range(len(s))]for s in zip(*g)],c+1)
-# p=lambda g,c=-1:c*g or p([[v or(8in s[:i])*(8in s[i:])*3for i,v in enumerate(s)]for s in zip(*g)],c+1)
+# lambda g,c=-1:c*g or p([[v or(8in s[:i])*(8in s[i:])*3for i,v in enumerate(s)]for s in zip(*g)],c+1)
 # lambda g,c=-1,a=0:c*g or[*zip(*[[v+(v<1)*(a:=a^(23<sum(s)+v))*3for v in s]for s in p(g,c+1)])]
-p=lambda g,c=-1,a=0:c*g or[*zip(*[[v+(v<(a:=a^(23<sum(s)+v)))*3for v in s]for s in p(g,c+1)])]
+# lambda g,c=-1,a=0:c*g or[*zip(*[[v+(v<(a:=a^(23<sum(s)+v)))*3for v in s]for s in p(g,c+1)])]
+p=lambda g,c=-1,a=0:c*g or[*zip(*[[v+(v<(a:=a^sum(s,v)//24))*3for v in s]for s in p(g,c+1)])]
