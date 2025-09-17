64b5a880a62b68983f346e4117d1880b19365880
key-moon <kymn0116@gmail.com>
2025-09-17 09:27:46 +0900

    342

--- base_yu/task342.py
+++ base_yu/task342.py
@@ -1,4 +1,4 @@
 # best: 111(jailctf merger) / others: 112(4atj sisyphus luke Seek mukundan), 119(xsot ovs att joking mewheni), 129(natte), 135(Yuchen20), 136(dbdr)
 # f p(g):i=sum(g,[]).index(8)//10;*x,=filter(int,sum([*zip(*g[:i]),*zip(*g[i+2:])],()));return[[v==8 and x.pop(0)for v in s]for s in g]
 # ==================================================== 111 ====================================================
-import re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub("([1-9])((.{32})+?[^[(]+?)8",r"0\2\1",str(p(g,c+1)))))][::-1]
+import re;p=lambda g,c=-3:c*g or[*zip(*eval(re.sub("([1-9])((.{32})+?[0, ]+)8",r"0\2\1",str(p(g,c+1)))))][::-1]
