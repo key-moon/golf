1916317db7a1741784d2ee9e4f76966062853e6d
Hiroki Shibata <shibata.hiroki.753@s.kyushu-u.ac.jp>
2025-09-18 19:59:48 +0900

    revert2

--- base_yu/task086.py
+++ base_yu/task086.py
@@ -1,34 +1,31 @@
 # best: 172(xsot ovs att joking mewheni) / others: 179(4atj sisyphus luke Seek mukundan), 192(jailctf merger), 233(intgrah jimboko awu macaque sammyuri), 243(natte), 272(MasukenSamba)
 # ================================================================================== 172 ===================================================================================
-import re
+
+# ↓このコードは [Yuくんのcommit](https://github.com/key-moon/golf/commit/11125032033087d2f61e09dcacaf810a6c1e4a0d) のもの
+# 変数名置換くんとの相性が悪くてなんか死んでるっぽいので一旦コメントアウトします、解決したら戻したい（Yuくんごめん）
+# import re
+# def p(g):
+ # c,d,_=sorted({*sum(g,[])},key=sum(g,[]).count)
+ # g=eval(re.sub("(?=[1-9])",r"c^d^",str(g)))
+ # g=[*zip(*eval(re.sub("0(?=(, %d|(, 0)?, %d, .), ., %d)"%(c,c,c),str(d),str(g)))[::-1])]
+ # g=[*zip(*eval(re.sub("0(?=(, %d|(, 0)?, %d, .), ., %d)"%(c,c,c),str(d),str(g)))[::-1])]
+ # g=[*zip(*eval(re.sub("0(?=(, %d|(, 0)?, %d, .), ., %d)"%(c,c,c),str(d),str(g)))[::-1])]
+ # g=[*zip(*eval(re.sub("0(?=(, %d|(, 0)?, %d, .), ., %d)"%(c,c,c),str(d),str(g)))[::-1])]
+# return g
+
+
 def p(g):
  c,d,_=sorted({*sum(g,[])},key=sum(g,[]).count)
- g=eval(re.sub("(?=[1-9])",r"c^d^",str(g)))
- g=[*zip(*eval(re.sub(f"0(?=(, {c}|(, 0)?, {c}, .), ., {c})",str(d),str(g)))[::-1])]
- g=[*zip(*eval(re.sub(f"0(?=(, {c}|(, 0)?, {c}, .), ., {c})",str(d),str(g)))[::-1])]
- g=[*zip(*eval(re.sub(f"0(?=(, {c}|(, 0)?, {c}, .), ., {c})",str(d),str(g)))[::-1])]
- g=[*zip(*eval(re.sub(f"0(?=(, {c}|(, 0)?, {c}, .), ., {c})",str(d),str(g)))[::-1])]
- # ↓この下4行は [Yuくんのcommit](https://github.com/key-moon/golf/commit/11125032033087d2f61e09dcacaf810a6c1e4a0d) のもの
- # 変数名置換くんとの相性が悪くてなんか死んでるっぽいので一旦コメントアウトします、解決したら戻したい（Yuくんごめん）
- # g=[*zip(*eval(re.sub("0(?=(, %d|(, 0)?, %d, .), ., %d)"%(c,c,c),str(d),str(g)))[::-1])]
- # g=[*zip(*eval(re.sub("0(?=(, %d|(, 0)?, %d, .), ., %d)"%(c,c,c),str(d),str(g)))[::-1])]
- # g=[*zip(*eval(re.sub("0(?=(, %d|(, 0)?, %d, .), ., %d)"%(c,c,c),str(d),str(g)))[::-1])]
- # g=[*zip(*eval(re.sub("0(?=(, %d|(, 0)?, %d, .), ., %d)"%(c,c,c),str(d),str(g)))[::-1])]
- return g
-
-
-# def p(g):
-#  c,d,_=sorted({*sum(g,[])},key=sum(g,[]).count)
-#  u=[s[:]for s in g]
-#  for i in range(len(g)):
-#   for j in range(len(g[0])):
-#    for k in range(4):
-#     if any(all(len(g)>i+~-abs(k-1)*t>-1<j+~-abs(k-2)*t<len(g[0])and d==g[i+~-abs(k-1)*t][j+~-abs(k-2)*t]for t in[s//2+1,s+3])for s in range(3)):
-#     # if any(all(len(g)>i+~-abs(k-1)*t>-1<j+~-abs(k-2)*t<len(g[0])and d==g[i+~-abs(k-1)*t][j+~-abs(k-2)*t]for t in s)for s in((1,3),(1,4),(2,5))):
-#      u[i][j]=d
-#    if g[i][j]:
-#     u[i][j]=g[i][j]^c^d
-#  return u
+ u=[s[:]for s in g]
+ for i in range(len(g)):
+  for j in range(len(g[0])):
+   for k in range(4):
+    if any(all(len(g)>i+~-abs(k-1)*t>-1<j+~-abs(k-2)*t<len(g[0])and d==g[i+~-abs(k-1)*t][j+~-abs(k-2)*t]for t in[s//2+1,s+3])for s in range(3)):
+    # if any(all(len(g)>i+~-abs(k-1)*t>-1<j+~-abs(k-2)*t<len(g[0])and d==g[i+~-abs(k-1)*t][j+~-abs(k-2)*t]for t in s)for s in((1,3),(1,4),(2,5))):
+     u[i][j]=d
+   if g[i][j]:
+    u[i][j]=g[i][j]^c^d
+ return u
 
 # def p(g):
 #  G=sum(g,[])
