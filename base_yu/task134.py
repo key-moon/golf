0cc9bcc8a0baca2a178f7f552dbb4cfad25fa826
Yu212 <yu212.mc@gmail.com>
2025-09-20 00:44:34 +0900

    134

---
+++ base_yu/task134.py
@@ -0,0 +1,8 @@
+# best: 168(biz) / others: 181(jailctf merger), 181(jacekwl Potatoman nauti), 186(4atj sisyphus luke Seek mukundan), 208(xsot ovs att joking mewheni), 208(ox jam)
+# ================================================================================ 168 =================================================================================
+def p(g):
+ _,n,m={*sum(g,[])}
+ if 5<str(g).count(f"{n}, "*2):n,m=m,n
+ u=[s for s in g if m in s]
+ k=len(u)//3
+ return[[n*(v==m)for*t,v in zip(*g,s)if m in t][::k]for s in u[::k]]
