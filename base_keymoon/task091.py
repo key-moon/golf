2e1752fd2fac53bb8c9ce30acb920d2308317e84
Yu212 <yu212.mc@gmail.com>
2025-09-19 12:49:21 +0900

    091

--- base_keymoon/task091.py
+++ base_keymoon/task091.py
@@ -1,7 +1,9 @@
 # best: 63(ox jam, 4atj sisyphus luke Seek mukundan, jailctf merger, intgrah jimboko awu macaque sammyuri, xsot ovs att joking mewheni) / others: 107(jacekwl Potatoman nauti), 110(Yuchen20), 111(natte), 116(MasukenSamba), 119(Afordancja)
 # ============================= 63 ============================
-# lambda g,c=-47:c*g or p([*zip(*g[1-(5in g[c&1]):][::-1])],c+1)
-p=lambda g,c=-47:c*g or p([*zip(*g[5not in g[c&1]:][::-1])],c+1)
+# p=lambda g,c=-47:c*g or p([*zip(*g[1-(5in g[c&1]):][::-1])],c+1)
+# p=lambda g,c=-47:c*g or p([*zip(*g[5not in g[c&1]:][::-1])],c+1)
+# p=lambda g,c=-47:c*g or p([*zip(*g[(5in g[-1-c%2])-2::-1])],c+1)
+p=lambda g,c=47:-c*g or p([*zip(*g[(5in g[~c|62])-2::-1])],c-1)
 
 # 1-(5in g[c&1])
 # 5not in g[c&1]
