440bd7baf2234257c8c341e543a825bb9956eae4
Yu212 <yu212.mc@gmail.com>
2025-09-20 20:50:20 +0900

    131

--- base_yu/task131.py
+++ base_yu/task131.py
@@ -2,7 +2,11 @@
 # =========================================================== 125 ===========================================================
 
 # p=lambda g,c=-3:c*g or p([*zip(*(2in(f:=[*map(max,g)])and(k:=f.index(2))>f.index(3)and sorted([[8]*len(g[0])]+g[:k],key=any)[1:]+g[k:]or g)[::-1])],c+1)
-p=lambda g,c=-3:c*g or p([*zip(*(g*(2in max(g,key=any))or sorted([[8]*len(g[0])]+g[:(k:=[*map(max,g)].index(2))],key=any)[1:]+g[k:])[::-1])],c+1)
+# p=lambda g,c=-3:c*g or p([*zip(*(g*(2in max(g,key=any))or sorted([[8]*len(g[0])]+g[:(k:=[*map(max,g)].index(2))],key=any)[1:]+g[k:])[::-1])],c+1)
+# p=lambda g,c=-3:c*g or p([*zip(*(sorted([[8]*len(g[0])]+g[:(k:=(2not in max(g,key=any))*[*map(max,g),2].index(2))],key=any)[1:]+g[k:])[::-1])],c+1)
+# p=lambda g,c=-3:c*g or p([*zip(*sorted([[8]*len(g[0])]+g[:(k:=(f:=(t:=[*map(max,g),2]).index(2))*any(t[:f]))],key=any)[1:]+g[k:])][::-1],c+1)
+# p=lambda g,c=-3:c*g or p([*zip(*(sorted([[8]*9]+g[:(k:=(f:=(t:=[*map(max,g),2]).index(2))*any(t[:f]))],key=any)+g[k:])[:0:-1])],c+1)
+p=lambda g,c=-3:c*g or p([*zip(*(sorted([[8]*9]+g[:(k:=g.index(l:=max(g[::-1],key=any))*(2in l))],key=any)+g[k:])[:0:-1])],c+1)
 
 # def p(g):
 #  for i in range(4):
