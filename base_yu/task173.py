75169d69c63319fc411d81dc5de2c90e670bee47
Yu212 <yu212.mc@gmail.com>
2025-09-21 03:41:52 +0900

    173

--- base_yu/task173.py
+++ base_yu/task173.py
@@ -4,8 +4,6 @@ def p(g):
   for j in range(len(g[0])-2):
    for y in range(len(g)-2):
     for x in range(len(g[0])-2):
-     a=[g[i+k//3][k%3+j] for k in range(9)]
-     if a==a[::-1]and sum(a)>a[4]>0and(sum(g[y+k//3][k%3+x]==g[i+k//3][k%3+j]for k in range(9))>7or a[4]==g[y+1][1+x]):
-      for k in range(9):
-       g[y+k//3][k%3+x]=g[i+k//3][k%3+j]
+     for k in range(9*(sum(a:=[g[i+k//3][k%3+j] for k in range(9)])>a[4]>0and a==a[::-1]and(sum(g[y+k//3][k%3+x]==g[i+k//3][k%3+j]for k in range(9))>7or a[4]==g[y+1][1+x]))):
+      g[y+k//3][k%3+x]=g[i+k//3][k%3+j]
  return g
