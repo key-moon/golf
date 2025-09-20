e5ad75a346787e6fd3d4ad3f99447993a2a69b66
Hiroki Shibata <shibata.hiroki.753@s.kyushu-u.ac.jp>
2025-09-20 14:22:23 +0900

    349

--- base_yu/task349.py
+++ base_yu/task349.py
@@ -3,10 +3,10 @@ def p(g):
  for i in range(n:=len(g)):
   for l in range(n):
    if (s:=g[i])[r:=l]>8:
-    while r<n and s[r]>8:r+=1
+    while s[r:]>[8]:r+=1
+    # while r<n and s[r]>8:r+=1
     for t in g[i:]:
-     for j in range(l,r):
-      t[j]=t[j]or 1
+     for j in range(l,r):t[j]|=1
     for k in range(i-(u:=r-l>>1),i+u+1):
      for j in range(l-u,r+u):
       if-1<k<n>j>-1and g[k][j]<9:g[k][j]=3
