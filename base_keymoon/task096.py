8bb93959e66179b34696b7169559f6175647a9ba
Hiroki Shibata <shibata.hiroki.753@s.kyushu-u.ac.jp>
2025-09-20 11:28:54 +0900

    096

--- base_keymoon/task096.py
+++ base_keymoon/task096.py
@@ -171,11 +171,11 @@
 from	random	import	*
 def	p(f,s=4):
 	seed([2024-s,'+u-)+e[','41-efe[','41*l}{','f(3,+e['][s*(0<s)])
-	g,t,d=randrange(13,20)==len(f[2]),randrange(13,20)==len(f),randrange(1,10)
-	u=sample([*{*range(1,10)}^{d*(s<0)}],a:=4+randrange(3))
+	g,t,d=randrange(13,20)==len(f[0]),randrange(13,20)==len(f),randrange(1,10)
+	u=sample([*{*range(1,10)}^{d*(s<0)}],a:=randrange(4,7))
 #	l=[1+(0<i)+randrange(max(1,i-1))for	i	in	range(a)]
-	e=[randrange(1+(0<i),1+(i<2)+i)for	i	in	range(a)]
-	o=[(2*a-1)*[d]for	i	in	range(a)]
+	e=[1+randrange(0<i,(i<2)+i)for	i	in	range(a)]
+	o=[(a*2-1)*[d]for	i	in	range(a)]
 	for	i	in	range(a):
 		# for	l	in	range(e[i]):o[-1-i][a-1-i+l]=o[l-1-i][a-1-i]=o[-1-i][a-1+i-l]=o[l-1-i][a-1+i]=u[i]
 		for	l	in	range(e[i]):l-=i;o[l-1][a+i-1]=o[l-1][a-i-1]=o[-i-1][a+l-1]=o[-i-1][a-l-1]=u[i]
