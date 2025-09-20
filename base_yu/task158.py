342fd11fdb684b67d7fbbe545ac6c92a214f748c
Yu212 <yu212.mc@gmail.com>
2025-09-21 02:27:42 +0900

    158

---
+++ base_yu/task158.py
@@ -0,0 +1,18 @@
+# best: 269(xsot ovs att joking mewheni, ox jam, jailctf merger) / others: 322(4atj sisyphus luke Seek mukundan), 325(HashPanda Pooja), 325(garrymoss), 325(Rafael Pooja), 325(natte)
+def p(g):
+ for i in range(len(g)-2):
+  for j in range(len(g[0])-2):
+   u=[g[i+k][j:j+3]for k in range(3)]
+   if len({*sum(u,[])})>3:
+    d=max(g[0],key=g[0].count)
+    c,={*u[1]}-{d}
+    for _ in range(4):
+     for s in range(1,4):
+      for y in range(len(g)-3*s):
+       for x in range(len(g[0])-3*s):
+        if all(g[y+k][x+l]==[u[k//s][l//s],d][u[k//s][l//s]==c]for k in range(3*s)for l in range(3*s)):
+         for k in range(3*s):
+          for l in range(3*s):
+           g[y+k][x+l]=u[k//s][l//s]
+     u[:]=zip(*u[::-1])
+    return g
