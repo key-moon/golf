6c2ff33181c96a10c49aa7fd529458f3ffc4bbff
Yu212 <yu212.mc@gmail.com>
2025-09-20 22:55:55 +0900

    358

--- base_yu/task358.py
+++ base_yu/task358.py
@@ -2,4 +2,4 @@
 # ============================================== 97 =============================================
 # p=lambda g,c=-1:c*g or p([1<(t:=len(s)-s.count(0))and[sum(s[i%t::t])for i in range(len(s))]or s for s in zip(*g)],c+1)
 # p=lambda g,c=-1:c*g or p([[s,[sum(s[i%(t:=~s.count(0)%len(s)+1)::t])for i in range(len(s))]][t>1]for s in zip(*g)],c+1)
-p=lambda g,c=-1:c*g or p([1<(t:=sum(map(bool,s)))and[sum(s[i%t::t])for i in range(len(s))]or s for s in zip(*g)],c+1)
+p=lambda g,c=-1:c*g or[1<(t:=sum(map(bool,s)))and[sum(s[i%t::t])for i in range(len(s))]or s for s in zip(*p(g,c+1))]
