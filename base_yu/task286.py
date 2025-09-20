d0f0ad197cee1c3b1c142edf58976c83d1d5038a
key-moon <kymn0116@gmail.com>
2025-09-21 02:59:28 +0900

    286

--- base_yu/task286.py
+++ base_yu/task286.py
@@ -5,7 +5,8 @@
 # import re;p=lambda g,c=-359:c*g or p(eval(re.sub("0(?=, [1-79](.{%d})?, ([1-79]))"%~-len(g*3),r"\2",str([*zip(*g[::1|c%3%-2])]))),c+1)
 
 # p=lambda g,c=-359:c*g or[*zip(*[(l:=0)or[(v<1<=l%8and sum({*sum(g,[])})-8-l)+(l:=v)for v in s]for s in p(g,c+1)][::-1])]
-p=lambda g,c=-359:c*g or[(l:=0)or[(v<1<=l%8and sum({*sum(g,[])})-8-l)+(l:=v)for v in s]for s in zip(*p(g,c+1)[::-1])]
+# lambda g,c=-359:c*g or[(l:=0)or[(v<1<=l%8and sum({*sum(g,[])})-8-l)+(l:=v)for v in s]for s in zip(*p(g,c+1)[::-1])]
+p=lambda g,c=-359:c*g or[(l:=0)or[(v<1<=l%8)*sum({*sum(g,[-8-l])})+(l:=v)for v in s]for s in zip(*p(g,c+1)[::-1])]
 
 # def p(g):
 #  u=sum({*sum(g,[])})-8
