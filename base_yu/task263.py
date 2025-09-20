b95d1bd5b2cb06632f72c1df125d732d4f66c132
key-moon <kymn0116@gmail.com>
2025-09-20 22:45:51 +0900

    263

--- base_yu/task263.py
+++ base_yu/task263.py
@@ -1,7 +1,10 @@
 # best: 122(ox jam, jailctf merger, xsot ovs att joking mewheni) / others: 130(4atj sisyphus luke Seek mukundan), 141(JRKX), 141(jonas ryno kg583 kabutack), 141(jonas ryno kg583), 158(natte)
 # ========================================================= 122 ==========================================================
-p=lambda g:len(g)>3and(f:=sorted([(sum(t:=g[i:i+3],g[0]).count(0),t)for i in range(0,len(g),3)]))[-(f[0][0]==f[1][0])][1]or[*zip(*p([*zip(*g)]))]
-#  f=sorted([((g[i]+g[i+1]+g[i+2]).count(0),g[i:i+3])for i in range(0,len(g),3)])
+# lambda g:len(g)>3and(f:=sorted([(sum(t:=g[i:i+3],g[0]).count(0),t)for i in range(0,len(g),3)]))[-(f[0][0]==f[1][0])][1]or[*zip(*p([*zip(*g)]))]
+# lambda g:len(g)>3and(f:=sorted([(str(t:=g[i:i+3]).count("0"),t)for i in range(0,len(g),3)]))[-(f[0][0]==f[1][0])][1]or[*zip(*p([*zip(*g)]))]
+# lambda g:len(g)>3and(f:=sorted(zip(*[iter(g)]*3),key=(h:=lambda t:str(t).count("0"))))[-(h(f[0])==h(f[1]))][1]or[*zip(*p([*zip(*g)]))]
+# lambda g,c=1:g*-c or(f:=sorted((str(t).count("0"),t)for t in zip(*[iter(zip(*p([*zip(*g)],c-1)))]*3))*2)[-(f[0][0]==f[1][0])][1]
+p=lambda g:len(g)>3and(f:=sorted((str(t).count("0"),t)for t in zip(*[iter(g)]*3)))[-(f[0][0]==f[1][0])][1]or[*zip(*p([*zip(*g)]))]
 
 
 # def p(g):
