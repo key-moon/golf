a42d6aad145a999c4ea0faf22253b8a768a16997
Yu212 <yu212.mc@gmail.com>
2025-09-21 16:55:03 +0900

    107

--- base_yu/task107.py
+++ base_yu/task107.py
@@ -3,9 +3,8 @@
 import re
 def p(g):
  c=len({*str(g)})-5
- f=lambda s:sum([[v*0!=0and f(v)or v]*c for v in s],[])
- t=f(g)
+ t=sum([c*[sum(zip(*[s]*c),())]for s in g],[])
  n=len(t)*3
  for _ in range(4):
-  t=[*zip(*eval(re.sub("0(?=(.{%d})*, 0.{%d}0, [13-9])"%(n+5,n-2),"2",str(t)))[::-1])]
+  t=[*zip(*eval(re.sub("0(?=(.{%d})*, 0.{%d}0, [^02])"%(n+5,n-2),"2",str(t)))[::-1])]
  return t
