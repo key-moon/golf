462d2ca9f9bbcb2db7a1c759f90e0a90259c39d8
Yu212 <yu212.mc@gmail.com>
2025-09-21 16:51:57 +0900

    269,289

--- base_yu/task269.py
+++ base_yu/task269.py
@@ -2,4 +2,6 @@
 # ============================= 63 ============================
 # p=lambda s,c=0:sum([[v*0!=0and p(v,c:=9-sum(s,[]).count(0))or v]*c for v in s],[])
 # p=lambda s,c=0:sum([[v*0!=0and p(v,c:=9-str(s).count("0"))or v]*c for v in s],[])
-p=lambda s,c=0:sum([[v*0!=0and p(v,c:=len({*sum(s,v)})-1)or v]*c for v in s],[])
+# p=lambda s,c=0:sum([[v*0!=0and p(v,c:=len({*str(s)})-5)or v]*c for v in s],[])
+# p=lambda g,c=-1:c*g or[*zip(*sum(zip(*[p(g,c+1)]*(len({*str(g)})-5)),()))]
+p=lambda g:sum([(k:=len({*str(g)})-5)*[sum(zip(*[s]*k),())]for s in g],[])
