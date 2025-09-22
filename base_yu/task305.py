07f6ab59b0fd73bc4e63eb212ba0a6f464f5af60
key-moon <kymn0116@gmail.com>
2025-09-22 14:29:40 +0900

    305

--- base_yu/task305.py
+++ base_yu/task305.py
@@ -1,3 +1,4 @@
 # best: 57(Yuchen20) / others: 62(ox jam), 62(xsot ovs att joking mewheni), 63(jailctf merger), 64(4atj sisyphus luke Seek mukundan), 64(JRKX)
 # ========================== 57 =========================
-p=lambda g,R=range(16):[[(i+j)%max(g[i])+1for j in R]for i in R]
+# lambda g,R=range(16):[[1+(i+j)%max(g[i])for j in R]for i in R]
+p=lambda g,R=range(16):[R:=[1+j%max(g[0])for j in R]for i in R]
