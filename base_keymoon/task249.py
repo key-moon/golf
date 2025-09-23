d8ab6b9794cf1a94f3a5cddc362adffe06f7a4b0
key-moon <kymn0116@gmail.com>
2025-09-23 22:19:24 +0900

    288

---
+++ base_keymoon/task249.py
@@ -0,0 +1,3 @@
+# best: 26(jacekw, cg-klogw, 4atj sisyphus luke Seek mukundan, cg, open source, KN, sekken, HashPanda, azakhtyamov, adakoda, Potatoman, kdmitrie, jacekw Potatoman nauti, el_presidente, JRKX, jonas ryno kg583, Yuchen20, duckyluuk, MKRC, ox jam, cubbus, jacekwl Potatoman nauti, Rafael Pooja, Afordancja, HashPanda Pooja, HETHAT, rucin93, dbdr, blob2822, scpchicken, kabutack, jailctf merger, JRK, jonas ryno kg583 kabutack, Ali, MasukenSamba, J&R, natte, intgrah jimboko awu macaque sammyuri, jacekwl) / others: 27(GPU Poor), 28(klogw)
+# ========== 26 ==========
+p=lambda g:[s*2for s in g]
---
+++ base_keymoon/task288.py
@@ -0,0 +1,13 @@
+# best: 89(jailctf merger) / others: 90(jacekw Potatoman nauti), 93(Yuchen20), 94(cg-klogw), 95(4atj sisyphus luke Seek mukundan), 97(MKRC)
+# ========================================== 89 =========================================
+# 112
+def p(g,a=3):
+ l=len(g)//2
+ for i in range(2-0**g[-2][l-1],l+1):g[-a][l-i]=g[-a][l+i]=g[-1][l];a+=1
+ return g
+# 111
+# def p(g,a=1):
+#  l=len(g)//2
+#  for s in g[-2-0**g[-2][l-1]::-1][:l]:s[l-a]=s[l+a]=s[l+a]or g[-1][l];a+=1
+#  return g
+
