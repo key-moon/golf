b96d83ed57205a0500bc430d70431c5dc7fc6d32
Yu212 <yu212.mc@gmail.com>
2025-09-23 16:54:21 +0900

    233

--- base_yu/task233.py
+++ base_yu/task233.py
@@ -1,19 +1,19 @@
 # best: 297(ox jam, jailctf merger, xsot ovs att joking mewheni) / others: 336(natte), 355(jacekw Potatoman nauti), 355(jacekwl Potatoman nauti), 367(4atj sisyphus luke Seek mukundan), 370(Rafael Pooja)
 def p(g):
- u=[s*1for s in g]
+ u=[[*s]for s in g]
  for _ in range(80):
-  u=[*map(list,zip(*u[("2, "*7in str(u[-1]))-2::-1]))]
+  u=[[*s]for s in zip(*u[("2, "*7in str(u[-1]))-2::-1])]
  for _ in range(8):
   for i in range(len(g)-2):
    for j in range(len(g[0])-2):
     for y in range(len(u)-2):
      for x in range(len(u[0])-2):
-      if all(g[i+k][j+m]==2 if u[y+k][x+m]==0 else g[i+k][j+m]|2!=2 for k in range(3)for m in range(3))*(y<1 or 0<u[y-1][x+1])*(y+4>len(u) or 0<u[y+3][x+1]):
+      if all([2==g[i+k][j+m],2!=g[i+k][j+m]|2][u[y+k][x+m]>0]for k in range(3)for m in range(3))*(u+[[1]*80]*3)[y-1][x+1]*(u+[[1]*80]*3)[y+3][x+1]:
        for k in range(3):
         for m in range(3):
          u[y+k][x+m]=g[i+k][j+m]
          g[i+k][j+m]=0
-  u=[*map(list,zip(*u[::-1]))]
+  u=[[*s]for s in zip(*u[::-1])]
  return u
 
 # def p(g):
