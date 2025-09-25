d542e4ce08f60f28a5b8d77dd097449bdbb60b24
Yu212 <yu212.mc@gmail.com>
2025-09-25 17:06:13 +0900

    066

--- base_yu/task066.py
+++ base_yu/task066.py
@@ -1,24 +1,57 @@
 # best: 268(4atj sisyphus luke Seek mukundan) / others: 275(ox jam), 290(jailctf merger), 299(natte), 303(duckyluuk), 306(Yuchen20)
 def p(g):
- n=len(g)
- R=range(n*n)
- for v in[e:=1,1,1,0]*2:
-  a,b,*_=[I for I in R if g[I//n][I%n]==3]
-  y=[I for I in R if g[I//n][I%n]==2][0]
-  l=r=[*g[b//n],8].index(8,b%n)
-  u=[*[s[r-1] for s in g],8].index(8,b//n)
-  w=y%n
-  if l>w:l,w=w+2,l
-  if(abs(a-b)==e)*(u==y//n+1)*~-(8*(r<n)in g[y//n][l:w]):
-#   if(abs(a-b)==e)*(u==y//n+1)*~-(8 in g[y//n][l:w])*(r<n):
-   g[b//n][b%n+1:r]=[3]*(r-b%n-1)
-   for s in g[b//n:u]:s[r-1]=e=3
-#    for i in R(b//n,u):g[i][r-1]=3
-   g[y//n][l:w]=[3]*(w-l)
-  if v:g=g[::-1]
-  # *g,=map(list,zip(*g))
-  g[:]=map(list,zip(*g))
- return g
+ d=[1,0,-1,0,1]
+ for k in range(16):
+  for i in range(len(g)):
+   for j in range(len(g)):
+    y,x=i,j
+    if g[y][x]==3:
+     a=3-k//4
+     c=0
+     o=0
+     t=[s*1 for s in g]
+     while 1:
+      u,v=y+d[a],x+d[a+1]
+      if len(g)>u>-1<v<len(g):
+       if t[u][v]==8:
+        a=(a+(k>>c&1)*2-1)%4
+        c+=1
+        if c>2:
+         break
+        continue
+       if t[u][v]==2:
+        if o:
+         return t
+        break
+       if t[u][v]==3:
+        o=1
+      else:
+       break
+      t[u][v]=3
+      y,x=u,v
+
+ 
+
+# def p(g):
+#  n=len(g)
+#  R=range(n*n)
+#  for v in[e:=1,1,1,0]*2:
+#   a,b,*_=[I for I in R if g[I//n][I%n]==3]
+#   y=[I for I in R if g[I//n][I%n]==2][0]
+#   l=r=[*g[b//n],8].index(8,b%n)
+#   u=[*[s[r-1] for s in g],8].index(8,b//n)
+#   w=y%n
+#   if l>w:l,w=w+2,l
+#   if(abs(a-b)==e)*(u==y//n+1)*~-(8*(r<n)in g[y//n][l:w]):
+# #   if(abs(a-b)==e)*(u==y//n+1)*~-(8 in g[y//n][l:w])*(r<n):
+#    g[b//n][b%n+1:r]=[3]*(r-b%n-1)
+#    for s in g[b//n:u]:s[r-1]=e=3
+# #    for i in R(b//n,u):g[i][r-1]=3
+#    g[y//n][l:w]=[3]*(w-l)
+#   if v:g=g[::-1]
+#   # *g,=map(list,zip(*g))
+#   g[:]=map(list,zip(*g))
+#  return g
 
 
 # def p(g):
