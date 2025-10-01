cb619b6d62d648cbcebbdd15965a2e365f6a19c9
Yu212 <yu212.mc@gmail.com>
2025-10-01 20:18:51 +0900

    099

--- base_yu/task099.py
+++ base_yu/task099.py
@@ -1,9 +1,16 @@
 # best: 115(4atj sisyphus luke Seek mukundan, jailctf merger) / others: 116(ox jam), 143(intgrah jimboko awu macaque sammyuri), 161(Afordancja), 182(jacekwl Potatoman nauti), 182(jacekw Potatoman nauti natte)
 # ====================================================== 115 ======================================================
-def p(g):
- w=1-any(g[1])
- u=g[w:]
- return(len(g)<6)*(g[:w]+[[v or any(t)*max(sum(g,[]))for*t,v in zip(*u,s)]for s in u])or p(g[:5])+p(g[5:])
+# def p(g):
+#  w=1-any(g[1])
+#  u=g[w:]
+#  return(len(g)<6)*(g[:w]+[[v or any(t)*max(sum(g,[]))for*t,v in zip(*u,s)]for s in u])or p(g[:5])+p(g[5:])
+
+# p=lambda g:(len(g)<6)*(g[:(w:=1-any(g[1]))]+[[v or any(t[1:])*max(sum(g,[]))for*t,v in zip(*g,s)]for s in g[w:]])or p(g[:5])+p(g[5:])
+# p=lambda g:[(i%5==-any(g[i//5*5+1]))*g[i] or [v or any(t[i//5*5:][1:5])*max(sum(g[i//5*5:][:5],[]))for*t,v in zip(*g,g[i])]for i in range(10)]
+# p=lambda g:[(i%5==-any(g[f:=i//5*5+1]))*g[i]or[v or any(t[f:f+5])*max(sum(g[f-1:f+4],[]))for*t,v in zip(*g,g[i])]for i in range(10)]
+p=lambda g:[[v or(i%5!=-any(g[f:=i-i%5+1]))*any(t[f:f+5])*max(sum(g[f:f+4],[]))for*t,v in zip(*g,g[i])]for i in range(10)]
+
+
 
 # def q(g):
 #  w=1-any(g[1])
