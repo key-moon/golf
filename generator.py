c6eab8d23b58d547d71f321183ab554e673f8edb
Hiroki Shibata <shibata.hiroki.753@s.kyushu-u.ac.jp>
2025-10-01 17:09:06 +0900

    update generator

--- generator.py
+++ generator.py
@@ -245,8 +245,7 @@ if __name__ == "__main__":
       exit(1)
 
     assert best_result.base_path is not None
-    # retire all other codes (if not /dist and genetic_algo)
-    if not best_result.base_path.startswith("dist") and 'genetic-algo' not in best_result.compressor:
+    if not best_result.base_path.startswith("dist"):
       for base_path in get_code_paths("base_*", i):
         if base_path == best_result.base_path: continue
         if base_path.split("/")[0] in DISALLOW_RETIRE: continue
