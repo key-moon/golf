6544cbb166d25e45b05fbbde53744ec9befe27cb
kq5y <contact@kq5.jp>
2025-10-11 10:26:22 +0000

    107

--- base_yu/task107.py
+++ base_yu/task107.py
@@ -5,6 +5,6 @@ def p(g):
  c=len({*str(g)})-5
  t=sum([c*[sum(zip(*[s]*c),())]for s in g],[])
  n=len(t)*3
- for _ in range(4):
+ for _ in'1'*4:
   t=[*zip(*eval(re.sub("0(?=(.{%d})*, 0.{%d}0, [^02])"%(n+5,n-2),"2",str(t)))[::-1])]
  return t
