58d617f62aa1cd4f94c741ed70bb59f998330a26
Yu212 <yu212.mc@gmail.com>
2025-10-29 01:20:14 +0900

    319

--- base_yu/task319.py
+++ base_yu/task319.py
@@ -11,18 +11,28 @@
 #      return[[v for*t,v in zip(*g,s)if u[0]in t]for s in g if u[0]in s]
 #  return g
 
+# def p(g):
+# #  B=max({*sum(g,[])},key=sum(g,[]).count)
+#  for c in range(10):
+#   for d in range(10):
+#    u={(i,j)for i in range(len(g))for j in range(len(g[i]))if g[i][j]==c}
+#    v={(i,j)for i in range(len(g))for j in range(len(g[i]))if g[i][j]==d}
+#    w={(i,j)for i in range(len(g))for j in range(len(g[i]))}
+#    if len(u)<30 and len(v)<60 and any(w&{((i-y)*2+a+k,(j-x)*2+b+l) for i,j in u for k in (0,1)for l in (0,1)}==v for y,x in u for a,b in v):
+#     return [[[max({*t},key=(s+t).count),v][v==c] for*t,v in zip(*g,s)if c in t]for s in g if c in s]
+# #     return [[[B,v][v==c] for*t,v in zip(*g,s)if c in t]for s in g if c in s]
+# #     return [[v for*t,v in zip(*g,s)if c in t]for s in g if c in s]
 
 # def p(g):
 #  *C,B=sorted({*sum(g,[])},key=sum(g,[]).count)
 #  for c in C:
 #   for d in C:
-#    u=[(i,j)for i in range(len(g))for j in range(len(g[i]))if g[i][j]==c]
-#    v=[(i,j)for i in range(len(g))for j in range(len(g[i]))if g[i][j]==d]
-#    for y,x in u:
-#     for a,b in v:
-#      w=[((i-y)*2+a+k,(j-x)*2+b+l) for i,j in u for k in (0,1)for l in (0,1) if len(g)>(i-y)*2+a+k>-1<(j-x)*2+b+l<len(g[0])]
-#      if v==sorted(w):
-#       return [[[B,v][v==c] for*t,v in zip(*g,s)if c in t]for s in g if c in s]
+#    u={(i,j)for i in range(len(g))for j in range(len(g[i]))if g[i][j]==c}
+#    v={(i,j)for i in range(len(g))for j in range(len(g[i]))if g[i][j]==d}
+#    w={(i,j)for i in range(len(g))for j in range(len(g[i]))}
+#    if any(w&{(i*2-y*2+a+k,j*2-x*2+b+l) for i,j in u for k in (0,1)for l in (0,1)}==v for y,x in u for a,b in v):
+#     return [[[B,v][v==c] for*t,v in zip(*g,s)if c in t]for s in g if c in s]
+
 
 def p(g):
  *C,b=sorted({*sum(g,[])},key=sum(g,[]).count)
@@ -32,7 +42,6 @@ def p(g):
     d=[g[i*2+y+k][j*2+x+l]for i in range(len(g))for j in range(len(g[i]))for k in range(2)for l in range(2)if g[i][j]==c if i*2+y+k in range(len(g))if j*2+x+l in range(len(g[i]))]
     if []<d==[d[0]]*sum(g,[]).count(d[0]):
      return [[[b,v][v==c] for*t,v in zip(*g,s)if c in t]for s in g if c in s]
-
 
 # def p(g):
 #  *C,b=sorted({*sum(g,[])},key=sum(g,[]).count)
