2ec50c0300045224dfdc915ced6125f4de8b596b
Yu212 <yu212.mc@gmail.com>
2025-10-30 01:30:28 +0900

    105

--- base_yu/task105.py
+++ base_yu/task105.py
@@ -1,6 +1,11 @@
 # best: 126(import itertools) / others: 145(jailctf merger), 148(Code Golf International), 148(4atj sisyphus luke Seek mukundan), 152(intgrah jimboko awu macaque sammyuri), 153(ox jam)
 # =========================================================== 126 ============================================================
-p=lambda g,c=33:g*-c or c<2and p([*zip(*[[v or(s[1:-1].count(1)>1or i%~-len(g)<1)*2for v in s]for i,s in enumerate(g)])],c-1)or[*zip(*p([*zip(*g[any(g[-1])-2::-1])],c-1))][::-1]+g[-1:]
+# p=lambda g,c=33:g*-c or c<2and p([*zip(*[[v or(s[1:-1].count(1)>1or i%~-len(g)<1)*2for v in s]for i,s in enumerate(g)])],c-1)or[*zip(*p([*zip(*g[any(g[-1])-2::-1])],c-1))][::-1]+g[-1:]
+# p=lambda g,c=35:g*-c or c<4and p([*zip(*[[v or(s[1:-1].count(1)>(i>0))*2for v in s]for i,s in enumerate(g)][::-1])],c-1)or[*zip(*p([*zip(*g[any(g[-1])-2::-1])],c-1))][::-1]+g[-1:]
+# p=lambda g,c=35:g*-c or c<4and p([*zip(*[[v or(i<s[1:-1].count(1)**3)*2for v in s]for i,s in enumerate(g)][::-1])],c-1)or[*zip(*p([*zip(*g[any(g[-1])-2::-1])],c-1))][::-1]+g[-1:]
+# p=lambda g,c=35,f=0:g*-c or c<4and p([*zip(*[f:=[v or(s[1:-1].count(1)>1 or f==0)*2for v in s]for s in g][::-1])],c-1)or[*zip(*p([*zip(*g[any(g[-1])-2::-1])],c-1))][::-1]+g[-1:]
+# p=lambda g,c=35,f=0:g*-c or c<4and p([*zip(*[[v or(f<s[1:-1].count(1)**3)*2for v in s]*(f:=1)for s in g][::-1])],c-1)or[*zip(*p([*zip(*g[any(g[-1])-2::-1])],c-1))][::-1]+g[-1:]
+p=lambda g,c=35,f=0:g*-c or c<4and[*zip(*[[v or(f<s[1:-1].count(1)**3)*2for v in s]*(f:=1)for s in p(g,c-1)][::-1])]or[*zip(*p([*zip(*g[any(g[-1])-2::-1])],c-1))][::-1]+g[-1:]
 
 # def p(g,c=-31):
 #  if c==3:
